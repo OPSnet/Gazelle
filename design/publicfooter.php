1446344d046c2c1ecedee389cd6ff08431367959
itismadness <it.is.madness@gmail.com>
2017-01-19 15:49:23 -0500

    remove more what.cd references

--- design/publicfooter.php
+++ design/publicfooter.php
@@ -2,7 +2,7 @@
 	</tr>
 </table>
 <div id="foot">
-	<span><a href="#"><?=SITE_NAME?></a> | <a href="https://what.cd/gazelle/">Project Gazelle</a></span>
+	<span><a href="#"><?=SITE_NAME?></a> | <a href="https://github.com/whatcd/gazelle">Project Gazelle</a></span>
 </div>
 </body>
 </html>
