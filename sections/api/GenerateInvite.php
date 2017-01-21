101a0ef150fb3c87ac0301611a809346e2a42dec
itismadness <it.is.madness@gmail.com>
2017-01-21 14:21:51 -0500

    send email and return registration link

--- sections/api/GenerateInvite.php
+++ sections/api/GenerateInvite.php
@@ -4,7 +4,28 @@ require_once(SERVER_ROOT.'/sections/api/AbstractAPI.php');
 
 class GenerateInvite extends AbstractAPI {
     public function run() {
-        $interviewer = db_string($_GET['interviewer']);
+        if (!isset($_GET['interviewer_id']) && !isset($_GET['interviewer_name'])) {
+            error('Missing interviewer_id or interviewer_name');
+        }
+        
+        if (empty($_GET['email'])) {
+            error('Missing invite email address');
+        }
+    
+        if (isset($_GET['interviewer_id'])) {
+            $where = "ID='".intval($_GET['interviewer_id'])."'";
+        }
+        else {
+            $where = "Username='".db_string($_GET['interviewer_name'])."'";
+        }
+        $this->db->query("SELECT ID, Username FROM users_main WHERE {$where}");
+        if ($this->db->record_count() === 0) {
+            error("Could not find interviewer");
+        }
+        $user = $this->db->next_record();
+        $interviewer_id = $user['ID'];
+        $interviewer_name = $user['Username'];
+        
         $email = db_string($_GET['email']);
         $expires = time_plus(60 * 60 * 24 * 3); // 3 days
         $key = db_string(make_secret());
@@ -21,16 +42,29 @@ class GenerateInvite extends AbstractAPI {
             error("Invite code already generated for this email address");
         }
         
-        $this->db->query("SELECT ID FROM users_main WHERE Username='{$interviewer}'");
-        if ($this->db->record_count() === 0) {
-            error("Could not find interviewer");
-        }
-        $user = $this->db->next_record();
-        
-        $interviewer_id = $user['ID'];
         $this->db->query("
 INSERT INTO invites (InviterID, InviteKey, Email, Expires, Reason)
 VALUES ('{$interviewer_id}', '{$key}', '{$email}', '{$expires}', '{$reason}')");
-        return array("key" => $key);
+        $site_url = "http";
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "") {
+            $site_url .= "s";
+        }
+        $site_url .= "://" . SITE_URL . "/register.php?invite={$key}";
+    
+        include(SERVER_ROOT.'/classes/templates.class.php');
+        $TPL = NEW TEMPLATE;
+        $TPL->open(SERVER_ROOT.'/templates/invite.tpl');
+    
+        $TPL->set('InviterName', $interviewer_name);
+        $TPL->set('InviteKey', $key);
+        $TPL->set('Email', $email);
+        $TPL->set('SITE_NAME', SITE_NAME);
+        $TPL->set('SITE_URL', SITE_URL);
+        $TPL->set('IRC_SERVER', BOT_SERVER);
+        $TPL->set('DISABLED_CHAN', BOT_DISABLED_CHAN);
+    
+        Misc::send_email($_REQUEST['email'], 'New account confirmation at '.SITE_NAME, $TPL->get(), 'noreply');
+        
+        return array("key" => $key, "invite_url" => $site_url);
     }
 }
