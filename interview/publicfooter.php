3a733ff3b821895035be37956db5bf5951396971
O <o@apollo.rip>
2017-07-27 03:52:45 +0300

    Upload new file

---
+++ interview/publicfooter.php
@@ -0,0 +1,7 @@
+	<div id="suggestions" style="text-align:center">
+		<a href="https://apollo.rip/">APOLLO</a> &mdash;
+		<a href="irc://irc.apollo.rip/help">IRC Chat</a>
+	</div>
+</div>
+</body>
+</html>
