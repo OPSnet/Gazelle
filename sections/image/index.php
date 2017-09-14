82cc0f8f265d1786958ca2722ff7a1d4f6ee095d
itismadness <itismadness@apollo.rip>
2017-09-14 14:06:53 -0400

    add back in timeout

--- sections/image/index.php
+++ sections/image/index.php
@@ -26,6 +26,7 @@ if (!isset($Data) || !$Data) {
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.1 Safari/537.11');
 	$Data = curl_exec($ch);
 	$rescode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
