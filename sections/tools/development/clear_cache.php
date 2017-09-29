a97ee15d1de0ae92df7478620dc723dfd0364863
itismadness <itismadness@apollo.rip>
2017-09-29 12:19:49 -0400

    add quick cache clear for torrent groups

--- sections/tools/development/clear_cache.php
+++ sections/tools/development/clear_cache.php
@@ -18,6 +18,14 @@ if (isset($_GET['cache'])) {
 		}
 		echo "<div class='save_message'>{$Count} users' caches cleared!</div>";
 	}
+	elseif ($_GET['cache'] === 'torrent_groups') {
+		$DB->query("SELECT count(*) as count FROM torrents_group");
+		list($Count) = $DB->next_record();
+		for ($i = 1; $i <= $Count; $i++) {
+			$Cache->delete_value('torrent_group_' . $i);
+			$Cache->delete_value('groups_artists_' . $i);
+		}
+	}
 }
 if (!empty($_GET['key'])) {
 	if ($_GET['submit'] == 'Multi') {
@@ -73,8 +81,11 @@ $MultiKeyTooltip = 'Enter cache keys delimited by any amount of whitespace.';
 			</td>
 		</tr>
 		<tr>
-			<td>Clear Common Caches:</td>
-			<td><a href="tools.php?action=clear_cache&cache=users">Users</a></td>
+			<td rowspan="2">Clear Common Caches:</td>
+			<td><a href="tools.php?action=clear_cache&cache=users">Users</a> (clears out users_stats_*, users_info_*, and users_info_heavy_*)</td>
+		</tr>
+		<tr>
+			<td><a href="tools.php?action=clear_cache&cache=torrent_groups">Torrent Groups</a> (clears out torrent_group_* and groups_artists_*)</td>
 		</tr>
 	</table>
 <?
