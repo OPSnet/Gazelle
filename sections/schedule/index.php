e51bb73307a965e32d5053d5273515b19cf4c0aa
itismadness <itismadness@apollo.rip>
2017-10-05 13:41:15 -0400

    update scheduler code'

--- sections/schedule/index.php
+++ sections/schedule/index.php
@@ -22,6 +22,15 @@ if ($PCount > 3) {
 	die("schedule.php is already running. Exiting ($PCount)\n");
 }
 
+$LineEnd = check_perms('admin_schedule') ? "<br />" : "\n";
+$RunEvery = false;
+$RunHourly = false;
+$RunDaily = false;
+$RunWeekly = false;
+$RunBiweekly = false;
+$RunManual = false;
+$RunTasks = null;
+
 /**
  * Given a directory name for one of the sections in schedule/, load all of the PHP files
  * and execute them. We use extract($GLOBALS) to give them access to variables in
@@ -31,36 +40,48 @@ if ($PCount > 3) {
  * @param string $Dir which dir to load all files from to run
  */
 function run_tasks($Dir) {
+	global $RunTasks, $LineEnd;
 	$Tasks = array_diff(scandir(SERVER_ROOT.'/sections/schedule/'.$Dir, 1), array('.', '..'));
 	extract($GLOBALS);
 	foreach ($Tasks as $Task) {
-		print('Running '.str_replace('.php', '', $Task).'...');
+		$TimeStart = microtime(true);
+		$Task = str_replace('.php', '', $Task);
+		if (!empty($RunTasks) && !in_array($Task, $RunTasks)) {
+			continue;
+		}
+		print("Running {$Task}...");
 		/** @noinspection PhpIncludeInspection */
-		require_once SERVER_ROOT."/sections/schedule/{$Dir}/{$Task}";
-		print("DONE!\n");
+		require_once SERVER_ROOT."/sections/schedule/{$Dir}/{$Task}.php";
+		print("DONE! (".number_format(microtime(true) - $TimeStart, 3).")".$LineEnd);
 	}
 }
-
-$run_hourly = false;
-$run_daily = false;
-$run_weekly = false;
-$run_hourly = false;
-$run_biweekly = false;
-$run_manual = false;
 
 if (PHP_SAPI === 'cli') {
 	if (!isset($argv[1]) || $argv[1] != SCHEDULE_KEY) {
 		error(403);
 	}
 	for ($i = 2; $i < count($argv); $i++) {
-		if (substr($argv[$i], 0, 4) === 'run_') {
-			${$argv[$i]} = true;
+		if ($argv[$i] === 'run_tasks') {
+			if ($i < count($argv) - 1) {
+				$RunTasks = array();
+				for (++$i; $i < count($argv); $i++) {
+					$RunTasks[] = $argv[$i];
+				}
+				foreach (array('RunEvery', 'RunHourly', 'RunDaily', 'RunWeekly', 'RunBiweekly', 'RunManual') as $Var) {
+					$$Var = true;
+				}
+			}
+		}
+		elseif (substr($argv[$i], 0, 4) === 'run_') {
+			$Var = str_replace('_', '', ucwords($argv[$i], '_'));
+			${$Var} = true;
 		}
 	}
 }
 else {
 	foreach($_GET as $Key => $Value) {
 		if (substr($Key, 0, 4) === 'run_') {
+			$Key = str_replace('_', '', ucwords($argv[$i]));
 			$$Key = true;
 		}
 	}
@@ -83,13 +104,17 @@ $CurrentHour = date('H');
 $CurrentDay = date('d');
 $CurrentBiWeek = ($CurrentDay < 22 && $CurrentDay >= 8) ? 22 : 8;
 
-
-$DB->query("
+if (empty($RunTasks) && !$RunManual && !$RunEvery && !$RunHourly && !$RunDaily && !$RunWeekly && !$RunBiweekly) {
+	// We set this true here just so we run the tasks as we're (trying) to run all sections and
+	// not just an individual one (or some collection of tasks)
+	$RunEvery = true;
+	$DB->query("
 	UPDATE schedule
 	SET
 		NextHour = $CurrentHour,
 		NextDay = $CurrentDay,
 		NextBiWeekly = $CurrentBiWeek");
+}
 
 $sqltime = sqltime();
 
@@ -103,9 +128,11 @@ minutes).
 
 \*************************************************************************/
 
-echo "Running every run tasks...\n";
-run_tasks('every');
-echo "\n";
+if ($RunEvery) {
+	echo "Running every run tasks...\n";
+	run_tasks('every');
+	echo "\n";
+}
 
 /*************************************************************************\
 //--------------Run every hour ------------------------------------------//
@@ -114,7 +141,7 @@ These functions are run every hour.
 
 \*************************************************************************/
 
-if ($Hour != $CurrentHour || $run_hourly) {
+if ($Hour != $CurrentHour || $RunHourly) {
 	echo "Running hourly tasks...\n";
 	run_tasks('hourly');
 	echo "\n";
@@ -127,7 +154,7 @@ These functions are run in the first 15 minutes of every day.
 
 \*************************************************************************/
 
-if ($Day != $CurrentDay || $run_daily) {
+if ($Day != $CurrentDay || $RunDaily) {
 	echo "Running daily tasks...\n";
 	run_tasks('daily');
 	echo "\n";
@@ -140,7 +167,7 @@ These functions are run in the first 15 minutes of the week (Sunday).
 
 \*************************************************************************/
 
-if (($Day != $CurrentDay || $run_daily) && date('w') == 0) {
+if (($Day != $CurrentDay || $RunDaily) && date('w') == 0) {
 	echo "Running weekly tasks...\n";
 	run_tasks('weekly');
 	echo "\n";
@@ -153,7 +180,7 @@ These functions are twice per month, on the 8th and the 22nd.
 
 \*************************************************************************/
 
-if ($BiWeek != $CurrentBiWeek || $run_biweekly) {
+if ($BiWeek != $CurrentBiWeek || $RunBiweekly) {
 	echo "Running bi-weekly tasks...\n";
 	run_tasks('biweekly');
 	echo "\n";
@@ -165,7 +192,7 @@ if ($BiWeek != $CurrentBiWeek || $run_biweekly) {
 These functions are only run when manual is specified via GET 'runmanual'
 
 \*************************************************************************/
-if ($run_manual) {
+if ($RunManual) {
 	echo "Running manual tasks...\n";
 	run_tasks('manually');
 }
