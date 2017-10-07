3d9bfd9211352bbaee1fd652356b69efe5994c44
itismadness <itismadness@apollo.rip>
2017-10-07 13:38:33 -0400

    fix for if seeding same file on multiple computers/clients

--- sections/schedule/hourly/update_seed_times.php
+++ sections/schedule/hourly/update_seed_times.php
@@ -1,9 +1,8 @@
-<?
+<?php
 //------------- Update seed times ---------------------------------------//
 
 $DB->query("
   UPDATE xbt_snatched AS xs
-  INNER JOIN xbt_files_users AS xfu
+  INNER JOIN (SELECT uid, fid, MAX(active) AS active FROM xbt_files_users GROUP BY uid,fid) AS xfu
     ON xs.uid = xfu.uid AND xs.fid = xfu.fid
   SET xs.seedtime = xs.seedtime + xfu.active");
-?>
