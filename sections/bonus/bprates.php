3d55cb1d8b212d04fcdd6f2ad8c365db0dc001ee
itismadness <itismadness@apollo.rip>
2017-11-12 02:38:59 +0630

    fix bprates showing nothing

--- sections/bonus/bprates.php
+++ sections/bonus/bprates.php
@@ -29,8 +29,8 @@ SELECT
 		)
 	)) AS TotalHourlyPoints
 FROM
-	(SELECT * FROM xbt_files_users WHERE active='1' AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR) GROUP BY uid,fid) AS xfu
-	JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.uid
+	(SELECT DISTINCT uid,fid FROM xbt_files_users WHERE active=1 AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR)) AS xfu
+	JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.fid
 	JOIN torrents AS t ON t.ID = xfu.fid
 WHERE
 	xfu.uid = {$UserID}");
@@ -50,8 +50,8 @@ $DB->query("
 SELECT
 	COUNT(*) as count
 FROM
-	(SELECT * FROM xbt_files_users WHERE active='1' AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR) GROUP BY uid,fid) AS xfu
-	JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.uid
+	(SELECT DISTINCT uid,fid FROM xbt_files_users WHERE active=1 AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR)) AS xfu
+	JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.fid
 WHERE
 	xfu.uid = {$UserID}");
 
@@ -128,8 +128,8 @@ if ($NumResults > 0) {
 			)
 		) AS HourlyPoints
 	FROM
-		(SELECT * FROM xbt_files_users WHERE active='1' AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR) GROUP BY uid,fid) AS xfu
-		JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.uid
+		(SELECT DISTINCT uid,fid FROM xbt_files_users WHERE active=1 AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR)) AS xfu
+		JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.fid
 		JOIN torrents AS t ON t.ID = xfu.fid
 	WHERE
 		xfu.uid = {$UserID}
--- sections/schedule/hourly/update_user_bonus_points.php
+++ sections/schedule/hourly/update_user_bonus_points.php
@@ -18,8 +18,8 @@ LEFT JOIN (
 			)
 		)) AS NewPoints
 	FROM
-		(SELECT * FROM xbt_files_users WHERE active='1' AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR) GROUP BY uid,fid) AS xfu
-		JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.uid
+		(SELECT DISTINCT uid,fid FROM xbt_files_users WHERE active='1' AND remaining=0 AND mtime > unix_timestamp(NOW() - INTERVAL 1 HOUR)) AS xfu
+		JOIN xbt_files_history AS xfh ON xfh.uid = xfu.uid AND xfh.fid = xfu.fid
 		JOIN users_main AS um ON um.ID = xfu.uid
 		JOIN users_info AS ui ON ui.UserID = xfu.uid
 		JOIN torrents AS t ON t.ID = xfu.fid
