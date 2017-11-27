da9465105ceae22b7fd24fb7ce26e9848b60144a
itismadness <itismadness@apollo.rip>
2017-11-28 02:58:09 +0630

    fix bad if statement

--- sections/bonus/store.php
+++ sections/bonus/store.php
@@ -35,16 +35,17 @@ HTML;
 $Cnt = 1;
 foreach (Bonus::$Items as $Key => $Item) {
 	$RowClass = ($Cnt % 2 === 0) ? 'rowb' : 'rowa';
-	$Price = number_format(Bonus::get_price($Item));
+	$Price = Bonus::get_price($Item);
+	$FormattedPrice = number_format($Price);
 	print <<<HTML
 			<tr class="$RowClass">
 				<td>{$Cnt}</td>
 				<td>{$Item['Title']}</td>
-				<td>{$Price}</td>
+				<td>{$FormattedPrice}</td>
 				<td>
 HTML;
 
-	if (G::$LoggedUser['BonusPoints'] >= $Item['Price']) {
+	if (G::$LoggedUser['BonusPoints'] >= $Price) {
 		$Url = array();
 		foreach ($Item['Options'] as $KKey => $Value) {
 			$Url[] = "{$KKey}={$Value}";
