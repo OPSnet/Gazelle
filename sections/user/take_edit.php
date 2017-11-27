dbe2078ec874e6cf9a89c25cc1539b43df6fd3c6
itismadness <itismadness@apollo.rip>
2017-11-27 12:43:50 +1100

    add stricter checking

--- sections/user/take_edit.php
+++ sections/user/take_edit.php
@@ -186,7 +186,7 @@ if (!$Err && !empty($_POST['cur_pass']) && !empty($_POST['new_pass_1']) && !empt
 	if (Users::check_password($_POST['cur_pass'], $PassHash)) {
 		if ($_POST['cur_pass'] == $_POST['new_pass_1']) {
 			$Err = 'Your new password cannot be the same as your old password.';
-		} else if ($_POST['new_pass_1'] != $_POST['new_pass_2']) {
+		} else if ($_POST['new_pass_1'] !== $_POST['new_pass_2']) {
 			$Err = 'You did not enter the same password twice.';
 		}
 		else {
