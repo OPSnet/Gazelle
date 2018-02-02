3b06924cb8fedc1a284134a0df4f94108544027a
itismadness <itismadness@apollo.rip>
2018-02-01 22:29:04 -0200

    Add documentation for VALIDATE class

--- classes/validate.class.php
+++ classes/validate.class.php
@@ -5,9 +5,70 @@ Complete the number and date validation
 Finish the GenerateJS stuff
 //-----------------------------------*/
 
+/**
+ * Validate class
+ *
+ * This class is used to validate the contents of an associated array (most likely $_POST).
+ *
+ * Basic usage of the class would be:
+ * $Validate = new VALIDATE();
+ * $Validate->SetFields('username', true, 'string', 'You need to set a username');
+ * $Validate->SetFields('password', true, 'string', 'You need to set a password');
+ *
+ * $Err = $Validate->ValidateForm($_POST);
+ *
+ * Where $Err should be NULL if the form was successfully validated, else it'll be a string
+ * which indicates the first found error when validating the fields of the form.
+ *
+ * You can also use this class to create some validation JS to be put on a page (given a form ID), however, it's
+ * not really clear how well this feature is actually used throughout the codebase.
+ *
+ * TODO: investigate how much we can replace in this class with usage of filter_var and its validation flags,
+ * such as email and link for sure.
+ */
 class VALIDATE {
 	var $Fields = array();
 
+	/**
+	 * Add a new field to be validated (or used for JS form generation) from the associated array to be validated.
+	 * For each field, you need to give it a name (its key in the array), whether the field is required or not (fields
+	 * that are not required and blank and not a date won't be checked, else it'll always be validated), the type of
+	 * field (see below), the error message to show users if validation fails, and any options (while you can set
+	 * all options, certain ones will only affect certain types).
+	 *
+	 * Listed here are all the allowed field types, as well as then the options that are used for that particular type.
+	 * See below for how exactly each type is checked.
+	 * - string
+	 *      - MaxLength (if not set, defaults to 255)
+	 *      - MinLength (if not set, defaults to 1)
+	 * - number
+	 *      - MaxLength (if not set, defaults to no upper bound)
+	 *      - MinLength (if not set, defaults to 0
+	 *      - AllowPeriod (allow a period in the number)
+	 *      - AllowComma (allow a comma in the number)
+	 * - email
+	 *      - MaxLength (if not set, defaults to 255)
+	 *      - MinLength (if not set, defaults to 6)
+	 * - link
+	 *      - MaxLength (if not set, defaults to 255)
+	 *      - MinLength (if not set, defaults to 10)
+	 * - username
+	 *      - MaxLength (if not set, defaults to 20)
+	 *      - MinLength (if not set, defaults to 1)
+	 * - checkbox
+	 * - compare
+	 *      - CompareField (required), what other field should this one be compared to in validation array
+	 * - inarray
+	 *      - InArray (required), what value to check for within the value of the field/key in the validation array
+	 * - regex
+	 *      - Regex (required), regular expression string to use within preg_match
+	 *
+	 * @param string $FieldName
+	 * @param bool   $Required
+	 * @param string $FieldType
+	 * @param string $ErrorMessage
+	 * @param array  $Options
+	 */
 	function SetFields($FieldName, $Required, $FieldType, $ErrorMessage, $Options = array()) {
 		$this->Fields[$FieldName]['Type'] = strtolower($FieldType);
 		$this->Fields[$FieldName]['Required'] = $Required;
@@ -35,6 +96,35 @@ class VALIDATE {
 		}
 	}
 
+	/**
+	 * Given an associate array, iterate through each key checking to see if we've set the field to be validated. If
+	 * the field is not blank or it's required or it's a date, then we must validate, else we can skip this field.
+	 *
+	 * Note: Regular expression constants can be found in classes/regex.php
+	 * Note: All checks against length (value for number type) is inclusive of the Min/Max lengths
+	 *
+	 * Field types and how we validate them (see above for options for each field type):
+	 * - string: make sure the string's length is within the set MinLength and MaxLength
+	 * - number: perform regular expression for digits + periods (if set) + commas (if set), and check that the numeric
+	 *      falls within MinLength and MaxLength (using weak type coercion as necessary). This field cannot be left
+	 *      empty.
+	 * - email: Checks to make sure the length of the email falls within MinLength and MaxLength and performs a
+	 *      preg_match using the EMAIL_REGEX constant against the field to check that it passes
+	 * - link: Makes sure the length of the link falls between MinLength and MaxLength and performs a preg_match
+	 *      using the URL_REGEX constant against the field
+	 * - username: checks that the length of the username falls within MinLength and MaxLength and performs a preg_match
+	 *      using the USERNAME_REGEX constant against the field
+	 * - checkbox: just checks if the field exists within the associate array, doesn't matter the value
+	 * - compare: compares the field against field specified in the CompareField option. Useful for stuff like password
+	 *      where you have to input it twice, check that the second password equals the first one
+	 * - inarray: checks that the value specified in InArray option is in the field (which we assume is an array)
+	 * - regex: performs a preg_match of the value of Regex option and the field
+	 *
+	 * TODO: date fields are not actually validated, need to figure out what the proper validation syntax should be.
+	 *
+	 * @param  array $ValidateArray
+	 * @return string|null
+	 */
 	function ValidateForm($ValidateArray) {
 		reset($this->Fields);
 		foreach ($this->Fields as $FieldKey => $Field) {
