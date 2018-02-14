6c133def6cf9b00efd6645b44121d93662a9fbab
itismadness <itismadness@apollo.rip>
2018-02-15 04:57:30 +0930

    Switch to whichbrowser/parser to get browser/os as well as version

--- sections/tools/data/platform_usage.php
+++ sections/tools/data/platform_usage.php
@@ -7,30 +7,27 @@ if (!check_perms('site_view_flow')) {
 View::show_header('OS and Browser Usage');
 
 ?>
-	<div class="header">
-		<h2>OS Usage</h2>
-	</div>
-	<table width="100%">
-		<tr class="colhead">
-			<td>OS</td>
-			<td>Count</td>
-		</tr>
+<div class="header">
+	<h2>OS Usage</h2>
+</div>
+<table width="100%">
+	<tr class="colhead">
+		<td>OS</td>
+		<td>Count</td>
+	</tr>
 
-		<?php
-		G::$DB->prepared_query("SELECT OperatingSystem, COUNT(*) FROM users_sessions GROUP BY OperatingSystem ORDER BY COUNT(*) DESC");
-		while (list($OperatingSystem, $Count) = G::$DB->fetch_record(0, 'OperatingSystem')) {
-			?>
-			<tr>
-				<td><?=$OperatingSystem?></td>
-				<td><?=$Count?></td>
-			</tr>
-			<?php
-		}
-		?>
-	</table>
 <?php
-
+G::$DB->prepared_query("SELECT OperatingSystem, OperatingSystemVersion, COUNT(*) FROM users_sessions GROUP BY OperatingSystem, OperatingSystemVersion ORDER BY COUNT(*) DESC");
+while (list($OperatingSystem, $OperatingSystemVersion, $Count) = G::$DB->fetch_record(0, 'OperatingSystem', 1, 'OperatingSystemVersion')) {
+	?>
+	<tr>
+		<td><?=$OperatingSystem?> <?=$OperatingSystemVersion?></td>
+		<td><?=$Count?></td>
+	</tr>
+	<?php
+}
 ?>
+</table>
 <div class="header">
 	<h2>Browser Usage</h2>
 </div>
@@ -41,11 +38,11 @@ View::show_header('OS and Browser Usage');
 	</tr>
 
 <?php
-G::$DB->prepared_query("SELECT Browser, COUNT(*) FROM users_sessions GROUP BY Browser ORDER BY COUNT(*) DESC");
-while (list($Browser, $Count) = G::$DB->fetch_record(0, 'Browser')) {
+G::$DB->prepared_query("SELECT Browser, BrowserVersion, COUNT(*) FROM users_sessions GROUP BY Browser, BrowserVersion ORDER BY COUNT(*) DESC");
+while (list($Browser, $BrowserVersion, $Count) = G::$DB->fetch_record(0, 'Browser', 1, 'BrowserVersion')) {
 	?>
 	<tr>
-		<td><?=$Browser?></td>
+		<td><?=$Browser?> <?=$BrowserVersion?></td>
 		<td><?=$Count?></td>
 	</tr>
 	<?php
