41c5cca2417805b432f18cf86ab234dc2c948419
itismadness <itismadness@apollo.rip>
2018-07-26 23:42:34 +0545

    Print out importance information

--- sections/tools/sandboxes/artist_importance_sandbox.php
+++ sections/tools/sandboxes/artist_importance_sandbox.php
@@ -5,7 +5,7 @@ if (!check_perms('users_mod')) {
 }
 $Title = "Artist Importance Sandbox";
 View::show_header($Title, 'bbcode_sandbox');
-$DB->prepared_query("SELECT aa.Name as ArtistName, tg.Name as GroupName, ta.GroupID, ta.ArtistID FROM torrents_artists AS ta LEFT JOIN torrents_group AS tg ON tg.ID = ta.GroupID LEFT JOIN artists_alias AS aa ON aa.ArtistID = ta.ArtistID WHERE ta.ArtistID=? ORDER BY ta.ArtistID, ta.GroupID", 353765);
+$DB->prepared_query("SELECT aa.Name as ArtistName, tg.Name as GroupName, ta.Importance, ta.GroupID, ta.ArtistID FROM torrents_artists AS ta LEFT JOIN torrents_group AS tg ON tg.ID = ta.GroupID LEFT JOIN artists_alias AS aa ON aa.ArtistID = ta.ArtistID WHERE ta.ArtistID=? ORDER BY ta.ArtistID, ta.GroupID", 353765);
 
 ?>
 	<div class="header">
@@ -16,16 +16,18 @@ $DB->prepared_query("SELECT aa.Name as ArtistName, tg.Name as GroupName, ta.Grou
 			<tr>
 				<th>Artist Name</th>
 				<td>Group Name</td>
+				<td>Importance</td>
 				<td>Artist Link</td>
 				<td>Group Link</td>
 			</tr>
 <?php
 
-		while (list($ArtistName, $GroupName, $GroupID, $ArtistID) = $DB->fetch_record()) {
+		while (list($ArtistName, $GroupName, $Importance, $GroupID, $ArtistID) = $DB->fetch_record()) {
 ?>
 			<tr>
 				<td><?=$ArtistName?></td>
 				<td><?=$GroupName?></td>
+				<td><?=($Importance === NULL) ? "NULL" : $Importance?></td>
 				<td>https://apollo.rip/torrents.php?id=<?=$GroupID?></td>
 				<td>https://apollo.rip/artist.php?id=<?=$ArtistID?></td>
 			</tr>
