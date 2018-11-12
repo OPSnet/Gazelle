8e4f09a1550e5ade138f76f37e63084c710b5c97
itismadness <itismadness@orpheus.network>
2018-11-13 12:10:02 +1300

    Remove interview from main gazelle repo

--- interview/publicfooter.php
+++
@@ -1,7 +0,0 @@
-	<div id="suggestions" style="text-align:center">
-		<a href="https://orpheus.network/">Orpheus</a> &mdash;
-		<a href="irc://irc.orpheus.network/help">IRC Chat</a>
-	</div>
-</div>
-</body>
-</html>
