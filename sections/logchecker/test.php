bd5dfeb35760d7407528e9392ec23650b1e391e9
Musaeus Eleusinius <musaeus@orpheus.network>
2018-12-01 22:32:08 +0000

    Clarification in logchecker on pasted logs have no checksum verification

--- sections/logchecker/test.php
+++ sections/logchecker/test.php
@@ -30,7 +30,7 @@ echo <<<HTML
 		<p>
 		Use this page to test our logchecker. You can either upload a log or paste it into the
 		text box below. This will then run the file/text against our logchecker displaying to you
-		what it would look like on our site.
+		what it would look like on our site. To verify checksum, you need to upload log file.
 		</p>
 		<table class="forum_post vertical_margin">
 			<tr class="colhead">
@@ -48,7 +48,7 @@ echo <<<HTML
 		</table>
 		<table class="forum_post vertical_margin">
 			<tr class="colhead">
-				<td colspan="2">Paste log</td>
+				<td colspan="2">Paste log (No checksum verification)</td>
 			</tr>
 			<tr>
 				<td>
