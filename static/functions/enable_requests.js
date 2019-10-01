e7008e2dd83961e618fcbbdda4c8559281044643
nobody <nobody@orpheus.network>
2019-10-01 18:25:53 +0000

    Revert "remove pointless "Are you sure?" (#30)"
    This reverts commit eb452380c533f54c7db9d5bc0f8c9ccb827ec542.
    
    The submit was handled in ajax, no PHP fallback, fuckers.

--- static/functions/enable_requests.js
+++ static/functions/enable_requests.js
@@ -29,6 +29,9 @@
             }
         });
         $("input[id^=outcome]").click(function() {
+            if ($(this).val() != 'Discard' && !confirm('Are you sure you wish to do this? This cannot be undone!')) {
+                return false;
+            }
             var id = $(this).data('id');
             if (id !== undefined) {
                 // Only resolving one row
