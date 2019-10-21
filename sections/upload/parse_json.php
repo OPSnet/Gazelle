afe57f174136d4e714ad06cec511b1c4d34dc951
itismadness <itismadness@orpheus.network>
2019-10-21 20:43:49 -0300

    Fix parsing boolean fields in JSON file upload

--- sections/upload/parse_json.php
+++ sections/upload/parse_json.php
@@ -7,31 +7,52 @@ if (!$_POST['data'] || empty($_POST['data'])) {
 
 $Data = $_POST['data'];
 
-$GroupFields = [
+$Fields = [
     'name',
     'recordLabel'
 ];
-
-foreach ($GroupFields as $GroupField) {
-    $Data['response']['group'][$GroupField] = reverse_display_str($Data['response']['group'][$GroupField]);
+foreach ($Fields as $Field) {
+    if (isset($Data['response']['group'][$Field])) {
+        $Data['response']['group'][$Field] = reverse_display_str($Data['response']['group'][$Field]);
+    }
 }
 
-$Tags = count($Data['response']['group']['tags']);
-for ($i = 0; $i < $Tags; $i++) {
-    $Data['response']['group']['tags'][$i] = reverse_display_str($Data['response']['group']['tags'][$i]);
+if (isset($Data['response']['group']['tags'])) {
+    $Tags = count($Data['response']['group']['tags']);
+    for ($i = 0; $i < $Tags; $i++) {
+        $Data['response']['group']['tags'][$i] = reverse_display_str($Data['response']['group']['tags'][$i]);
+    }
 }
+
 
 // Artist Name is not escaped for whatever reason, so do not have to try and reverse it
+if (isset($Data['response']['group']['wikiBody'])) {
+    $Data['response']['group']['wikiBody'] = Text::parse_html($Data['response']['group']['wikiBody']);
+}
 
-$Data['response']['group']['wikiBody'] = Text::parse_html($Data['response']['group']['wikiBody']);
-
-$TorrentFields = [
+$Fields = [
     'remasterRecordLabel',
     'description'
 ];
+foreach ($Fields as $Field) {
+    if (isset($Data['response']['torrent'][$Field])) {
+        $Data['response']['torrent'][$Field] = reverse_display_str($Data['response']['torrent'][$Field]);
+    }
+}
 
-foreach ($TorrentFields as $TorrentField) {
-    $Data['response']['torrent'][$TorrentField] = reverse_display_str($Data['response']['torrent'][$TorrentField]);
+// Convert these fields back to booleans
+$Fields = [
+    'remastered',
+    'scene',
+    'hasLog',
+    'hasCue',
+    'freeTorrent',
+    'reported'
+];
+foreach ($Fields as $Field) {
+    if (isset($Data['response']['torrent'][$Field])) {
+        $Data['response']['torrent'][$Field] = strtolower($Data['response']['torrent'][$Field]) === "true";
+    }
 }
 
 header('Content-type: application/json');
