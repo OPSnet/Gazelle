5f3adfa8701a8920154070bdfbc61ae492894819
Spine <spine@orpheus.network>
2020-02-12 22:36:29 +0100

    revamp torrents stats admin page

--- sections/tools/data/torrent_stats.php
+++ sections/tools/data/torrent_stats.php
@@ -5,69 +5,113 @@ if (!check_perms('site_view_flow')) {
 View::show_header('Torrents');
 
 if (!$TorrentStats = $Cache->get_value('new_torrent_stats')) {
-    $DB->query(" SELECT COUNT(*), SUM(Size), SUM(FileCount) FROM torrents");
-    list($TorrentCount, $TotalSize, $TotalFiles) = $DB->next_record();
     $NumUsers = Users::get_enabled_users_count();
-    $DB->query("SELECT COUNT(*), SUM(Size), SUM(FileCount) FROM torrents WHERE Time > now() - INTERVAL 1 DAY)");
-    list($DayNum, $DaySize, $DayFiles) = $DB->next_record();
-    $DB->query("SELECT COUNT(*), SUM(Size), SUM(FileCount) FROM torrents WHERE Time > now() - INTERVAL 1 WEEK)");
-    list($WeekNum, $WeekSize, $WeekFiles) = $DB->next_record();
-    $DB->query("SELECT COUNT(*), SUM(Size), SUM(FileCount) FROM torrents WHERE Time > now() - INTERVAL 1 MONTH)");
-    list($MonthNum, $MonthSize, $MonthFiles) = $DB->next_record();
-    $Cache->cache_value('new_torrent_stats', [$TorrentCount, $TotalSize, $TotalFiles,
-                        $NumUsers, $DayNum, $DaySize, $DayFiles,
-                        $WeekNum, $WeekSize, $WeekFiles, $MonthNum,
-                        $MonthSize, $MonthFiles], 3600);
-} else {
-    list($TorrentCount, $TotalSize, $TotalFiles, $NumUsers, $DayNum, $DaySize, $DayFiles,
-        $WeekNum, $WeekSize, $WeekFiles, $MonthNum, $MonthSize, $MonthFiles) = $TorrentStats;
-}
+    $DB->query("SELECT count(*), coalesce(sum(Size), 0), coalesce(sum(FileCount), 0) FROM torrents");
+    list($TorrentCount, $TotalSize, $TotalFiles) = $DB->next_record();
 
-?>
-<div class="thin">
-    <div class="box">
-        <div class="head">Overall stats</div>
-        <div class="pad">
-            <ul class="stats nobullet">
-                <li><strong>Total torrents: </strong><?=number_format($TorrentCount)?></li>
-                <li><strong>Total size: </strong><?=Format::get_size($TotalSize)?></li>
-                <li><strong>Total files: </strong><?=number_format($TotalFiles)?></li>
-                <br />
-                <li><strong>Mean torrents per user: </strong><?=number_format($TorrentCount / $NumUsers)?></li>
-                <li><strong>Mean torrent size: </strong><?=Format::get_size($TotalSize / $TorrentCount)?></li>
-                <li><strong>Mean files per torrent: </strong><?=number_format($TotalFiles / $TorrentCount)?></li>
-                <li><strong>Mean filesize: </strong><?=Format::get_size($TotalSize / $TotalFiles)?></li>
-            </ul>
+    $DB->query("SELECT count(*), coalesce(sum(Size), 0), coalesce(sum(FileCount), 0) FROM torrents WHERE Time > now() - INTERVAL 1 DAY");
+    list($DayNum, $DaySize, $DayFiles) = $DB->next_record();
+
+    $DB->query("SELECT count(*), coalesce(sum(Size), 0), coalesce(sum(FileCount), 0) FROM torrents WHERE Time > now() - INTERVAL 1 WEEK");
+    list($WeekNum, $WeekSize, $WeekFiles) = $DB->next_record();
+
+    $DB->query("SELECT count(*), coalesce(sum(Size), 0), coalesce(sum(FileCount), 0) FROM torrents WHERE Time > now() - INTERVAL 30 DAY");
+    list($MonthNum, $MonthSize, $MonthFiles) = $DB->next_record();
+
+    $DB->query("SELECT count(*), coalesce(sum(Size), 0), coalesce(sum(FileCount), 0) FROM torrents WHERE Time > now() - INTERVAL 120 DAY");
+    list($QuartNum, $QuartSize, $QuartFiles) = $DB->next_record();
+
+    $Cache->cache_value('new_torrent_stats',
+        [
+            $TorrentCount, $TotalSize, $TotalFiles, $NumUsers,
+            $DayNum, $DaySize, $DayFiles,
+            $WeekNum, $WeekSize, $WeekFiles,
+            $MonthNum, $MonthSize, $MonthFiles,
+            $QuartNum, $QuartSize, $QuartFiles,
+        ], 3600);
+} else {
+        list(
+            $TorrentCount, $TotalSize, $TotalFiles, $NumUsers,
+            $DayNum, $DaySize, $DayFiles,
+            $WeekNum, $WeekSize, $WeekFiles,
+            $MonthNum, $MonthSize, $MonthFiles,
+            $QuartNum, $QuartSize, $QuartFiles,
+        ) = $TorrentStats;
+    }
+
+    ?>
+    <div class="thin">
+        <div class="box">
+            <div class="head">Overall stats</div>
+            <div class="pad">
+                <table>
+                <tr>
+                    <td>Total torrents:</td><td><?=number_format($TorrentCount)?></td>
+                    <td>Mean torrents per user:</td><td><?=number_format($TorrentCount / $NumUsers)?></td>
+                    <td>Mean files per torrent:</td><td><?=number_format($TotalFiles / $TorrentCount)?></td>
+                </tr>
+                <tr>
+                    <td>Total size:</td><td><?=Format::get_size($TotalSize)?></td>
+                    <td>Mean torrent size:</td><td><?=Format::get_size($TotalSize / $TorrentCount)?></td>
+                    <td>Mean filesize:</td><td><?=Format::get_size($TotalSize / $TotalFiles)?></td>
+                </tr>
+                <tr>
+                    <td>Total files:</td><td><?=number_format($TotalFiles)?></td>
+                </tr>
+                </table>
+            </div>
         </div>
-    </div>
-    <br />
-    <div class="box">
-        <div class="head">Upload frequency</div>
-        <div class="pad">
-            <ul class="stats nobullet">
-                <li><strong>Torrents today: </strong><?=number_format($DayNum)?></li>
-                <li><strong>Size today: </strong><?=Format::get_size($DaySize)?></li>
-                <li><strong>Files today: </strong><?=number_format($DayFiles)?></li>
-                <br />
-                <li><strong>Torrents this week: </strong><?=number_format($WeekNum)?></li>
-                <li><strong>Size this week: </strong><?=Format::get_size($WeekSize)?></li>
-                <li><strong>Files this week: </strong><?=number_format($WeekFiles)?></li>
-                <br />
-                <li><strong>Torrents per day this week: </strong><?=number_format($WeekNum / 7)?></li>
-                <li><strong>Size per day this week: </strong><?=Format::get_size($WeekSize / 7)?></li>
-                <li><strong>Files per day this week: </strong><?=number_format($WeekFiles / 7)?></li>
-                <br />
-                <li><strong>Torrents this month: </strong><?=number_format($MonthNum)?></li>
-                <li><strong>Size this month: </strong><?=Format::get_size($MonthSize)?></li>
-                <li><strong>Files this month: </strong><?=number_format($MonthFiles)?></li>
-                <br />
-                <li><strong>Torrents per day this month: </strong><?=number_format($MonthNum / 30)?></li>
-                <li><strong>Size per day this month: </strong><?=Format::get_size($MonthSize / 30)?></li>
-                <li><strong>Files per day this month: </strong><?=number_format($MonthFiles / 30)?></li>
-            </ul>
+        <br />
+        <div class="box">
+            <div class="head">Upload frequency</div>
+            <div class="pad">
+                <table>
+                    <tr>
+                    <th></th>
+                    <th>Today</th>
+                    <th>This week</th>
+                    <th>Per day this week</th>
+                    <th>This month</th>
+                    <th>Per day this month</th>
+                    <th>This quarter</th>
+                    <th>Per day this quarter</th>
+                    </tr>
+
+                    <tr>
+                    <th>Torrents</th>
+                    <td align="right"><?=number_format($DayNum)?></td>
+                    <td align="right"><?=number_format($WeekNum)?></td>
+                    <td align="right"><?=number_format($WeekNum / 7)?></td>
+                    <td align="right"><?=number_format($MonthNum)?></td>
+                    <td align="right"><?=number_format($MonthNum / 30)?></td>
+                    <td align="right"><?=number_format($QuartNum)?></td>
+                    <td align="right"><?=number_format($QuartNum / 120)?></td>
+                    </tr>
+
+                    <tr>
+                    <th>Size</th>
+                    <td align="right"><?=number_format($DaySize)?></td>
+                    <td align="right"><?=number_format($WeekSize)?></td>
+                    <td align="right"><?=number_format($WeekSize / 7)?></td>
+                    <td align="right"><?=number_format($MonthSize)?></td>
+                    <td align="right"><?=number_format($MonthSize / 30)?></td>
+                    <td align="right"><?=number_format($QuartSize)?></td>
+                    <td align="right"><?=number_format($QuartSize / 120)?></td>
+                    </tr>
+
+                    <tr>
+                    <th>Files</th>
+                    <td align="right"><?=number_format($DayFiles)?></td>
+                    <td align="right"><?=number_format($WeekFiles)?></td>
+                    <td align="right"><?=number_format($WeekFiles / 7)?></td>
+                    <td align="right"><?=number_format($MonthFiles)?></td>
+                    <td align="right"><?=number_format($MonthFiles / 30)?></td>
+                    <td align="right"><?=number_format($QuartFiles)?></td>
+                    <td align="right"><?=number_format($QuartFiles / 120)?></td>
+                    </tr>
+                </table>
         </div>
     </div>
 </div>
 <?php
 View::show_footer();
-?>
