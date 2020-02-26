32550cc7e887eadfea6f5117d877a5874ae93ef5
webhead <webhead@orpheus.network>
2020-02-25 18:11:40 -0600

    fixes min length of release description
    https://orpheus.network/forums.php?action=viewthread&threadid=5329

--- sections/upload/upload_handle.php
+++ sections/upload/upload_handle.php
@@ -179,7 +179,7 @@ switch ($Type) {
             '0','link','The image URL you entered was invalid.', ['maxlength'=>255, 'minlength'=>12]);
 
         $Validate->SetFields('release_desc',
-            '0','string','The release description has a minimum length of 10 characters.', ['maxlength'=>1000000, 'minlength'=>10]);
+            '0','string','The release description you entered is too long.', ['maxlength'=>1000000]);
 
         $Validate->SetFields('groupid', '0', 'number', 'Group ID was not numeric');
 
@@ -218,7 +218,7 @@ switch ($Type) {
             '1','string','You must enter at least one tag. Maximum length is 200 characters.', ['maxlength'=>200, 'minlength'=>2]);
 
         $Validate->SetFields('release_desc',
-            '0','string','The release description has a minimum length of 10 characters.', ['maxlength'=>1000000, 'minlength'=>10]);
+            '0','string','The release description you entered is too long.', ['maxlength'=>1000000]);
 
         $Validate->SetFields('image',
             '0','link','The image URL you entered was invalid.', ['maxlength'=>255, 'minlength'=>12]);
@@ -235,7 +235,7 @@ switch ($Type) {
             '1','string','You must enter at least one tag. Maximum length is 200 characters.', ['maxlength'=>200, 'minlength'=>2]);
 
         $Validate->SetFields('release_desc',
-            '0','string','The release description has a minimum length of 10 characters.', ['maxlength'=>1000000, 'minlength'=>10]);
+            '0','string','The release description you entered is too long.', ['maxlength'=>1000000]);
 
         $Validate->SetFields('image',
             '0','link','The image URL you entered was invalid.', ['maxlength'=>255, 'minlength'=>12]);
