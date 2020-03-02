6eaedd52b01f201a964f301692db03ece074f4f9
nobody <nobody@orpheus.network>
2020-03-02 08:41:26 +0000

    fix scheduler

--- schedule.php
+++ schedule.php
@@ -3,4 +3,4 @@ define('MEMORY_EXCEPTION', true);
 define('TIME_EXCEPTION', true);
 define('ERROR_EXCEPTION', true);
 $_SERVER['SCRIPT_FILENAME'] = 'schedule.php'; // CLI fix
-require_once(__DIR__.'classes/script_start.php');
+require_once(__DIR__.'/classes/script_start.php');
