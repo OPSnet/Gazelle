1d37d51be59e50a0e987acab4c8017f7391694be
nobody <nobody@orpheus.network>
2020-03-05 11:55:43 +0000

    fix column name

--- app/Schedule/Tasks/PromoteUsers.php
+++ app/Schedule/Tasks/PromoteUsers.php
@@ -26,7 +26,7 @@ class PromoteUsers extends \Gazelle\Schedule\Task
                         WHERE users_main.PermissionID = ?
                         AND ui.Warned = '0000-00-00 00:00:00'
                         AND uls.Uploaded + IFNULL(b.Bounty, 0) >= ?
-                        AND (uls.Download = 0 OR uls.Uploaded / uls.Downloaded >= ?)
+                        AND (uls.Downloaded = 0 OR uls.Uploaded / uls.Downloaded >= ?)
                         AND ui.JoinDate < now() - INTERVAL ? WEEK
                         AND (
                             SELECT count(ID)
