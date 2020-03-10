0a55c9ea8fd61d1e132d0cef325b0dab373bc6b7
Spine <spine@orpheus.network>
2020-03-10 22:49:16 +0000

    Apply suggestion to app/Schedule/Tasks/UpdateUserBonusPoints.php

--- app/Schedule/Tasks/UpdateUserBonusPoints.php
+++ app/Schedule/Tasks/UpdateUserBonusPoints.php
@@ -6,8 +6,8 @@ class UpdateUserBonusPoints extends \Gazelle\Schedule\Task
 {
     public function run()
     {
-        $Bonus = new \Gazelle\Bonus($DB, $Cache);
-        $Bonus->givePoints();
+        $bonus = new \Gazelle\Bonus($this->db, $this->cache);
+        $bonus->givePoints();
         $this->db->prepared_query("SELECT UserID FROM users_info WHERE DisablePoints = '0'");
         if ($this->db->has_results()) {
             while(list($userID) = $this->db->next_record()) {
