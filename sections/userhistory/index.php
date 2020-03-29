4d0bf7abb1ae758bdefda2c5dfedc763938e93c9
Spine <spine@orpheus.network>
2020-03-29 18:09:04 +0200

    search by user or ipaddr on tracker history

--- sections/userhistory/index.php
+++ sections/userhistory/index.php
@@ -2,7 +2,6 @@
 /*****************************************************************
 User history switch center
 
-
 This page acts as a switch that includes the real user history pages (to keep
 the root less cluttered).
 
@@ -16,112 +15,60 @@ enforce_login();
 if ($_GET['action']) {
     switch ($_GET['action']) {
         case 'ips':
-            //Load IP history page
-            include('ip_history.php');
+            require_once(__DIR__ . '/ip_history.php');
             break;
         case 'tracker_ips':
-            include('ip_tracker_history.php');
+            require_once(__DIR__ . '/ip_tracker_history.php');
             break;
         case 'passwords':
-            //Load Password history page
-            include('password_history.php');
+            require_once(__DIR__ . '/password_history.php');
             break;
         case 'email':
-            //Load email history page
-            include('email_history.php');
+            require_once(__DIR__ . '/email_history.php');
             break;
         case 'email2':
-            //Load email history page
-            include('email_history2.php');
+            require_once(__DIR__ . '/email_history2.php');
             break;
         case 'copypaste':
-            //Load copy-paste page
-            include('copypaste.php');
+            require_once(__DIR__ . '/copypaste.php');
             break;
         case 'passkeys':
-            //Load passkey history page
-            include('passkey_history.php');
+            require_once(__DIR__ . '/passkey_history.php');
             break;
         case 'posts':
-            //Load post history page
-            include('post_history.php');
+            require_once(__DIR__ . '/post_history.php');
             break;
         case 'topics':
-            include('topic_history.php');
+            require_once(__DIR__ . '/topic_history.php');
             break;
         case 'subscriptions':
-            // View subscriptions
-            require('subscriptions.php');
+            require_once(__DIR__ . '/subscriptions.php');
             break;
         case 'thread_subscribe':
-            require('thread_subscribe.php');
+            require_once(__DIR__ . '/thread_subscribe.php');
             break;
         case 'comments_subscribe':
-            require('comments_subscribe.php');
+            require_once(__DIR__ . '/comments_subscribe.php');
             break;
         case 'catchup':
-            require('catchup.php');
+            require_once(__DIR__ . '/catchup.php');
             break;
         case 'collage_subscribe':
-            require('collage_subscribe.php');
+            require_once(__DIR__ . '/collage_subscribe.php');
             break;
         case 'subscribed_collages':
-            require('subscribed_collages.php');
+            require_once(__DIR__ . '/subscribed_collages.php');
             break;
         case 'catchup_collages':
-            require('catchup_collages.php');
+            require_once(__DIR__ . '/catchup_collages.php');
             break;
         case 'token_history':
-            require('token_history.php');
+            require_once(__DIR__ . '/token_history.php');
             break;
         case 'quote_notifications':
-            require('quote_notifications.php');
+            require_once(__DIR__ . '/quote_notifications.php');
             break;
         default:
-            //You trying to mess with me query string? To the home page with you!
             header('Location: index.php');
     }
 }
-
-/* Database Information Regarding This Page
-
-users_history_ips:
-    id (auto_increment, index)
-    userid (index)
-    ip (stored using ip2long())
-    timestamp
-
-users_history_passwd:
-    id (auto_increment, index)
-    userid (index)
-    changed_by (index)
-    old_pass
-    new_pass
-    timestamp
-
-users_history_email:
-    id (auto_increment, index)
-    userid (index)
-    changed_by (index)
-    old_email
-    new_email
-    timestamp
-
-users_history_passkey:
-    id (auto_increment, index)
-    userid (index)
-    changed_by (index)
-    old_passkey
-    new_passkey
-    timestamp
-
-users_history_stats:
-    id (auto_increment, index)
-    userid (index)
-    uploaded
-    downloaded
-    ratio
-    timestamp
-
-*/
-?>
--- sections/userhistory/ip_tracker_history.php
+++ sections/userhistory/ip_tracker_history.php
@@ -1,7 +1,5 @@
 <?php
 /************************************************************************
-||------------|| User IP history page ||---------------------------||
-
 This page lists previous IPs a user has connected to the site with. It
 gets called if $_GET['action'] == 'ips'.
 
@@ -10,48 +8,114 @@ user.
 
 ************************************************************************/
 
-define('IPS_PER_PAGE', 25);
+require_once(__DIR__ . '/../torrents/functions.php');
+
+define('IPS_PER_PAGE', 50);
 
 if (!check_perms('users_mod')) {
     error(403);
 }
 
-$UserID = $_GET['userid'];
-if (!is_number($UserID)) {
+$userId = $_GET['userid'] ?? null;
+$ipAddr     = $_GET['ip'] ?? null;
+
+if (!(is_number($userId) || preg_match('/^' . IP_REGEX . '$/', $ipAddr))) {
     error(404);
 }
 
-$DB->query("
+$DB->prepared_query('
     SELECT um.Username,
         p.Level AS Class
     FROM users_main AS um
-        LEFT JOIN permissions AS p ON p.ID = um.PermissionID
-    WHERE um.ID = $UserID");
+    LEFT JOIN permissions AS p ON (p.ID = um.PermissionID)
+    WHERE um.ID = ?
+    ', $userId
+);
 list($Username, $Class) = $DB->next_record();
 
 if (!check_perms('users_view_ips', $Class)) {
     error(403);
 }
-
-$UsersOnly = $_GET['usersonly'];
-
-View::show_header("Tracker IP address history for $Username");
+View::show_header('Tracker IP address history for ' . ($userId ? $Username : "IP address $ipAddr"));
 ?>
 <script type="text/javascript">
 function ShowIPs(rowname) {
     $('tr[name="'+rowname+'"]').gtoggle();
 }
 </script>
+<div class="thin">
+    <div class="header">
+<?php if ($userId) { ?>
+        <h2>Tracker IP address history for <a href="user.php?id=<?= $userId ?>"><?= $Username ?></a></h2>
+<?php } else { ?>
+        <h2>Tracker history for IP address <?= $ipAddr ?></h2>
+<?php } ?>
+    </div>
 <?php
-list($Page, $Limit) = Format::page_limit(IPS_PER_PAGE);
-
-$TrackerIps = $DB->query("
-    SELECT IP, fid, tstamp
+$searchCol = is_null($ipAddr) ? 'uid' : 'IP';
+$DB->prepared_query("
+    SELECT IP,
+        count(*) AS total,
+        from_unixtime(min(tstamp)) AS first,
+        from_unixtime(max(tstamp)) AS last
     FROM xbt_snatched
-    WHERE uid = $UserID
-        AND IP != ''
-    ORDER BY tstamp DESC
-    LIMIT $Limit");
+    WHERE $searchCol = ?
+    GROUP BY IP
+    ORDER BY max(tstamp) DESC
+    ", is_null($ipAddr) ? $userId : $ipAddr
+);
+$summary = $DB->to_array(MYSQLI_ASSOC);
+
+if ($summary) {
+    $urlStem = $_SERVER['SCRIPT_NAME'] . '?action=tracker_ips&amp;ip=';
+?>
+    <div class="header">
+        <h3>Summary</h3>
+    </div>
+    <table>
+        <tr class="colhead">
+<?php if (check_perms('users_mod')) { ?>
+            <td title="Click on an address to view list of users seen on that address">IP address</td>
+<?php } else { ?>
+            <td>IP address</td>
+<?php } ?>
+            <td>Total</td>
+            <td>First Seen</td>
+            <td>Last Seen</td>
+        </tr>
+<?php foreach ($summary as $s) { ?>
+        <tr>
+<?php if (check_perms('users_mod')) { ?>
+            <td><a href="<?= $urlStem . $s['IP'] ?>"><?= $s['IP'] ?></a></td>
+<?php } else { ?>
+            <td>"><?= $s['IP'] ?></td>
+<?php } ?>
+            <td><?= $s['total'] ?></td>
+            <td><?= time_diff($s['first']) ?></td>
+            <td><?= time_diff($s['last']) ?></td>
+<?php } ?>
+    </table>
+<?php
+}
+
+list($Page, $Limit) = Format::page_limit(IPS_PER_PAGE);
+$TrackerIps = $userId
+    ? $DB->prepared_query('
+        SELECT IP, fid, tstamp
+        FROM xbt_snatched
+        WHERE uid = ?
+        ORDER BY tstamp DESC
+        LIMIT ?
+        ', $userId, $Limit
+    )
+    : $DB->prepared_query('
+        SELECT uid, fid, tstamp
+        FROM xbt_snatched
+        WHERE ip = ?
+        ORDER BY tstamp DESC
+        LIMIT ?
+        ', $ipAddr, $Limit
+    );
 
 $DB->query('SELECT FOUND_ROWS()');
 list($NumResults) = $DB->next_record();
@@ -60,29 +124,38 @@ $DB->set_query_id($TrackerIps);
 $Pages = Format::get_pages($Page, $NumResults, IPS_PER_PAGE, 9);
 
 ?>
-<div class="thin">
     <div class="header">
-        <h2>Tracker IP address history for <a href="user.php?id=<?=$UserID?>"><?=$Username?></a></h2>
+        <h3>Detail</h3>
     </div>
-    <div class="linkbox"><?=$Pages?></div>
+
+    <div class="linkbox"><?= $Pages ?></div>
     <table>
         <tr class="colhead">
+<?php if ($userId) { ?>
             <td>IP address</td>
+<?php } else { ?>
+            <td>Username</td>
+<?php } ?>
             <td>Torrent</td>
             <td>Time</td>
         </tr>
 <?php
 $Results = $DB->to_array();
 foreach ($Results as $Index => $Result) {
-    list($IP, $TorrentID, $Time) = $Result;
-
+    list($value, $torrentId, $Time) = $Result;
+    list($torrentInfo) = get_torrent_info($torrentId, true, 0, true, true);
+    $urlStem = $_SERVER['SCRIPT_NAME'] . '?action=tracker_ips&amp;userid=';
 ?>
     <tr class="rowa">
         <td>
-            <?=$IP?> (<?=Tools::get_country_code_by_ajax($IP)?>)<br /><?=Tools::get_host_by_ajax($IP)?>
-            <a href="http://whatismyipaddress.com/ip/<?=display_str($IP)?>" class="brackets tooltip" title="Search WIMIA.com">WI</a>
+<?php if ($userId) { ?>
+            <?=$ipAddr?> (<?=Tools::get_country_code_by_ajax($ipAddr)?>)<br /><?=Tools::get_host_by_ajax($ipAddr)?>
+            <a href="http://whatismyipaddress.com/ip/<?=display_str($ipAddr)?>" class="brackets tooltip" title="Search WIMIA.com">WI</a>
+<?php } else { ?>
+            <a href="<?= $urlStem . $value ?>"><?= Users::user_info($value)['Username'] ?></a>
+<?php } ?>
         </td>
-        <td><a href="torrents.php?torrentid=<?=$TorrentID?>"><?=$TorrentID?></a></td>
+        <td><a href="torrents.php?torrentid=<?= $torrentId ?>"><?= $torrentInfo['Name'] ?></a></td>
         <td><?=date('Y-m-d g:i:s', $Time)?></td>
     </tr>
 <?php
@@ -90,10 +163,9 @@ foreach ($Results as $Index => $Result) {
 ?>
 </table>
 <div class="linkbox">
-    <?=$Pages?>
+    <?= $Pages ?>
 </div>
 </div>
 
 <?php
 View::show_footer();
-?>
