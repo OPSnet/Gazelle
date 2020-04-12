72cdf8b8bbb7dc16e980d5fbaf30c7b9ebf31abe
itismadness <itismadness@orpheus.network>
2020-04-12 07:39:31 +0700

    Exit in failure from docker web entrypoint if a command fails

--- .docker/web/entrypoint.sh
+++ .docker/web/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+run_service()
+{
+    service $1 start
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
 # Wait for MySQL...
 counter=1
 while ! mysql -h mysql -ugazelle -ppassword -e "show databases;" > /dev/null 2>&1; do
@@ -18,15 +26,29 @@ fi
 echo "Run migrate..."
 LOCK_MY_DATABASE=1 /var/www/vendor/bin/phinx migrate
 
+if [ $? -ne 0 ]; then
+    echo "PHINX FAILED TO MIGRATE"
+    exit 1
+fi
+
+echo -e "\n"
+
 if [ ! -f /srv/gazelle.txt ]; then
-    touch /srv/gazelle.txt
+    echo "Run seed:run..."
     /var/www/vendor/bin/phinx seed:run -s InitialUserSeeder
+    if [ $? -ne 0 ]; then
+        echo "PHINX FAILED TO SEED"
+        exit 1
+    fi
+    touch /srv/gazelle.txt
+    echo -e "\n"
 fi
 
 echo "Start services..."
-service cron start
-service nginx start
-service php7.3-fpm start
+
+run_service cron
+run_service nginx
+run_service php7.3-fpm
 
 crontab /var/www/.docker/web/crontab
 
