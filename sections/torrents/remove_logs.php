807a6b2a7ed02bec91945034be6a3a19f7070e33
Spine <spine@orpheus.network>
2020-04-17 15:32:01 +0200

    use prepared queries

--- sections/torrents/remove_logs.php
+++ sections/torrents/remove_logs.php
@@ -1,30 +1,26 @@
 <?php
-$TorrentID = $_GET['torrentid'];
-
-if (!$TorrentID || !is_number($TorrentID)) {
-    error(404);
-}
 
 if (!check_perms('torrents_delete')) {
     error(403);
 }
 
-$DB->query("SELECT LogID FROM torrents_logs WHERE TorrentID = ".$TorrentID);
-
-if (!$DB->has_results()) {
+$TorrentID = intval($_GET['torrentid']);
+if (!$TorrentID) {
+    error(404);
+}
+if (!$DB->scalar('SELECT 1 FROM torrents_logs WHERE TorrentID = ?', $TorrentID)) {
     error('Torrent has no logs.');
 }
 
-$DB->query("SELECT GroupID FROM torrents WHERE ID = ".$TorrentID);
-list($GroupID) = $DB->next_record();
+$DB->prepared_query('DELETE FROM torrents_logs WHERE TorrentID = ?', $TorrentID);
+$DB->prepared_query("UPDATE torrents SET HasLog='1', HasLogDB='0', LogScore=0, LogChecksum='0' WHERE ID = ?", $TorrentID);
 
-$DB->query("DELETE FROM torrents_logs WHERE TorrentID=".$TorrentID);
-$DB->query("UPDATE torrents SET HasLog='1', HasLogDB=0, LogScore=0, LogChecksum=0 WHERE ID=".$TorrentID);
-$DB->query(sprintf("INSERT INTO group_log (GroupID, TorrentID, UserID, Time, Info) VALUES(%d, %d, %d, '%s', 'Logs removed from torrent')",
-    $GroupID, $TorrentID, $LoggedUser['ID'], sqltime()));
+$GroupID = $DB->scalar('SELECT GroupID FROM torrents WHERE ID = ?', $TorrentID);
+$DB->prepared_query('
+    INSERT INTO group_log (GroupID, TorrentID, UserID, Info, Time) VALUES(?, ?, ?, ?, now())
+    ', $GroupID, $TorrentID, $LoggedUser['ID'], 'Logs removed from torrent'
+);
 
-$Cache->delete_value("torrent_group_{$GroupID}");
-$Cache->delete_value("torrents_details_{$GroupID}");
+$Cache->deleteMulti(["torrent_group_$GroupID", "torrents_details_$GroupID"]);
 
-$Location = (empty($_SERVER['HTTP_REFERER'])) ? "torrents.php?torrentid={$TorrentID}" : $_SERVER['HTTP_REFERER'];
-header("Location: {$Location}");
+header('Location: ' . empty($_SERVER['HTTP_REFERER']) ? "torrents.php?torrentid={$TorrentID}" : $_SERVER['HTTP_REFERER']);
