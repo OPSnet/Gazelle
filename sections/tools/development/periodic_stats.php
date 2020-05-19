9d73e6dd5c8080fdb0b6716ed1d36251c971aaf2
Quetzal <quetzal@orpheus.network>
2020-05-19 15:52:26 +0000

    Per-task stat graphs

--- sections/tools/development/periodic_stats.php
+++ sections/tools/development/periodic_stats.php
@@ -26,11 +26,11 @@ View::show_header('Periodic Task Statistics');
             <td>Errors</td>
         </tr>
         <tr>
-            <td><?=$stats['totals']['runs']?></td>
-            <td><?=$stats['totals']['duration']?></td>
-            <td><?=$stats['totals']['processed']?></td>
-            <td><?=$stats['totals']['events']?></td>
-            <td><?=$stats['totals']['errors']?></td>
+            <td><?=number_format($stats['totals']['runs'])?></td>
+            <td><?=number_format($stats['totals']['duration'])?> ms</td>
+            <td><?=number_format($stats['totals']['processed'])?></td>
+            <td><?=number_format($stats['totals']['events'])?></td>
+            <td><?=number_format($stats['totals']['errors'])?></td>
         </tr>
     </table>
     <br />
