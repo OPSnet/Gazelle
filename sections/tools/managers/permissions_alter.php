7d9dcc6e5ba0a9149533313b85d9ba12da0d9db3
Spine <spine@orpheus.network>
2020-05-24 10:36:06 +0000

    error() early, error() often

--- sections/tools/managers/permissions_alter.php
+++ sections/tools/managers/permissions_alter.php
@@ -41,18 +41,20 @@ if ($id) {
 
     if (!empty($_POST['submit'])) {
         $err = $Val->ValidateForm($_POST);
+        if ($err) {
+            error($err);
+        }
 
         if (!is_numeric($id)) {
             if ($DB->scalar('SELECT ID FROM permissions WHERE Level = ?', $_REQUEST['level'])) {
-                $err = 'There is already a permission class with that level.';
+                error('There is already a permission class with that level.');
             }
         } else if (empty($_REQUEST['secondary']) == $secondary) {
             if (!$secondary && $DB->scalar('SELECT count(*) FROM users_main WHERE PermissionID = ?', $id) ||
                  $secondary && $DB->scalar('SELECT count(*) FROM users_levels WHERE PermissionID = ?', $id)) {
-                $err = "You can't toggle secondary when there are users";
+                error("You can't toggle secondary when there are users");
             }
         }
-
 
         $values = [];
         foreach ($_REQUEST as $key => $perms) {
@@ -75,85 +77,77 @@ if ($id) {
 
         $values['MaxCollages'] = $_REQUEST['maxcollages'];
 
-        if (!$err) {
-            if (!is_numeric($id)) {
-                $DB->prepared_query('
-                    INSERT INTO permissions
-                        (Level, Name, Secondary, PermittedForums, `Values`, DisplayStaff, StaffGroup, badge)
-                    VALUES
-                        (?,     ?,    ?,         ?,                ?,       ?,            ?,          ?)
-                    ', $level, $name, $secondary, $forums, serialize($values), $displayStaff, $staffGroup, $badge
-                );
-            } else {
-                $DB->prepared_query('
-                    UPDATE permissions
-                    SET Level = ?,
-                        Name = ?,
-                        Secondary = ?,
-                        PermittedForums = ?,
-                        `Values` = ?,
-                        DisplayStaff = ?,
-                        StaffGroup = ?,
-                        badge = ?
-                    WHERE ID = ?
-                    ', $level, $name, $secondary, $forums, serialize($values), $displayStaff, $staffGroup, $badge, $id
-                );
-                $Cache->delete_value('perm_'.$id);
-                if ($secondary) {
-                    $DB->prepared_query("
-                        SELECT DISTINCT concat('user_info_heavy_', UserID)
-                        FROM users_levels
-                        WHERE PermissionID = ?
-                        ", $id
-                    );
-                } else {
-                    $DB->prepared_query("
-                        SELECT DISTINCT concat('user_info_heavy_', UserID)
-                        FROM users_main
-                        WHERE PermissionID = ?
-                        ", $id
-                    );
-                }
-                $Cache->deleteMulti($DB->collect(0, false));
-            }
-            $Cache->delete_value('classes');
-            $Cache->delete_value('staff');
+        if (!is_numeric($id)) {
+            $DB->prepared_query('
+                INSERT INTO permissions
+                       (Level, Name, Secondary, PermittedForums, `Values`, DisplayStaff, StaffGroup, badge)
+                VALUES (?,     ?,    ?,         ?,                ?,       ?,            ?,          ?)
+                ', $level, $name, $secondary, $forums, serialize($values), $displayStaff, $staffGroup, $badge
+            );
         } else {
-            error($err);
-        }
-    }
-
-    include(__DIR__.'/permissions_edit.php');
-} else {
-    $id = $_REQUEST['removeid'] ?? null;
-    if ($id) {
-        if ($DB->scalar('SELECT count(*) FROM users_main WHERE PermissionID = ?', $id)) {
-            $err = 'You cannot delete a class with users.';
-        } else {
-            if ($DB->scalar('SELECT Secondary FROM permissions WHERE ID = ?', $id)) {
+            $DB->prepared_query('
+                UPDATE permissions
+                SET Level = ?,
+                    Name = ?,
+                    Secondary = ?,
+                    PermittedForums = ?,
+                    `Values` = ?,
+                    DisplayStaff = ?,
+                    StaffGroup = ?,
+                    badge = ?
+                WHERE ID = ?
+                ', $level, $name, $secondary, $forums, serialize($values), $displayStaff, $staffGroup, $badge, $id
+            );
+            if ($secondary) {
                 $DB->prepared_query("
                     SELECT DISTINCT concat('user_info_heavy_', UserID)
                     FROM users_levels
                     WHERE PermissionID = ?
                     ", $id
                 );
-                $Cache->deleteMulti($DB->collect(0, false));
-                $DB->prepared_query('
-                    DELETE FROM users_levels
-                    WHERE PermissionId = ?
-                    ', $id
+            } else {
+                $DB->prepared_query("
+                    SELECT DISTINCT concat('user_info_heavy_', ID)
+                    FROM users_main
+                    WHERE PermissionID = ?
+                    ", $id
                 );
             }
-
-            $DB->prepared_query('
-                DELETE FROM permissions
-                WHERE ID = ?
-                ', $id
-            );
-
-            $Cache->delete_value('classes');
+            $Cache->deleteMulti(array_merge(['perm_'.$i], $DB->collect(0, false)));
         }
+        $Cache->deleteMulti(['classes', 'staff']);
     }
 
-    include(__DIR__.'/permissions_list.php');
+    require(__DIR__ . '/permissions_edit.php');
+} else {
+    $id = $_REQUEST['removeid'] ?? null;
+    if ($id) {
+        if ($DB->scalar('SELECT count(*) FROM users_main WHERE PermissionID = ?', $id)) {
+            error('You cannot delete a class with users.');
+        }
+        if ($DB->scalar('SELECT Secondary FROM permissions WHERE ID = ?', $id)) {
+            $DB->prepared_query("
+                SELECT DISTINCT concat('user_info_heavy_', UserID)
+                FROM users_levels
+                WHERE PermissionID = ?
+                ", $id
+            );
+            $Cache->deleteMulti($DB->collect(0, false));
+            $DB->prepared_query('
+                DELETE FROM users_levels
+                WHERE PermissionId = ?
+                ', $id
+            );
+        }
+
+        $DB->prepared_query('
+            DELETE FROM permissions
+            WHERE ID = ?
+            ', $id
+        );
+
+        $Cache->delete_value('classes');
+    }
+
+    require(__DIR__ . '/permissions_list.php');
 }
