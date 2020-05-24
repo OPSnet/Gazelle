bd22cf68903cd03842b386053640b871fb62bc4b
Spine <spine@orpheus.network>
2020-05-24 20:08:54 +0200

    no more zerodates in changelog table

---
+++ db/migrations/20200524180602_no_zerodate_changelog.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class NoZerodateChangelog extends AbstractMigration {
+    public function up() {
+        $this->execute("ALTER TABLE changelog MODIFY Time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
+    }
+
+    public function down() {
+        $this->execute("ALTER TABLE changelog MODIFY Time datetime NOT NULL DEFAULT '0000-00-00 00:00:00'");
+    }
+}
