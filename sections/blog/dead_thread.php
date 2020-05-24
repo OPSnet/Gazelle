51ccbb5a3b5cce3f3a48213b534c21814d20339b
Spine <spine@orpheus.network>
2020-05-24 19:56:58 +0200

    no more zerodate in the blog table

--- sections/blog/dead_thread.php
+++ sections/blog/dead_thread.php
@@ -6,12 +6,9 @@ if (!isset($_GET['id'])) {
     error('Please provide an ID for a blog post to remove the thread link from.');
 }
 
-$ID = intval($_GET['id']);
-G::$DB->prepared_query('UPDATE blog SET ThreadID = NULL WHERE ID = ? ', $ID);
-
+G::$DB->prepared_query('UPDATE blog SET ThreadID = NULL WHERE ID = ? ', (int)$_GET['id']);
 if (G::$DB->affected_rows() > 0) {
-    $Cache->delete_value('blog');
-    $Cache->delete_value('feed_blog');
+    $Cache->deleteMulti[('blog', 'feed_blog']);
 }
 
 header('Location: blog.php');
--- sections/blog/index.php
+++ sections/blog/index.php
@@ -4,26 +4,23 @@ enforce_login();
 define('ANNOUNCEMENT_FORUM_ID', 12);
 
 if (check_perms('admin_manage_blog')) {
-    if (!empty($_REQUEST['action'])) {
-        switch ($_REQUEST['action']) {
-            case 'deadthread':
-                require(SERVER_ROOT.'/sections/blog/dead_thread.php');
-                break;
-            case 'takeeditblog':
-                require(SERVER_ROOT.'/sections/blog/take_edit_blog.php');
-                break;
-            case 'deleteblog':
-                require(SERVER_ROOT.'/sections/blog/delete_blog.php');
-                break;
-            case 'takenewblog':
-                require(SERVER_ROOT.'/sections/blog/take_new_blog.php');
-                break;
-            // Fall through as we just need to include blog_page
-            case 'editblog':
-            default:
-                break;
-        }
+    switch ($_REQUEST['action'] ?? 'editblog') {
+        case 'deadthread':
+            require(__DIR__ . '/dead_thread.php');
+            break;
+        case 'takeeditblog':
+            require(__DIR__ . '/take_edit_blog.php');
+            break;
+        case 'deleteblog':
+            require(__DIR__ . '/delete_blog.php');
+            break;
+        case 'takenewblog':
+            require(__DIR__ . '/take_new_blog.php');
+            break;
+        case 'editblog':
+        default:
+            break;
     }
 }
 
-require(SERVER_ROOT.'/sections/blog/blog_page.php');
+require(__DIR__ . '/blog_page.php');
