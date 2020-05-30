48884daa59c8ffaad517d9823accb7ef6b86fe6a
Spine <spine@orpheus.network>
2020-05-30 12:59:19 +0000

    Apply suggestion to sections/torrents/remove_logs.php

--- sections/torrents/remove_logs.php
+++ sections/torrents/remove_logs.php
@@ -16,7 +16,7 @@ $DB->prepared_query('DELETE FROM torrents_logs WHERE TorrentID = ?', $TorrentID)
 $DB->prepared_query("UPDATE torrents SET HasLog='1', HasLogDB='0', LogScore=0, LogChecksum='0' WHERE ID = ?", $TorrentID);
 
 $GroupID = $DB->scalar('SELECT GroupID FROM torrents WHERE ID = ?', $TorrentID);
-Torrents::write_group_log($GroupID, 0, $LoggedUser['ID'], "All logs removed from torrent", 0);
+Torrents::write_group_log($GroupID, $TorrentID, $LoggedUser['ID'], "All logs removed from torrent", 0);
 
 $Cache->deleteMulti(["torrent_group_$GroupID", "torrents_details_$GroupID"]);
 header('Location: ' . (empty($_SERVER['HTTP_REFERER']) ? "torrents.php?torrentid={$TorrentID}" : $_SERVER['HTTP_REFERER']));
