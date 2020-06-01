f8f0a0351e399cbedb8f3acc05946d3dde6ff7a7
Spine <spine@orpheus.network>
2020-06-01 13:39:57 +0000

    no more zerodates in login_attempts table

--- sections/login/2fa.php
+++ sections/login/2fa.php
@@ -8,10 +8,10 @@ if (strtotime($BannedUntil) < time()) {
     <form class="auth_form" name="login" id="loginform" method="post" action="login.php?act=2fa">
         <?php
 
-        if (!empty($BannedUntil) && $BannedUntil != '0000-00-00 00:00:00') {
+        if ($BannedUntil) {
             $DB->prepared_query("
                 UPDATE login_attempts
-                SET BannedUntil = '0000-00-00 00:00:00', Attempts = '0'
+                SET BannedUntil = NULL, Attempts = 0
                 WHERE ID = ?
                 ", $AttemptID
             );
