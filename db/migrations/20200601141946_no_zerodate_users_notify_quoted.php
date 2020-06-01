a4a2707935bf865da3727ae62583d16fec3089e0
Spine <spine@orpheus.network>
2020-06-01 18:41:02 +0200

    no more zerodates in users_notify_quoted table

---
+++ db/migrations/20200601141946_no_zerodate_users_notify_quoted.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class NoZerodateUsersNotifyQuoted extends AbstractMigration {
+    public function up() {
+        $this->execute("ALTER TABLE users_notify_quoted
+            MODIFY Date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ");
+    }
+
+    public function down() {
+        $this->execute("ALTER TABLE users_notify_quoted
+            MODIFY Date datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+        ");
+    }
+}
