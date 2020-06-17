cbe9502c64b56cf0691224132c68d12c509c7fbb
Spine <spine@orpheus.network>
2020-06-17 19:33:25 +0200

    allow bulk stickiness editing of messages

--- sections/inbox/massdelete_handle.php
+++ sections/inbox/massdelete_handle.php
@@ -15,50 +15,65 @@ if (!isset($_POST['messages']) || !is_array($_POST['messages'])) {
     elseif (isset($_POST['read'])) {
         $Message = 'to mark as read';
     }
-    error("You forgot to select messages $Message.");
+    elseif (isset($_POST['sticky'])) {
+        $Message = 'to mark as (un)sticky';
+    }
+    error("You forgot to select any messages $Message.");
     header('Location: ' . Inbox::getLinkQuick($Section, $UnreadFirst, Inbox::RAW));
     die();
 }
 
 $Messages = $_POST['messages'];
 foreach ($Messages AS $ConvID) {
-    $ConvID = trim($ConvID);
-    if (!is_number($ConvID)) {
+    if ((int)$ConvID < 1) {
         error(0);
     }
 }
-$ConvIDs = implode(',', $Messages);
-$DB->query("
-    SELECT COUNT(ConvID)
+$placeholders = implode(',', array_fill(0, count($Messages), '?'));
+$args = array_merge($Messages, [$UserID]);
+$MessageCount = $DB->scalar("
+    SELECT count(*)
     FROM pm_conversations_users
-    WHERE ConvID IN ($ConvIDs)
-        AND UserID=$UserID");
-list($MessageCount) = $DB->next_record();
+    WHERE ConvID IN ($placeholders) AND UserID = ?
+    ", ...$args
+);
 if ($MessageCount != count($Messages)) {
     error(0);
 }
 
 if (isset($_POST['delete'])) {
-    $DB->query("
-        UPDATE pm_conversations_users
-        SET
-            InInbox='0',
-            InSentbox='0',
-            Sticky='0',
-            UnRead='0'
-        WHERE ConvID IN($ConvIDs)
-            AND UserID=$UserID");
+    $DB->prepared_query("
+        UPDATE pm_conversations_users SET
+            InInbox   = '0',
+            InSentbox = '0',
+            Sticky    = '0',
+            UnRead    = '0'
+        WHERE ConvID IN ($placeholders) AND UserID = ?
+        ", ...$args
+    );
 } elseif (isset($_POST['unread'])) {
-    $DB->query("
-        UPDATE pm_conversations_users
-        SET Unread='1'
-        WHERE ConvID IN($ConvIDs) AND UserID=$UserID");
+    $DB->prepared_query("
+        UPDATE pm_conversations_users SET
+            Unread = '1'
+        WHERE ConvID IN ($placeholders) AND UserID = ?
+        ", ...$args
+    );
 } elseif (isset($_POST['read'])) {
-    $DB->query("
-        UPDATE pm_conversations_users
-        SET Unread='0'
-        WHERE ConvID IN($ConvIDs) AND UserID=$UserID");
+    $DB->prepared_query("
+        UPDATE pm_conversations_users SET
+            Unread = '0'
+        WHERE ConvID IN ($placeholders) AND UserID = ?
+        ", ...$args
+    );
+} elseif (isset($_POST['sticky'])) {
+    $DB->prepared_query("
+        UPDATE pm_conversations_users SET
+            Sticky = CASE WHEN Sticky = '0' THEN '1' ELSE '0' END
+        WHERE ConvID IN ($placeholders) AND UserID = ?
+        ", ...$args
+    );
 }
+
 $Cache->delete_value('inbox_new_'.$UserID);
 
 header('Location: ' . Inbox::getLinkQuick($Section, $UnreadFirst, Inbox::RAW));
