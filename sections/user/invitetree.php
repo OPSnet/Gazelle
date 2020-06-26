9f9e11468ed2defd970bddf4cd6a6add8ad7c3e4
Spine <spine@orpheus.network>
2020-06-27 00:02:47 +0200

    fix permissions check for viewing invites

--- sections/user/invitetree.php
+++ sections/user/invitetree.php
@@ -1,20 +1,22 @@
 <?php
-if (isset($_GET['userid']) && check_perms('users_view_invites')) {
-    if (!is_number($_GET['userid'])) {
-        error(403);
-    }
 
-    $UserID = $_GET['userid'];
-    $Sneaky = true;
-} else {
+if (!isset($_GET['userid'])) {
     $UserCount = Users::get_enabled_users_count();
     $UserID = $LoggedUser['ID'];
     $Sneaky = false;
+} else {
+    if (!check_perms('users_view_invites')) {
+        error(403);
+    }    
+    $UserID = (int)$_GET['userid'];
+    if ($UserID < 1) {
+        error(404);
+    }
+    $Sneaky = true;
 }
 
-list($UserID, $Username, $PermissionID) = array_values(Users::user_info($UserID));
+list($UserID, $Username) = array_values(Users::user_info($UserID));
 
-include(SERVER_ROOT.'/classes/invite_tree.class.php');
 $Tree = new INVITE_TREE($UserID);
 
 View::show_header($Username.' &gt; Invites &gt; Tree');
@@ -24,7 +26,7 @@ View::show_header($Username.' &gt; Invites &gt; Tree');
         <h2><?=Users::format_username($UserID, false, false, false)?> &gt; <a href="user.php?action=invite&amp;userid=<?=$UserID?>">Invites</a> &gt; Tree</h2>
     </div>
     <div class="box pad">
-<?php    $Tree->make_tree(); ?>
+<?php $Tree->make_tree(); ?>
     </div>
 </div>
 <?php
