b2da722ced3d454ac6df6fcebfb9f3d3728853cd
Spine <spine@orpheus.network>
2020-08-10 12:17:23 +0200

    Do not die if a class cannot be loaded

--- classes/classloader.php
+++ classes/classloader.php
@@ -12,7 +12,6 @@ spl_autoload_register(function ($ClassName) {
 
     $FilePath = __DIR__ . '/' . strtolower($ClassName) . '.class.php';
     if (!file_exists($FilePath)) {
-        // TODO: Rename the following classes to conform with the code guidelines
         switch ($ClassName) {
             case 'DB_MYSQL':
                 $FileName = 'mysql.class';
@@ -21,12 +20,12 @@ spl_autoload_register(function ($ClassName) {
             case 'BENCODE_LIST':
                 $FileName = 'torrent.class';
                 break;
-            default:
-                die("Couldn't import class $ClassName");
         }
         $FilePath = __DIR__ . "/$FileName.php";
     }
-    require_once($FilePath);
+    if (file_exists($FilePath)) {
+        require_once($FilePath);
+    }
 });
 
 require(__DIR__.'/../vendor/autoload.php');
