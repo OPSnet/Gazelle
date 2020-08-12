ddd6eb97e3d89a176e79eedbb8f5e71cf9689f2e
Spine <spine@orpheus.network>
2020-08-12 22:02:13 +0200

    Allow different ways of displaying stats related to tables holding deleted data

--- sections/tools/data/database_specifics.php
+++ sections/tools/data/database_specifics.php
@@ -10,9 +10,10 @@ if (!check_perms('site_database_specifics')) {
 if (!empty($_GET['table'])) {
     if (preg_match('/([\w-]+)/', $_GET['table'], $match)) {
         $DB->prepared_query('SHOW CREATE TABLE ' . $match[1]);
-        list(,$definition) = $DB->next_record(MYSQLI_NUM, false);
+        [,$definition] = $DB->next_record(MYSQLI_NUM, false);
         header('Content-type: text/plain');
-        die($definition);
+        echo $definition;
+        exit;
     }
 }
 
@@ -21,21 +22,57 @@ $SortOrderMap = [
     'datasize' => ['data_length', 'desc', 'table size'],
     'freeratio' => ['CASE WHEN data_length = 0 THEN 0 ELSE data_free / data_length END', 'desc', 'table bloat'],
     'indexsize' => ['index_length', 'desc', 'index size'],
-    'name' => ['table_name', 'desc', 'name'],
+    'name' => ['table_name', 'asc', 'name'],
     'rows' => ['table_rows', 'desc', 'row counts'],
     'rowsize' => ['avg_row_length', 'desc', 'mean row length'],
-    'totalsize' => ['data_length + index_length', 'desc', 'total table size'],
+    'totalsize' => ['total_length', 'desc', 'total table size'],
 ];
-$SortOrder = (!empty($_GET['order']) && isset($SortOrderMap[$_GET['order']])) ? $_GET['order'] : 'totalsize';
-$orderBy = $SortOrderMap[$SortOrder][0];
-$orderWay = (empty($_GET['sort']) || $_GET['sort'] == $SortOrderMap[$SortOrder][1])
+
+$SortOrder = $_GET['order'] ?? 'totalsize';
+$orderBy   = $SortOrderMap[$SortOrder][0];
+$orderWay  = (empty($_GET['sort']) || $_GET['sort'] == $SortOrderMap[$SortOrder][1])
     ? $SortOrderMap[$SortOrder][1]
     : SortableTableHeader::SORT_DIRS[$SortOrderMap[$SortOrder][1]];
 
+$header = new SortableTableHeader([
+    'datafree' => 'Free Size',
+    'datasize' => 'Data Size',
+    'freeratio' => 'Bloat %',
+    'indexsize' => 'Index Size',
+    'name' => 'Name',
+    'rows' => 'Rows',
+    'rowsize' => 'Row Size',
+    'totalsize' => 'Total Size',
+], $SortOrder, $orderWay);
+
+$mode = $_GET['mode'] ?? 'show';
+switch($mode) {
+    case 'show':
+        $tableColumn = 'table_name';
+        $where = '';
+        break;
+    case 'merge':
+        $tableColumn = "replace(table_name, 'deleted_', '')";
+        $where = '';
+        break;
+    case 'exclude':
+        $tableColumn = 'table_name';
+        $where = "AND table_name NOT LIKE 'deleted%'";
+        break;
+}
+
 $DB->prepared_query("
-    SELECT table_name, engine, table_rows, avg_row_length, data_length, index_length, data_free
+    SELECT $tableColumn AS table_name,
+        engine,
+        sum(table_rows) AS table_rows,
+        avg(avg_row_length) AS avg_row_length,
+        sum(data_length) AS data_length,
+        sum(index_length) AS index_length,
+        sum(index_length + data_length) AS total_length,
+        sum(data_free) AS data_free
     FROM information_schema.tables
-    WHERE table_schema = ?
+    WHERE table_schema = ? $where
+    GROUP BY $tableColumn, engine
     ORDER by $orderBy $orderWay
     ", SQLDB
 );
@@ -48,7 +85,7 @@ foreach ($Tables as $name => $info) {
             $data[$name] = round($info['data_length'] == 0 ? 0 : $info['data_free'] / $info['data_length'], 2);
             break;
         case 'totalsize':
-            $data[$name] = $info['data_length'] + $info['index_length'];
+            $data[$name] = $info['total_length'];
             break;
         default:
             $data[$name] = $info[$SortOrderMap[$SortOrder][0]];
@@ -57,7 +94,11 @@ foreach ($Tables as $name => $info) {
 
 View::show_header('Database Specifics');
 ?>
-
+<div class="linkbox">
+    <a href="tools.php?action=database_specifics&amp;mode=show" title="Tables of deleted data are shown separately" class="brackets">Show deleted data</a>
+    <a href="tools.php?action=database_specifics&amp;mode=merge" title="Stats of tables of deleted data are merged with their source table" class="brackets">Merge deleted data</a>
+    <a href="tools.php?action=database_specifics&amp;mode=exclude" title="Tables of deleted data are excluded" class="brackets">Exclude deleted data</a>
+</div>
 <script src="<?= STATIC_SERVER ?>functions/highcharts.js"></script>
 <script src="<?= STATIC_SERVER ?>functions/highcharts_custom.js"></script>
 <script>
@@ -106,28 +147,17 @@ Highcharts.chart('statistics', {
 </figure>
 </div>
 <br />
-<?php
-$header = new SortableTableHeader([
-    'datafree' => 'Free Size',
-    'datasize' => 'Data Size',
-    'freeratio' => 'Bloat %',
-    'indexsize' => 'Index Size',
-    'name' => 'Name',
-    'rows' => 'Rows',
-    'rowsize' => 'Row Size',
-    'totalsize' => 'Total Size',
-], $SortOrder, $orderWay);
-?>
+
 <table>
     <tr class="colhead" style="text-align:right">
         <td style="text-align:left" class="nobr"><?= $header->emit('name', $SortOrderMap['name'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('rows', $SortOrderMap['rows'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('rowsize', $SortOrderMap['rowsize'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('datasize', $SortOrderMap['datasize'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('indexsize', $SortOrderMap['indexsize'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('datafree', $SortOrderMap['datafree'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('freeratio', $SortOrderMap['freeratio'][1]) ?></td>
-        <td class="nobr"><?= $header->emit('totalsize', $SortOrderMap['totalsize'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('rows', $SortOrderMap['rows'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('rowsize', $SortOrderMap['rowsize'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('datasize', $SortOrderMap['datasize'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('indexsize', $SortOrderMap['indexsize'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('datafree', $SortOrderMap['datafree'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('freeratio', $SortOrderMap['freeratio'][1]) ?></td>
+        <td style="text-align:right" class="nobr"><?= $header->emit('totalsize', $SortOrderMap['totalsize'][1]) ?></td>
     </tr>
 <?php
 $TotalRows = 0;
@@ -164,14 +194,14 @@ foreach ($Tables as $t) {
 }
 ?>
     <tr>
+        <td><b>Grand totals</b></td>
+        <td class="number_column"><b><?= number_format($TotalRows) ?></b></td>
         <td></td>
-        <td class="number_column"><?= number_format($TotalRows) ?></td>
-        <td></td>
-        <td class="number_column"><?= Format::get_size($TotalDataSize) ?></td>
-        <td class="number_column"><?= Format::get_size($TotalIndexSize) ?></td>
-        <td class="number_column"><?= Format::get_size($TotalFreeSize) ?></td>
-        <td class="number_column"><?= sprintf('%0.2f', $TotalDataSize == 0 ? 0 : ($TotalFreeSize / $TotalDataSize) * 100) ?></td>
-        <td class="number_column"><?= Format::get_size($TotalDataSize + $TotalIndexSize) ?></td>
+        <td class="number_column"><b><?= Format::get_size($TotalDataSize) ?></b></td>
+        <td class="number_column"><b><?= Format::get_size($TotalIndexSize) ?></b></td>
+        <td class="number_column"><b><?= Format::get_size($TotalFreeSize) ?></b></td>
+        <td class="number_column"><b><?= sprintf('%0.2f', $TotalDataSize == 0 ? 0 : ($TotalFreeSize / $TotalDataSize) * 100) ?></b></td>
+        <td class="number_column"><b><?= Format::get_size($TotalDataSize + $TotalIndexSize) ?></b></td>
     </tr>
 </table>
 <?php
