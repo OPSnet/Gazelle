8109e72ec34ba950394f406bc6ae87ca2f88ec63
Spine <spine@orpheus.network>
2020-08-14 23:13:07 +0200

    squashing more zero dates

---
+++ db/migrations/20200814203732_null_date_columns.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class NullDateColumns extends AbstractMigration {
+    public function up() {
+        $this->execute("ALTER TABLE deleted_torrents MODIFY LastReseedRequest datetime DEFAULT NULL");
+        $this->execute("ALTER TABLE torrents_bad_folders MODIFY TimeAdded datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        $this->execute("ALTER TABLE forums_topic_notes MODIFY AddedTime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        $this->execute("ALTER TABLE referral_users MODIFY Joined timestamp NULL");
+    }
+
+    public function down() {
+        $this->execute("ALTER TABLE deleted_torrents MODIFY LastReseedRequest datetime DEFAULT NOT NULL");
+        $this->execute("ALTER TABLE torrents_bad_folders MODIFY TimeAdded datetime NOT NULL");
+        $this->execute("ALTER TABLE forums_topic_notes MODIFY AddedTime datetime NOT NULL");
+        $this->execute("ALTER TABLE referral_users MODIFY Joined timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
+    }
+}
