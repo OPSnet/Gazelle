a7d0ddf5fb3cd237d9e63b67b3ab60a88a2c20ac
Spine <spine@orpheus.network>
2020-09-01 17:06:52 +0000

    add info(source, version) and group(releaseName) entries

---
+++ app/Json.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Gazelle;
+
+abstract class Json extends Base {
+    protected $version;
+    protected $source;
+    protected $mode;
+
+    public function __construct() {
+        parent::__construct();
+        $this->source = SITE_NAME;
+        $this->mode = 0;
+        $this->version = 1;
+    }
+
+    /**
+     * The payload of a valid JSON response, implemented in the child class.
+     * @return array Payload to be passed to json_encode()
+     *         null if the payload cannot be produced (permissions, id not found, ...).
+     */
+    abstract public function payload(): ?array;
+
+    /**
+     * Configure JSON printing (any of the json_encode  JSON_* constants)
+     *
+     * @param int $mode the bit-or'ed values to confgure encoding results
+     */
+    public function setMode(string $mode) {
+        $this->mode = $mode;
+        return $this;
+    }
+
+    /**
+     * set the version of the Json payload. Increment the
+     * value when there is significant change in the payload.
+     * If not called, the version defaults to 1.
+     *
+     * @param int version
+     */
+    public function setVersion(int $version) {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * General failure routine for when bad things happen.
+     *
+     * @param string $message The error set in the JSON response
+     */
+    public function failure(string $message) {
+        print json_encode(
+            array_merge([
+                    'status' => 'failure',
+                    'response' => [],
+                    'error' => $message,
+                    'info' => [
+                        'source'  => $this->source,
+                        'version' => $this->version,
+                    ],
+                ],
+                $this->debug()
+            ),
+            $this->mode
+        );
+    }
+
+    public function emit() {
+        $payload = $this->payload();
+        if (!$payload) {
+            return;
+        }
+        print json_encode(
+            array_merge([
+                    'status' => 'success',
+                    'response' => $payload,
+                    'info' => [
+                        'source'  => $this->source,
+                        'version' => $this->version,
+                    ],
+                ],
+                $this->debug()
+            ),
+            $this->mode
+        );
+    }
+
+    protected function debug() {
+        if (!check_perms('site_debug')) {
+            return [];
+        }
+        global $Debug;
+        return [
+            'queries'  => $Debug->get_queries(),
+            'searches' => $Debug->get_sphinxql_queries(),
+        ];
+    }
+}
---
+++ app/Json/Torrent.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Gazelle\Json;
+
+class Torrent extends \Gazelle\Json {
+    protected $id;
+    protected $infohash;
+    protected $userId;
+    protected $showSnatched;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function setViewer(int $userId) {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    public function setShowSnatched(int $showSnatched) {
+        $this->showSnatched = $showSnatched;
+        return $this;
+    }
+
+    public function setId(int $id) {
+        if (!$id) {
+            $this->failure("bad id parameter");
+            return null;
+        }
+        $this->id = $id;
+        $this->infohash = null;
+        return $this;
+    }
+
+    public function setIdFromHash(string $hash) {
+        $torMan = new \Gazelle\Manager\Torrent;
+        if (!$torMan->isValidHash($hash)) {
+            $this->failure("bad hash parameter");
+            return null;
+        } else {
+            $this->id = $torMan->hashToTorrentId($hash);
+            if (!$this->id) {
+                $this->failure("bad hash parameter");
+                return null;
+            }
+        }
+        $this->infohash = $hash;
+        return $this;
+    }
+
+    public function payload(): ?array {
+        if (!$this->userId) {
+            $this->failure('viewer not set');
+            return null;
+        }
+
+        [$details, $torrent] = (new \Gazelle\Manager\Torrent)
+            ->setTorrentId($this->id)
+            ->setViewer($this->userId)
+            ->setShowSnatched($this->showSnatched ?? 0)
+            ->torrentInfo();
+        if (!$details) {
+            $this->failure("bad id parameter");
+            return null;
+        }
+        $groupID = $details['ID'];
+
+        // TODO: implement as a Gazelle classes
+        global $ReleaseTypes;
+        global $Categories;
+        $categoryName = ($details['CategoryID'] == 0) ? "Unknown" : $Categories[$details['CategoryID'] - 1];
+
+        // Convert file list back to the old format
+        $fileList = explode("\n", $torrent['FileList']);
+        foreach ($fileList as &$file) {
+            $file = \Torrents::filelist_old_format($file);
+        }
+
+        return [
+            'group' => [
+                'wikiBody'        => \Text::full_format($details['WikiBody']),
+                'wikiBBcode'      => $details['WikiBody'],
+                'wikiImage'       => $details['WikiImage'],
+                'id'              => $details['ID'],
+                'name'            => $details['Name'],
+                'year'            => $details['Year'],
+                'recordLabel'     => $details['RecordLabel'] ?? '',
+                'catalogueNumber' => $details['CatalogueNumber'] ?? '',
+                'releaseType'     => $details['ReleaseType'] ?? '',
+                'releaseName'     => $ReleaseTypes[$details['ReleaseType'] ?? 21], // fallback to Unknown release
+                'categoryId'      => $details['CategoryID'],
+                'categoryName'    => $categoryName,
+                'time'            => $details['Time'],
+                'vanityHouse'     => $details['VanityHouse'],
+                'isBookmarked'    => (new \Gazelle\Bookmark)->isTorrentBookmarked($this->userId, $groupID),
+                'tags'            => explode('|', $details['tagNames']),
+                'musicInfo'       => ($categoryName != "Music")
+                    ? null : \Artists::get_artist_by_type($groupID),
+            ],
+            'torrent' => array_merge(
+                !is_null($this->infohash) || $torrent['UserID'] == $this->userId
+                    ? [ 'infoHash' => $torrent['InfoHash'] ]
+                    : [],
+                [
+                    'id'            => $torrent['ID'],
+                    'media'         => $torrent['Media'],
+                    'format'        => $torrent['Format'],
+                    'encoding'      => $torrent['Encoding'],
+                    'remastered'    => $torrent['Remastered'] == 1,
+                    'remasterYear'  => (int)$torrent['RemasterYear'],
+                    'remasterTitle' => $torrent['RemasterTitle'] ?? '',
+                    'remasterRecordLabel' => $torrent['RemasterRecordLabel'] ?? '',
+                    'remasterCatalogueNumber' => $torrent['RemasterCatalogueNumber'] ?? '',
+                    'scene'         => $torrent['Scene'],
+                    'hasLog'        => $torrent['HasLog'],
+                    'hasCue'        => $torrent['HasCue'],
+                    'logScore'      => $torrent['LogScore'],
+                    'logChecksum'   => (int)$torrent['LogChecksum'],
+                    'logCount'      => $torrent['LogCount'],
+                    'fileCount'     => $torrent['FileCount'],
+                    'size'          => $torrent['Size'],
+                    'seeders'       => $torrent['Seeders'],
+                    'leechers'      => $torrent['Leechers'],
+                    'snatched'      => $torrent['Snatched'],
+                    'freeTorrent'   => $torrent['FreeTorrent'],
+                    'reported'      => count(\Torrents::get_reports($this->id)) > 0,
+                    'time'          => $torrent['Time'],
+                    'description'   => $torrent['Description'],
+                    'fileList'      => implode('|||', $fileList),
+                    'filePath'      => $torrent['FilePath'],
+                    'userId'        => $torrent['UserID'],
+                    'username'      => \Users::user_info($torrent['UserID'])['Username'],
+                ]
+            ),
+        ];
+    }
+}
