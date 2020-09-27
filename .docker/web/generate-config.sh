08392fbd023a3d25dc4c625c1837987819c0d021
itismadness <itismadness@orpheus.network>
2020-09-27 01:01:31 +0000

    fix config generator for docker

--- .docker/web/generate-config.sh
+++ .docker/web/generate-config.sh
@@ -16,23 +16,23 @@ sed -i -e "8s/''/'localhost:8080'/" ${CONF_FILE}
 
 sed -i -e "10s/''/'localhost'/" ${CONF_FILE}
 sed -i -e "11s|'https://'.SITE_HOST|'http://localhost:8080'|" ${CONF_FILE}
+sed -i -e "12s|/path|/var/www|g" ${CONF_FILE}
 sed -i -e "13s|/path|/var/www|g" ${CONF_FILE}
-sed -i -e "14s|/path|/var/www|g" ${CONF_FILE}
-sed -i -e "16s|''|'http://localhost:34000'|" ${CONF_FILE}
-sed -i -e "17s|''|'https://localhost:3400'|" ${CONF_FILE}
+sed -i -e "15s|''|'http://localhost:34000'|" ${CONF_FILE}
+sed -i -e "16s|''|'https://localhost:3400'|" ${CONF_FILE}
 
-sed -i -e "39s/localhost/mysql/" ${CONF_FILE}
+sed -i -e "37s/localhost/mysql/" ${CONF_FILE}
+sed -i -e "38s/''/'${MYSQL_USER}'/" ${CONF_FILE}
+sed -i -e "39s/''/'${MYSQL_PASSWORD}'/" ${CONF_FILE}
 sed -i -e "40s/''/'${MYSQL_USER}'/" ${CONF_FILE}
 sed -i -e "41s/''/'${MYSQL_PASSWORD}'/" ${CONF_FILE}
-sed -i -e "42s/''/'${MYSQL_USER}'/" ${CONF_FILE}
-sed -i -e "43s/''/'${MYSQL_PASSWORD}'/" ${CONF_FILE}
-sed -i -e "51s/.*/    ['host' => 'memcached', 'port' => 11211]/" ${CONF_FILE}
+sed -i -e "49s/.*/    ['host' => 'memcached', 'port' => 11211]/" ${CONF_FILE}
 
-sed -i -e "55s/localhost/sphinxsearch/" ${CONF_FILE}
+sed -i -e "53s/localhost/sphinxsearch/" ${CONF_FILE}
 sed -i -e "s/127.0.0.1/sphinxsearch/" ${CONF_FILE}
 
-sed -i -e "64s/false/true/" ${CONF_FILE}
+sed -i -e "62s/false/true/" ${CONF_FILE}
 
-sed -i -e "84s/false/true/" ${CONF_FILE}
+sed -i -e "82s/false/true/" ${CONF_FILE}
 
 echo ""
