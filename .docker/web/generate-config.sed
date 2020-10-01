9c6e5ec98992458de7be6ea1fd7dedd3599e7c2c
Spine <spine@orpheus.network>
2020-10-01 18:07:14 +0000

    Do not attempt to run seeds in a db that has already been set up

---
+++ .docker/web/generate-config.sed
@@ -0,0 +1,20 @@
+s/'SITE_NAME', *'/&Gazelle Dev/
+s/'SITE_HOST', *'/&localhost/
+
+s/'NONSSL_SITE_URL', *'/&localhost:8080/
+s/'SSL_SITE_URL', *'/&localhost:8080/
+s/'SSL_HOST', *'/&localhost/
+
+s/('SITE_URL', *'http)s/\1/
+
+s/'(SERVER_ROOT(_LIVE)?)', *'\/path/'\1', '\/var\/www/
+
+s|'ANNOUNCE_HTTP_URL', *'|&http://localhost:34000|
+s|'ANNOUNCE_HTTPS_URL', *'|&https://localhost:3400|
+
+s/('SQLHOST', *')localhost/\1mysql/
+s/('SPHINX(QL)?_HOST', *')(localhost|127\.0\.0\.1)/\1sphinxsearch/
+
+s|('host' *=>) *'unix:///var/run/memcached.sock'(, *'port' *=>) *0|\1 'memcached'\2 11211|
+
+s/('(DEBUG_MODE|DISABLE_TRACKER)',) *false/\1 true/
--- .docker/web/generate-config.sh
+++ .docker/web/generate-config.sh
@@ -2,37 +2,17 @@
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -f ${THIS_DIR}/../../classes/config.php ]; then
+TARGET=${THIS_DIR}/../../classes/config.php
+
+if [ -f ${TARGET} ]; then
     exit 0;
 fi
 
 echo "GENERATING GAZELLE CONFIG..."
 echo ""
-cp ${THIS_DIR}/../../classes/config.template.php ${THIS_DIR}/../../classes/config.php
-CONF_FILE=${THIS_DIR}/../../classes/config.php
-sed -i -e "6s/''/'Gazelle Dev'/" ${CONF_FILE}
-sed -i -e "7s/''/'localhost:8080'/" ${CONF_FILE}
-sed -i -e "8s/''/'localhost:8080'/" ${CONF_FILE}
-
-sed -i -e "10s/''/'localhost'/" ${CONF_FILE}
-sed -i -e "11s|'https://'.SITE_HOST|'http://localhost:8080'|" ${CONF_FILE}
-sed -i -e "12s|/path|/var/www|g" ${CONF_FILE}
-sed -i -e "13s|/path|/var/www|g" ${CONF_FILE}
-sed -i -e "15s|''|'http://localhost:34000'|" ${CONF_FILE}
-sed -i -e "16s|''|'https://localhost:3400'|" ${CONF_FILE}
-
-sed -i -e "37s/localhost/mysql/" ${CONF_FILE}
-sed -i -e "38s/''/'${MYSQL_USER}'/" ${CONF_FILE}
-sed -i -e "39s/''/'${MYSQL_PASSWORD}'/" ${CONF_FILE}
-sed -i -e "40s/''/'${MYSQL_USER}'/" ${CONF_FILE}
-sed -i -e "41s/''/'${MYSQL_PASSWORD}'/" ${CONF_FILE}
-sed -i -e "49s/.*/    ['host' => 'memcached', 'port' => 11211]/" ${CONF_FILE}
-
-sed -i -e "53s/localhost/sphinxsearch/" ${CONF_FILE}
-sed -i -e "s/127.0.0.1/sphinxsearch/" ${CONF_FILE}
-
-sed -i -e "62s/false/true/" ${CONF_FILE}
-
-sed -i -e "82s/false/true/" ${CONF_FILE}
+sed -Ef $THIS_DIR/generate-config.sed \
+    -e "s/('SQL(LOGIN|_PHINX_USER)', *')/\1${MYSQL_USER}/" \
+    -e "s/('SQL(_PHINX_)?PASS', *')/\1${MYSQL_PASSWORD}/" \
+    ${THIS_DIR}/../../classes/config.template.php > ${TARGET}
 
 echo ""
