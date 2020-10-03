980a74ed36b2277b482f276cf8341a137c17ce96
Spine <spine@orpheus.network>
2020-10-03 11:41:44 +0200

    rename mysql.class => db_mysql.class and simplify class loader

--- classes/classloader.php
+++ classes/classloader.php
@@ -3,31 +3,23 @@
 /**
  * Load classes automatically when they're needed
  *
- * @param string $ClassName class name
+ * @param string $className class name
  */
-spl_autoload_register(function ($ClassName) {
-    if (strpos($ClassName, 'Gazelle\\') === 0) {
-        return;
-    }
-
-    $FilePath = __DIR__ . '/' . strtolower($ClassName) . '.class.php';
-    if (!file_exists($FilePath)) {
-        switch ($ClassName) {
-            case 'DB_MYSQL':
-                $FileName = 'mysql.class';
-                break;
+spl_autoload_register(function (string $className) {
+    $file = __DIR__ . '/' . strtolower($className) . '.class.php';
+    if (!file_exists($file)) {
+        switch ($className) {
             case 'BENCODE_DICT':
             case 'BENCODE_LIST':
-                $FileName = 'torrent.class';
+                $file = __DIR__ . "/torrent.class.php";
                 break;
             default:
                 return;
         }
-        $FilePath = __DIR__ . "/$FileName.php";
     }
-    if (file_exists($FilePath)) {
-        require_once($FilePath);
+    if (file_exists($file)) {
+        require_once($file);
     }
 });
 
-require(__DIR__.'/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
