a810ff3b636727c1769be095890176d763fa1757
itismadness <itismadness@orpheus.network>
2020-10-03 15:16:38 +0000

    fix permalink being invalid for comments

--- app/CommentViewer/Artist.php
+++ app/CommentViewer/Artist.php
@@ -6,7 +6,7 @@ class Artist extends \Gazelle\CommentViewer {
 
     public function __construct(\Twig\Environment $twig, int $viewerId, int $artistId) {
         parent::__construct($twig, $viewerId);
-        $this->baseLink = "artist.php?id={$artistId}&amp;postid=%d#post%d";
+        $this->baseLink = "artist.php?id={$artistId}&postid=%d#post%d";
         $this->page     = 'artist';
     }
 }
--- app/CommentViewer/Request.php
+++ app/CommentViewer/Request.php
@@ -6,7 +6,7 @@ class Request extends \Gazelle\CommentViewer {
 
     public function __construct(\Twig\Environment $twig, int $viewerId, int $requestId) {
         parent::__construct($twig, $viewerId);
-        $this->baseLink = "requests.php?action=view&amp;id={$requestId}&amp;postid=%d#post%d";
+        $this->baseLink = "requests.php?action=view&id={$requestId}&postid=%d#post%d";
         $this->page     = 'request';
     }
 }
