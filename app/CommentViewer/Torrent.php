12ed493a59f070315634324e3b1cb58a6188625b
itismadness <itismadness@orpheus.network>
2020-10-03 15:16:38 +0000

    fix permalink being invalid for comments

--- app/CommentViewer/Torrent.php
+++ app/CommentViewer/Torrent.php
@@ -6,7 +6,7 @@ class Torrent extends \Gazelle\CommentViewer {
 
     public function __construct(\Twig\Environment $twig, int $viewerId, int $groupId) {
         parent::__construct($twig, $viewerId);
-        $this->baseLink = "torrents.php?id={$groupId}&amp;postid=%d#post%d";
+        $this->baseLink = "torrents.php?id={$groupId}&postid=%d#post%d";
         $this->page     = 'torrent';
     }
 }
