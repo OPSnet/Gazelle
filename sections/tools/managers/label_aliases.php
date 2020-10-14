fdef0ae0dbbc7d21618cd4a25e491b1e58078ded
Spine <spine@orpheus.network>
2020-10-14 17:25:15 +0000

    rewrite prepared queries in tools/managers

--- sections/tools/managers/label_aliases.php
+++ sections/tools/managers/label_aliases.php
@@ -1,9 +1,4 @@
 <?php
-/*
-if (!check_perms('torrents_edit') || $LoggedUser['DisableWiki']) {
-    error(403);
-}
-*/
 
 if (!check_perms('users_mod') && !$LoggedUser['ExtraClasses'][DELTA_TEAM]) {
     error(403);
@@ -11,55 +6,45 @@ if (!check_perms('users_mod') && !$LoggedUser['ExtraClasses'][DELTA_TEAM]) {
 
 View::show_header('Label Aliases');
 
-$OrderBy = $_GET['order'] === 'BadLabels' ? 'BadLabel' : 'AliasLabel';
-/*
-$LabelID = (int)$_GET['id'];
-$LabelNameSQL = '';
-//TODO join with labels table to get label name
-if (!empty($LabelID)) {
-    $DB->query("
-        SELECT name
-        FROM labels
-        WHERE ID = '$LabelID'");
-    if ($DB->has_results()) {
-        list($LabelName) = $DB->next_record();
-    }
-    $LabelNameSQL = " WHERE AliasLabel = '$LabelName'";
-}
-*/
-
 if (isset($_POST['newalias'])) {
-    $BadLabel = db_string($_POST['BadLabel']);
-    $AliasLabel = db_string($_POST['AliasLabel']);
-
-    $DB->query("
-        INSERT INTO label_aliases (BadLabel, AliasLabel)
-        VALUES ('$BadLabel', '$AliasLabel')");
+    $DB->prepared_query("
+        INSERT INTO label_aliases
+               (BadLabel, AliasLabel)
+        VALUES (?,        ?)
+        ", trim($_POST['BadLabel']), trim($_POST['AliasLabel'])
+    );
 }
-
-if (isset($_POST['changealias']) && is_number($_POST['aliasid'])) {
-    $AliasID = $_POST['aliasid'];
-    $BadLabel = db_string($_POST['BadLabel']);
-    $AliasLabel = db_string($_POST['AliasLabel']);
-
-    if ($_POST['save']) {
-        $DB->query("
-            UPDATE label_aliases
-            SET BadLabel = '$BadLabel', AliasLabel = '$AliasLabel'
-            WHERE ID = '$AliasID' ");
-    }
-    if ($_POST['delete']) {
-        $DB->query("
+elseif (isset($_POST['changealias']) && isset($_POST['aliasid']) && (int)$_POST['aliasid'] > 0) {
+    $aliasId = (int)$_POST['aliasid'];
+    if (isset($_POST['save'])) {
+        $DB->prepared_query("
+            UPDATE label_aliases SET
+                BadLabel = ?,
+                AliasLabel = ?
+            WHERE ID = ?
+            ", $badLabel, $aliasLabel, $aliasId
+        );
+    } elseif (isset($_POST['delete'])) {
+        $DB->prepared_query("
             DELETE FROM label_aliases
-            WHERE ID = '$AliasID'");
+            WHERE ID = ?
+            ", $aliasId
+        );
     }
 }
+
+$orderBy = ($_GET['order'] ?? 'AliasLabel') === 'good' ? 3 : 2;
+$DB->prepared_query("
+    SELECT ID, BadLabel, AliasLabel
+    FROM label_aliases
+    ORDER BY $orderBy
+");
 ?>
 <div class="header">
-    <h2>Label Aliases<?=($LabelName ? " for <a href=\"labels.php?id=$LabelID\">$LabelName</a>" : '')?></h2>
+    <h2>Label Aliases</h2>
     <div class="linkbox">
-        <a href="tools.php?action=label_aliases&amp;order=GoodLabels" class="brackets">Sort by good labels</a>
-        <a href="tools.php?action=label_aliases&amp;order=BadLabels" class="brackets">Sort by bad labels</a>
+        <a href="tools.php?action=label_aliases&amp;order=good" class="brackets">Sort by good labels</a>
+        <a href="tools.php?action=label_aliases&amp;order=bad" class="brackets">Sort by bad labels</a>
     </div>
 </div>
 <table width="100%">
@@ -83,23 +68,16 @@ if (isset($_POST['changealias']) && is_number($_POST['aliasid'])) {
             </td>
         </form>
     </tr>
-<?php
-$DB->query("
-    SELECT ID, BadLabel, AliasLabel
-    FROM label_aliases
-    $LabelNameSQL
-    ORDER BY $OrderBy");
-while (list($ID, $BadLabel, $AliasLabel) = $DB->next_record()) {
-?>
+<?php while ([$id, $badLabel, $aliasLabel] = $DB->next_record()) { ?>
     <tr>
         <form method="post" action="">
             <input type="hidden" name="changealias" value="1" />
-            <input type="hidden" name="aliasid" value="<?=$ID?>" />
+            <input type="hidden" name="aliasid" value="<?=$id?>" />
             <td>
-                <input type="text" name="AliasLabel" value="<?=$AliasLabel?>" />
+                <input type="text" name="AliasLabel" value="<?=$aliasLabel?>" />
             </td>
             <td>
-                <input type="text" name="BadLabel" value="<?=$BadLabel?>" />
+                <input type="text" name="BadLabel" value="<?=$badLabel?>" />
             </td>
             <td>
                 <input type="submit" name="save" value="Save alias" />
@@ -107,8 +85,7 @@ while (list($ID, $BadLabel, $AliasLabel) = $DB->next_record()) {
             </td>
         </form>
     </tr>
-<?php
-}
-?>
+<?php } ?>
 </table>
-<?php View::show_footer(); ?>
+<?php
+View::show_footer();
