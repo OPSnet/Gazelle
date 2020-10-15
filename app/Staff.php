cb38704e0513402962c17c95b6b15b42b93b6a7c
webhead <webhead@orpheus.network>
2020-10-15 17:10:38 +0000

    Staff inbox links

--- app/Staff.php
+++ app/Staff.php
@@ -2,29 +2,32 @@
 
 namespace Gazelle;
 
+use Permissions;
+use Users;
+
 class Staff extends Base {
 
-    /** @var int */
-    protected  $id;
+    /** @var array */
+    protected $user;
 
-    public function __construct(int $id) {
+    public function __construct(array $user) {
         parent::__construct();
-        $this->id = $id;
+        $this->user = $user;
     }
 
     public function id() {
-        return $this->id;
+        return $this->user['ID'];
     }
 
     public function blogAlert() {
-        if (($readTime = $this->cache->get_value('staff_blog_read_'. $this->id)) === false) {
+        if (($readTime = $this->cache->get_value('staff_blog_read_'. $this->user['ID'])) === false) {
             $readTime = $this->db->scalar('
                 SELECT unix_timestamp(Time)
                 FROM staff_blog_visits
                 WHERE UserID = ?
-                ', $this->id
+                ', $this->user['ID']
             ) ?? 0;
-            $this->cache->cache_value('staff_blog_read_' . $this->id, $readTime, 1209600);
+            $this->cache->cache_value('staff_blog_read_' . $this->user['ID'], $readTime, 1209600);
         }
         if (($blogTime = $this->cache->get_value('staff_blog_latest_time')) === false) {
             $blogTime = $this->db->scalar('
@@ -35,5 +38,26 @@ class Staff extends Base {
             $this->cache->cache_value('staff_blog_latest_time', $blogTime, 1209600);
         }
         return $readTime < $blogTime;
+    }
+
+    public function pmCount() {
+        $conditions = [
+            "Status = 'Unanswered'",
+            '(AssignedToUser = ? OR LEAST(?, Level) <= ?)',
+        ];
+        $params = [$this->user['ID'], Permissions::get_level_cap(), $this->user['EffectiveClass']];
+        [$classes] = Users::get_classes();
+        if (check_perms('users_mod')) {
+            $conditions[] = 'Level >= ?';
+            $params[] = $classes[MOD]['Level'];
+        } elseif ($this->user['PermissionID'] === FORUM_MOD) {
+            $conditions[] = 'Level >= ?';
+            $params[] = $classes[FORUM_MOD]['Level'];
+        }
+
+        return $this->db->scalar("
+            SELECT count(*)
+            FROM staff_pm_conversations
+            WHERE " . implode(' AND ', $conditions), ...$params);
     }
 }
