98c08586ac51d5d448bfd1efb8c49f91334dabb5
itismadness <itismadness@orpheus.network>
2020-11-02 20:27:32 +0000

    fix collation for forum title migration

--- db/migrations/20201031182917_forum_thread_title.php
+++ db/migrations/20201031182917_forum_thread_title.php
@@ -5,13 +5,7 @@ use Phinx\Migration\AbstractMigration;
 class ForumThreadTitle extends AbstractMigration {
     public function up() {
         $this->table('forums_topics')
-            ->changeColumn('Title', 'string', ['collation' => 'utf8mb4_bin'])
-            ->update();
-    }
-
-    public function down() {
-        $this->table('forums_topics')
-            ->changeColumn('Title', 'string', ['collation' => 'utf8'])
+            ->changeColumn('Title', 'string', ['collation' => 'utf8mb4_unicode_ci'])
             ->update();
     }
 }
