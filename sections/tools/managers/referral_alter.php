657f56032e425152ac1ec8dec328adcfc35a819a
Spine <spine@orpheus.network>
2020-11-23 20:15:07 +0000

    remove db_array() from codebase

--- sections/tools/managers/referral_alter.php
+++ sections/tools/managers/referral_alter.php
@@ -5,15 +5,14 @@ if (!check_perms('admin_manage_referrals')) {
     error(403);
 }
 
-$P = db_array($_POST);
-$ReferralManager = new \Gazelle\Manager\Referral;
+$ReferralManager = new Gazelle\Manager\Referral;
 
 if ($_POST['submit'] == 'Delete') {
-    if (!is_number($_POST['id']) || $_POST['id'] == '') {
+    $id = (int)$_POST['id'];
+    if (!$id) {
         error(0);
     }
-
-    $ReferralManager->deleteAccount($_POST['id']);
+    $ReferralManager->deleteAccount($id);
 } else {
     $Val->SetFields('site', '1', 'string', 'The site must be set, and has a max length of 30 characters', ['maxlength' => 30]);
     $Val->SetFields('url', '1', 'string', 'The URL must be set, and has a max length of 30 characters', ['maxlength' => 30]);
@@ -22,29 +21,22 @@ if ($_POST['submit'] == 'Delete') {
     $Val->SetFields('active', '1', 'checkbox', '');
     $Err = $Val->ValidateForm($_POST);
 
-    if (substr($P['url'], -1) !== '/') {
-        $P['url'] .= '/';
+    if (substr($_POST['url'], -1) !== '/') {
+        $_POST['url'] .= '/';
     }
 
-    if ($_POST['submit'] == 'Create') {
-        $ReferralManager->createAccount($P['site'], $P['url'], $P['user'], $P['password'],
-            $P['active'] == 'on' ? 1 : 0, $P['type'], $P['cookie']);
-    } elseif ($_POST['submit'] == 'Edit') {
-        if (!is_number($_POST['id']) || $_POST['id'] == '') {
+    if ($_POST['submit'] === 'Create') {
+        $ReferralManager->createAccount($_POST['site'], $_POST['url'], $_POST['user'], $_POST['password'],
+            $_POST['active'] == 'on' ? 1 : 0, $_POST['type'], $_POST['cookie']);
+    } elseif ($_POST['submit'] === 'Edit') {
+        $id = (int)$_POST['id'];
+        if (!$id || !$ReferralManager->getAccount($id)) {
             error(0);
         }
 
-        $account = $ReferralManager->getAccount($P['id']);
-        if ($account == null) {
-            error(0);
-        }
-
-        $P['cookie'] = str_replace('\\"', '"', $P['cookie']);
-        $P['password'] = str_replace('\\"', '"', $P['password']);
-        $ReferralManager->updateAccount($P['id'], $P['site'], $P['url'], $P['user'],
-            $P['password'], $P['active'] == 'on' ? 1 : 0, $P['type'], $P['cookie']);
+        $ReferralManager->updateAccount($_POST['id'], $_POST['site'], $_POST['url'], $_POST['user'],
+            $_POST['password'], $_POST['active'] == 'on' ? 1 : 0, $_POST['type'], $_POST['cookie']);
     }
 }
 
 header('Location: tools.php?action=referral_accounts');
-?>
