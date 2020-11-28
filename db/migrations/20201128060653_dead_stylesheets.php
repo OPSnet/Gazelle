b7eb208dfb80df324d4de7a3878c68d272152fa5
itismadness <itismadness@orpheus.network>
2020-11-28 06:47:15 +0000

    create migration to clear out dead stylesheets

---
+++ db/migrations/20201128060653_dead_stylesheets.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Clears out some old stylesheets. Will have to delete the `stylesheets` cache key after running this.
+ */
+final class DeadStylesheets extends AbstractMigration
+{
+    public function up(): void
+    {
+        $removedStylesheets = [
+            'Hydro',
+            'Anorex',
+            'Mono',
+            'Shiro',
+            'Minimal',
+            'Whatlove',
+            'White.cd',
+            'GTFO Spaceship',
+            'Haze',
+            'Apollo Mat',
+        ];
+        $this->query("DELETE FROM stylesheets WHERE `Name` IN ('" . implode("','", $removedStylesheets) . "')");
+
+        $row = $this->query("SELECT * FROM stylesheets WHERE `Name` = 'Dark Cake'")->fetch(\PDO::FETCH_NUM);
+        if ($row === false) {
+            $this->table('stylesheets')->insert([
+                ['Name' => 'Dark Cake', 'Description' => 'Dark Cake for Orpheus']
+            ])->save();
+        }
+    }
+
+    public function down(): void
+    {
+        $this->query("DELETE FROM stylesheets WHERE `Name` = 'Dark Cake'");
+    }
+}
