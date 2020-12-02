0c8222f80ca3b50b7828e151590abdf7721f6f0f
Spine <spine@orpheus.network>
2020-12-02 10:43:43 +0100

    Don't broadcast information needlessly

--- design/publicfooter.php
+++ design/publicfooter.php
@@ -1,13 +1,6 @@
 </div>
 <div id="foot">
 <span class="links">
-<?php
-$Y = date('Y');
-if ($Y != SITE_LAUNCH_YEAR) {
-    $Y = SITE_LAUNCH_YEAR . "-$Y";
-}
-?>
-Site and design &copy; <?= $Y ?> <?=SITE_NAME?> | <a href='https://github.com/OPSnet/Gazelle'>Project Gazelle</a>
 </span>
 </div>
 </body>
