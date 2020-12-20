36e272fac74cad75cbdf784433004e97886a7dea
Spine <spine@orpheus.network>
2020-12-20 18:46:01 +0100

    fix report comment persistence

--- sections/reportsv2/ajax_update_comment.php
+++ sections/reportsv2/ajax_update_comment.php
@@ -1,12 +1,13 @@
 <?php
 // perform the back end of updating a report comment
 
+authorize();
+
 if (!check_perms('admin_reports')) {
     error(403);
 }
 
-authorize();
-$id = (int)$_GET['id'];
+$id = (int)$_POST['reportid'];
 if ($id) {
     (new Gazelle\ReportV2($id))->comment($_POST['comment']);
 }
