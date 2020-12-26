c37cbf25c5f73e52e2e2cfe26aa71cb0c466f97c
Spine <spine@orpheus.network>
2020-12-26 20:30:10 +0000

    twigify duplicate IP dashboard

---
+++ app/Manager/DuplicateIP.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Gazelle\Manager;
+
+class DuplicateIP extends \Gazelle\Base {
+
+    public function total(int $threshold): int {
+        return $this->db->scalar("
+            SELECT count(*)
+            FROM users_main AS um
+            WHERE um.Enabled = '1'
+                AND um.IP != '127.0.0.1'
+                AND (
+                    SELECT count(DISTINCT h.UserID)
+                    FROM users_history_ips AS h
+                    WHERE h.IP = um.IP
+                ) >= ?
+            ", $threshold
+        );
+    }
+
+    public function page(int $threshold, int $limit, int $offset): array {
+        $this->db->prepared_query("
+            SELECT um.ID    AS user_id,
+                um.IP       AS ipaddr,
+                ui.JoinDate AS joined,
+                (
+                    SELECT count(DISTINCT h.UserID)
+                    FROM users_history_ips AS h
+                    WHERE h.IP = um.IP
+                ) AS uses
+            FROM users_main AS um
+            INNER JOIN users_info AS ui ON (ui.UserID = um.ID)
+            WHERE um.Enabled = '1'
+                AND um.IP != '127.0.0.1'
+                AND (
+                    SELECT count(DISTINCT h.UserID)
+                    FROM users_history_ips AS h
+                    WHERE h.IP = um.IP
+                ) >= ?
+            ORDER BY Uses DESC
+            LIMIT ? OFFSET ?
+            ", $threshold, $limit, $offset
+        );
+        return $this->db->to_array(false, MYSQLI_ASSOC, false);
+    }
+}
