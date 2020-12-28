c67b782bcec102e9c739b827607e0fc5c6936fa6
Spine <spine@orpheus.network>
2020-12-28 23:57:37 +0100

    refactor uploading missing logs

--- sections/logchecker/index.php
+++ sections/logchecker/index.php
@@ -1,28 +1,19 @@
 <?php
 enforce_login();
-if (!empty($_REQUEST['action'])) {
-    switch ($_REQUEST['action']) {
-        case 'test':
-            require(__DIR__.'/test.php');
-            break;
-        case 'take_test':
-            // upload one log and see what it would score in our log checker
-            require(__DIR__.'/take_test.php');
-            break;
-        case 'upload':
-            require(__DIR__.'/upload.php');
-            break;
-        case 'update':
-            // Update torrents that have logs, regardless of score
-            require(__DIR__.'/update.php');
-            break;
-        case 'take_upload':
-            // this actually saves a log for a torrent
-            require(__DIR__.'/take_upload.php');
-            break;
-        default:
-            error(404);
-    }
-} else {
-    require(__DIR__.'/test.php');
+
+switch ($_REQUEST['action'] ?? 'test') {
+    case 'take_test':
+        require_once('take_test.php');
+        break;
+    case 'take_upload':
+        require_once('take_upload.php');
+        break;
+    case 'update':
+        require_once('update.php');
+        break;
+    case 'upload':
+        require_once('upload.php');
+        break;
+    default:
+        require_once('test.php');
 }
