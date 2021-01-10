14fd2960388c8d3cd60446c66ead271286712f8e
Spine <spine@orpheus.network>
2021-01-10 20:15:52 +0000

    remove query() calls from forum voting

--- sections/forums/change_vote.php
+++ sections/forums/change_vote.php
@@ -1,28 +1,27 @@
 <?php
 authorize();
-$ThreadID = $_GET['threadid'];
-$NewVote = $_GET['vote'];
-
-if (is_number($ThreadID) && is_number($NewVote)) {
-    if (!check_perms('site_moderate_forums')) {
-        $DB->query("
-            SELECT ForumID
-            FROM forums_topics
-            WHERE ID = $ThreadID");
-        list($ForumID) = $DB->next_record();
-        if (!in_array($ForumID, $ForumsRevealVoters)) {
-            error(403);
-        }
-    }
-
-    $DB->query("
-        UPDATE forums_polls_votes
-        SET Vote = $NewVote
-        WHERE TopicID = $ThreadID
-            AND UserID = ".$LoggedUser['ID']);
-    $Cache->delete_value('polls_'.$ThreadID);
-    header("Location: forums.php?action=viewthread&threadid=".$ThreadID);
-
-} else {
-    error(404);
+$ThreadID = (int)$_GET['threadid'];
+$NewVote = (int)$_GET['vote'];
+if (!$ThreadID || !$NewVote) {
+    error(403);
 }
+
+if (!check_perms('site_moderate_forums')) {
+    $ForumID = $DB->scalar("
+        SELECT ForumID FROM forums_topics WHERE ID = ?
+        ", $ThreadID
+    );
+    if (!in_array($ForumID, $ForumsRevealVoters)) {
+        error(403);
+    }
+}
+
+$DB->prepared_query("
+    UPDATE forums_polls_votes SET 
+        Vote = ?
+    WHERE TopicID = ?  AND UserID = ?
+    ", $NewVote, $ThreadID, $LoggedUser['ID']
+);
+
+$Cache->delete_value('polls_'.$ThreadID);
+header("Location: forums.php?action=viewthread&threadid=".$ThreadID);
