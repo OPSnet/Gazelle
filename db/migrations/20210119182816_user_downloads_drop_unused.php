3607a6a534b8399c7bf1e50b69ac73b0f9aa3122
Spine <spine@orpheus.network>
2021-01-19 19:32:36 +0100

    another useless index

---
+++ db/migrations/20210119182816_user_downloads_drop_unused.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * On a small site, or if the site is offline it is safe to run this
+ * migration directly. If you are sure, then set the environment variable
+ * LOCK_MY_DATABASE to a value that evaluates as truth, e.g. 1 and then run
+ * again.
+ */
+
+final class UserDownloadsDropUnused extends AbstractMigration
+{
+    public function up(): void {
+        if (!getenv('LOCK_MY_DATABASE')) {
+            die("Migration cannot proceed, use the source: " . __FILE__ . "\n");
+        }
+        $this->execute("
+            ALTER TABLE users_downloads DROP KEY IF EXISTS UserID
+        ");
+    }
+
+    public function down(): void {
+        if (!getenv('LOCK_MY_DATABASE')) {
+            die("Migration cannot proceed, use the source: " . __FILE__ . "\n");
+        }
+        $this->execute("
+            ALTER TABLE users_downloads ADD KEY IF NOT EXISTS UserID (UserID)
+        ");
+    }
+}
