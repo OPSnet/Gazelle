ba766f3dead8fe1cf2475bb80da5d84890b1395e
Spine <spine@orpheus.network>
2021-01-25 17:21:04 +0000

    Move invite removal into User class

--- sections/user/delete_invite.php
+++ sections/user/delete_invite.php
@@ -1,42 +1,17 @@
 <?php
 authorize();
 
-$InviteKey = trim($_GET['invite']);
-$DB->prepared_query('
-    SELECT InviterID
-    FROM invites
-    WHERE InviteKey = ?
-    ', $InviteKey
+$inviteKey = trim($_GET['invite']);
+$userId = $DB->scalar("
+    SELECT InviterID FROM invites WHERE InviteKey = ?
+    ", $inviteKey
 );
-list($UserID) = $DB->next_record();
-if (!$DB->has_results() || $UserID != $LoggedUser['ID']) {
+if (is_null($userId)) {
     error(404);
 }
-
-$DB->prepared_query('
-    DELETE FROM invites
-    WHERE InviteKey = ?
-    ', $InviteKey
-);
-
-if (!check_perms('site_send_unlimited_invites')) {
-    $DB->prepared_query('
-        SELECT Invites
-        FROM users_main
-        WHERE ID = ?
-        ', $UserID
-    );
-    list($Invites) = $DB->next_record();
-    if ($Invites < 10) {
-        $DB->prepared_query('
-            UPDATE users_main
-            SET Invites = Invites + 1
-            WHERE ID = ?
-            ', $UserID
-        );
-        $Cache->begin_transaction("user_info_heavy_$UserID");
-        $Cache->update_row(false, ['Invites' => '+1']);
-        $Cache->commit_transaction(0);
-    }
+if ($userId != $LoggedUser['ID']) {
+    error(403);
 }
+
+(new Gazelle\User($userId))->removeInvite($inviteKey);
 header('Location: user.php?action=invite');
