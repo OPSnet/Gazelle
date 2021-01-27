d8e782820eb095146391509905e34febf282d1ff
Spine <spine@orpheus.network>
2021-01-27 18:08:38 +0000

    remove SQL_CALC_FOUND_ROWS from staff pm list rendering

--- app/Staff.php
+++ app/Staff.php
@@ -2,32 +2,31 @@
 
 namespace Gazelle;
 
-use Permissions;
 use Users;
 
 class Staff extends Base {
 
-    /** @var array */
+    /** @var \Gazelle\User */
     protected $user;
 
-    public function __construct(array $user) {
+    public function __construct(\Gazelle\User $user) {
         parent::__construct();
         $this->user = $user;
     }
 
     public function id() {
-        return $this->user['ID'];
+        return $this->user->id();
     }
 
     public function blogAlert() {
-        if (($readTime = $this->cache->get_value('staff_blog_read_'. $this->user['ID'])) === false) {
+        if (($readTime = $this->cache->get_value('staff_blog_read_'. $this->user->id())) === false) {
             $readTime = $this->db->scalar('
                 SELECT unix_timestamp(Time)
                 FROM staff_blog_visits
                 WHERE UserID = ?
-                ', $this->user['ID']
+                ', $this->user->id()
             ) ?? 0;
-            $this->cache->cache_value('staff_blog_read_' . $this->user['ID'], $readTime, 1209600);
+            $this->cache->cache_value('staff_blog_read_' . $this->user->id(), $readTime, 1209600);
         }
         if (($blogTime = $this->cache->get_value('staff_blog_latest_time')) === false) {
             $blogTime = $this->db->scalar('
@@ -41,23 +40,49 @@ class Staff extends Base {
     }
 
     public function pmCount() {
-        $conditions = [
+        $cond = [
             "Status = 'Unanswered'",
-            '(AssignedToUser = ? OR LEAST(?, Level) <= ?)',
+            '(AssignedToUser = ? OR LEAST((SELECT max(Level) FROM permissions), Level) <= ?)',
         ];
-        $params = [$this->user['ID'], Permissions::get_level_cap(), $this->user['EffectiveClass']];
+        $effectiveClass = $this->user->effectiveClass();
+        $args = [$this->user->id(), $effectiveClass];
         [$classes] = Users::get_classes();
-        if ($this->user['EffectiveClass'] >= $classes[MOD]['Level']) {
-            $conditions[] = 'Level >= ?';
-            $params[] = $classes[MOD]['Level'];
-        } elseif ($this->user['EffectiveClass'] === $classes[FORUM_MOD]['Level']) {
-            $conditions[] = 'Level >= ?';
-            $params[] = $classes[FORUM_MOD]['Level'];
+        if ($effectiveClass >= $classes[MOD]['Level']) {
+            $cond[] = 'Level >= ?';
+            $args[] = $classes[MOD]['Level'];
+        } elseif ($effectiveClass === $classes[FORUM_MOD]['Level']) {
+            $cond[] = 'Level >= ?';
+            $args[] = $classes[FORUM_MOD]['Level'];
         }
 
         return $this->db->scalar("
             SELECT count(*)
             FROM staff_pm_conversations
-            WHERE " . implode(' AND ', $conditions), ...$params);
+            WHERE " . implode(' AND ', $cond), ...$args);
+    }
+
+    public function userStaffPmList(int $viewerId): array {
+        $this->db->prepared_query("
+            SELECT spc.ID   AS pm_id,
+                spc.Subject AS subject,
+                spc.Status  AS status,
+                if(spc.Level = (SELECT max(Level) FROM permissions),
+                    p.Name,
+                    concat(coalesce(p.Name, 'First Line Support'), '+')
+                )                  AS reader,
+                spc.AssignedToUser AS assigned_to_user,
+                spc.Date           AS date,
+                spc.ResolverID     AS resolver_id,
+                count(spm.ID) - 1  AS replies
+            FROM staff_pm_conversations AS spc
+            INNER JOIN staff_pm_messages spm ON (spm.ConvID = spc.ID)
+            LEFT JOIN permissions p USING (Level)
+            WHERE spc.UserID = ?
+                AND (spc.Level <= ? OR spc.AssignedToUser = ?)
+            GROUP BY spc.ID
+            ORDER BY spc.Date DESC
+            ", $this->user->id(), $this->user->effectiveClass(), $viewerId
+        );
+        return $this->db->to_array(false, MYSQLI_ASSOC, false);
     }
 }
