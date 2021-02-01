af63e4edd2b079b47385a4aac1c12505a803888c
Spine <spine@orpheus.network>
2021-02-01 18:12:08 +0000

    remove geoip_country and associated code, since MaxMind licensing has changed

---
+++ db/migrations/20210201180413_nuke_geoip.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class NukeGeoip extends AbstractMigration
+{
+    public function up(): void {
+        $this->table('geoip_country')->drop()->update();
+        $this->execute("DELETE FROM periodic_task WHERE classname = 'UpdateGeoip'");
+    }
+
+    public function down(): void {
+        $this->table('geoip_country', [
+                'id' => false,
+                'primary_key' => ['StartIP', 'EndIP'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8',
+                'collation' => 'utf8_general_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('StartIP', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'signed' => false,
+            ])
+            ->addColumn('EndIP', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'signed' => false,
+            ])
+            ->addColumn('Code', 'string', [
+                'null' => false,
+                'limit' => 2,
+                'collation' => 'utf8_general_ci',
+                'encoding' => 'utf8',
+            ])
+            ->create();
+
+        $this->table('periodic_task')
+             ->insert([
+                'name' => 'Update Geoip',
+                'classname' => 'UpdateGeoip',
+                'description' => 'Updates geoip distributions',
+                'period' => 60 * 60 * 24,
+                'is_enabled' => false
+            ])->save();
+    }
+}
