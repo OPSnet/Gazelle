0e4feb2bb6885c73f2a58e392ce40a7bf32cb68d
Spine <spine@orpheus.network>
2021-03-07 12:18:38 +0000

    Twigify ocelot info and nuke $HeavyInfo usage

--- sections/tools/data/ocelot_info.php
+++ sections/tools/data/ocelot_info.php
@@ -3,61 +3,9 @@ if (!check_perms('users_mod')) {
     error(403);
 }
 
-if (isset($_GET['userid']) && is_number($_GET['userid'])) {
-    $UserHeavyInfo = Users::user_heavy_info($_GET['userid']);
-    if (isset($UserHeavyInfo['torrent_pass'])) {
-        $TorrentPass = $UserHeavyInfo['torrent_pass'];
-        $UserPeerStats = Tracker::user_peer_count($TorrentPass);
-        $UserInfo = Users::user_info($_GET['userid']);
-        $Classes = (new Gazelle\Manager\User)->classList();
-        $UserLevel = $Classes[$UserInfo['PermissionID']]['Level'];
-        if (!check_paranoia('leeching+', $UserInfo['Paranoia'], $UserLevel, $_GET['userid'])) {
-            $UserPeerStats[0] = false;
-        }
-        if (!check_paranoia('seeding+', $UserInfo['Paranoia'], $UserLevel, $_GET['userid'])) {
-            $UserPeerStats[1] = false;
-        }
-    } else {
-        $UserPeerStats = false;
-    }
-} else {
-    $MainStats = Tracker::info();
-}
-
-View::show_header('Tracker info');
-?>
-<div class="thin">
-    <div class="header">
-        <h2>Tracker info</h2>
-    </div>
-    <div class="linkbox">
-        <a href="?action=<?=$_REQUEST['action']?>" class="brackets" />Main stats</a>
-    </div>
-    <div class="sidebar">
-        <div class="box box2">
-            <div class="head"><strong>User stats</strong></div>
-            <div class="pad">
-                <form method="get" action="">
-                    <input type="hidden" name="action" value="ocelot_info" />
-                    <span class="label">Get stats for user</span><br />
-                    <input type="text" name="userid" placeholder="User ID" value="<?php Format::form('userid'); ?>" />
-                    <input type="submit" value="Go" />
-                </form>
-            </div>
-        </div>
-    </div>
-    <div class="main_column">
-        <div class="box box2">
-            <div class="head"><strong>Numbers and such</strong></div>
-            <div class="pad">
-<?php
-if (!empty($UserPeerStats)) {
-?>
-                User ID: <?=$_GET['userid']?><br />
-                Leeching: <?=$UserPeerStats[0] === false ? "hidden" : number_format($UserPeerStats[0])?><br />
-                Seeding: <?=$UserPeerStats[1] === false ? "hidden" : number_format($UserPeerStats[1])?><br />
-<?php
-} elseif (!empty($MainStats)) {
+if (!isset($_GET['userid'])) {
+    $MainStats = Tracker::info() ?? [];
+    $main = [];
     foreach ($MainStats as $Key => $Value) {
         if (is_numeric($Value)) {
             if (substr($Key, 0, 6) === "bytes ") {
@@ -67,27 +15,26 @@ if (!empty($UserPeerStats)) {
                 $Value = number_format($Value);
             }
         }
-?>
-                <?="$Value $Key<br />\n"?>
-<?php
+        $main[$Key] = $Value;
     }
-} elseif (isset($TorrentPass)) {
-?>
-                Failed to get stats for user <?=$_GET['userid']?>
-<?php
-} elseif (isset($_GET['userid'])) {
-?>
-                User <?=display_str($_GET['userid'])?> doesn't exist
-<?php
 } else {
-?>
-                Failed to get tracker info
-<?php
+    $user = (new Gazelle\Manager\User)->find($_GET['userid']);
+    if (!$user) {
+        $UserPeerStats = false;
+    } else {
+    echo $user->username();
+        $TorrentPass = $user->announceKey();
+        $UserPeerStats = Tracker::user_peer_count($TorrentPass);
+        $_GET['userid'] = $user->id();
+    }
 }
-?>
-            </div>
-        </div>
-    </div>
-</div>
-<?php
+
+View::show_header('Tracker info');
+echo G::$Twig->render('admin/tracker-info.twig', [
+    'action'       => $_REQUEST['action'],
+    'announce_key' => $TorrentPass,
+    'main_stats'   => $MainStats ?? null,
+    'peer_stats'   => $UserPeerStats ?? null,
+    'user_id'      => $_GET['userid'] ?? null,
+]);
 View::show_footer();
