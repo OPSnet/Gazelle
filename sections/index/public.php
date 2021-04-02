b601c80e47a4900dffc878089eb03a3d939db512
Spine <spine@orpheus.network>
2021-04-02 21:42:33 +0000

    twigify public homepage

--- sections/index/public.php
+++ sections/index/public.php
@@ -4,41 +4,5 @@ if (!SHOW_PUBLIC_INDEX) {
     exit;
 }
 View::show_header();
-?>
-
-<div id="logo">
-<img src="<?= STATIC_SERVER ?>/styles/public/images/loginlogo.png" alt="Orpheus Network" title="Orpheus Network" />
-</div>
-
-<div class="main">
-<div class="para">Orpheus with his lute made trees<br />
-And the mountain tops that freeze<br />
-Bow themselves when he did sing:<br />
-To his music plants and flowers<br />
-Ever sprung; as sun and showers<br />
-There had made a lasting spring.</div>
-
-<div class="para">Every thing that heard him play,<br />
-Even the billows of the sea,<br />
-Hung their heads and then lay by.<br />
-In sweet music is such art,<br />
-Killing care and grief of heart<br />
-Fall asleep, or hearing, die.</div>
-</div>
-
-<div class="actions">
-    <span class="action-bar">
-    <a href="/login.php">Enter</a>
-<?php if (OPEN_REGISTRATION) { ?>
-    <a title="Obtain an account by supplying a valid email address" href="register.php">Register</a>
-<?php } ?>
-<?php if (OPEN_EXTERNAL_REFERRALS) { ?>
-    <a title="Obtain an account by proving your membership on a site we trust" href="referral.php">Referral</a>
-<?php } ?>
-<?php if (RECOVERY) { ?>
-    <a title="Obtain a new account by proving your membership on the previous site" href="recovery.php">Recovery</a>
-<?php } ?>
-    </span>
-</div>
-<?php
+echo G::$Twig->render('index/public.twig');
 View::show_footer();
