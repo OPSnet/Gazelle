32df179ecccc472cb47205ab8b2506a32edc5a6e
Spine <spine@orpheus.network>
2021-04-30 16:42:10 +0000

    cleaner twig rendering of invite tree

--- sections/user/invitetree.php
+++ sections/user/invitetree.php
@@ -8,28 +8,14 @@ if (!isset($_GET['userid'])) {
     }
     $userId = (int)$_GET['userid'];
 }
-
-// Cannot use Users::user_info() because an inexistent ID will return 'Unknown'
-[$userId, $Username] = $DB->row("
-    SELECT ID, Username
-    FROM users_main
-    WHERE ID = ?
-    ", $userId
-);
-if (!$userId) {
+$user = (new Gazelle\Manager\User)->findById($userId);
+if (is_null($user)) {
     error(404);
 }
 
-$tree = new Gazelle\InviteTree($userId);
-View::show_header($Username.' &rsaquo; Invites &rsaquo; Tree');
-?>
-<div class="thin">
-    <div class="header">
-        <h2><?=Users::format_username($userId, false, false, false)?> &rsaquo; <a href="user.php?action=invite&amp;userid=<?=$userId?>">Invites</a> &rsaquo; Tree</h2>
-    </div>
-    <div class="box pad">
-        <?= $tree->render($Twig) ?>
-    </div>
-</div>
-<?php
+View::show_header($user->username() . ' &rsaquo; Invites &rsaquo; Tree');
+echo $Twig->render('user/invite-tree.twig', array_merge(
+    ['user' => $user],
+    (new Gazelle\InviteTree($userId))->details(),
+));
 View::show_footer();
