af11548e1b42a1fcb2fa97b2f87e4dfe81755bdc
Spine <spine@orpheus.network>
2021-05-08 10:58:26 +0000

    twigify wiki create/edit

--- sections/wiki/edit.php
+++ sections/wiki/edit.php
@@ -4,43 +4,23 @@ $ArticleID = (int)$_GET['id'];
 if (!$ArticleID) {
     error(404);
 }
-
+$Viewer = new Gazelle\User($LoggedUser['ID']);
 $wikiMan = new Gazelle\Manager\Wiki;
-[$Revision, $Title, $Body, $Read, $Edit, $Date] = $wikiMan->article($ArticleID);
-if ($Edit > $LoggedUser['EffectiveClass']) {
+[$Revision, $Title, $Body, $Read, $Edit] = $wikiMan->article($ArticleID);
+if ($Edit > $Viewer->effectiveClass()) {
     error('You do not have access to edit this article.');
 }
 
-View::show_header('Edit '.$Title);
-?>
-<div class="thin">
-    <div class="box pad">
-        <form class="edit_form" name="wiki_article" action="wiki.php" method="post">
-            <input type="hidden" name="action" value="edit" />
-            <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
-            <input type="hidden" name="id" value="<?=$ArticleID?>" />
-            <input type="hidden" name="revision" value="<?=$Revision?>" />
-            <div>
-                <h3>Title</h3>
-                <input type="text" name="title" size="92" maxlength="100" value="<?=$Title?>" />
-                <h3>Body </h3>
-<?php
-    $ReplyText = new TEXTAREA_PREVIEW('body', 'body', $Body, 91, 22, true, false);
-
-    if (check_perms('admin_manage_wiki')) {
-?>
-                <h3>Access</h3>
-                <p>There are some situations in which the viewing or editing of an article should be restricted to a certain class.</p>
-                <strong>Restrict read:</strong> <select name="minclassread"><?=class_list($Read)?></select>
-                <strong>Restrict edit:</strong> <select name="minclassedit"><?=class_list($Edit)?></select>
-<?php    } ?>
-                <div style="text-align: center;">
-                    <input type="button" value="Preview" class="hidden button_preview_<?=$ReplyText->getID()?>" tabindex="1" />
-                    <input type="submit" value="Submit" />
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
-<?php
+View::show_header('Edit ' . $Title);
+echo $Twig->render('wiki/article.twig', [
+    'action'     => 'edit',
+    'body'       => new Gazelle\Util\Textarea('body', $Body, 92, 20),
+    'class_list' => (new Gazelle\Manager\User)->classList(),
+    'edit'       => $Edit,
+    'read'       => $Read,
+    'id'         => $ArticleID,
+    'revision'   => $Revision,
+    'title'      => $Title,
+    'viewer'     => $Viewer,
+]);
 View::show_footer();
