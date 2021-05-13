7026f7ea01ec9074bf8735d564bdb2fdfe30ba4f
Spine <spine@orpheus.network>
2021-05-13 20:10:41 +0200

    nuke Views::parse() and obsolete textarea rendering functionality

--- classes/view.class.php
+++ classes/view.class.php
@@ -57,44 +57,4 @@ class View {
             require_once('../design/privatefooter.php');
         }
     }
-
-    /**
-     * This method simply renders a PHP file (PHTML) with the supplied variables.
-     *
-     * All files must be placed within {self::IncludePath}. Create and organize
-     * new paths and files. (e.g.: /design/views/artist/, design/view/forums/, etc.)
-     *
-     * @static
-     * @param string  $TemplateFile A relative path to a PHTML file
-     * @param array   $Variables Assoc. array of variables to extract for the template
-     * @param boolean $Buffer enables Output Buffer
-     * @return boolean|string
-     *
-     * @example <pre><?php
-     *  // box.phtml
-     *  <p id="<?=$id?>">Data</p>
-     *
-     *  // The variable $id within box.phtml will be filled by $some_id
-     *    View::parse('section/box.phtml', array('id' => $some_id));
-     *
-     *  // Parse a template without outputing it
-     *  $SavedTemplate = View::parse('sec/tion/eg.php', $DataArray, true);
-     *  // later . . .
-     *  echo $SavedTemplate; // Output the buffer
-     * </pre>
-     */
-    public static function parse($TemplateFile, array $Variables = [], $Buffer = false) {
-        $Template = self::IncludePath . $TemplateFile;
-        if (file_exists($Template)) {
-            extract($Variables);
-            if ($Buffer) {
-                ob_start();
-                include $Template;
-                $Content = ob_get_contents();
-                ob_end_clean();
-                return $Content;
-            }
-            return include $Template;
-        }
-    }
 }
