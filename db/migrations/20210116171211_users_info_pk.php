90216ce7eb54572d9db99dc8db368038a4b979e9
itismadness <itismadness@orpheus.network>
2021-05-13 21:05:18 +0000

    Fix users pk migration to work on docker

--- db/migrations/20210116171211_users_info_pk.php
+++ db/migrations/20210116171211_users_info_pk.php
@@ -3,14 +3,25 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
+// This migration is dealing with an issue in production with the schema
+// having a unique key constraint, but was lacking a primary key. This
+// does not exist though within the schema created from this repo, so we
+// have to work around that.
 final class UsersInfoPk extends AbstractMigration
 {
     public function up(): void {
-        $this->execute("
-            ALTER TABLE users_info
-                DROP KEY UserID,
-                ADD PRIMARY KEY (UserID)
-        ");
+        $alter = [];
+        $table = $this->table('users_info');
+        if ($table->hasIndex('UserID')) {
+            $alter[] = 'DROP KEY UserID';
+        }
+        if (!$table->getAdapter()->hasPrimaryKey($table->getName(), 'UserID')) {
+            $alter[] = 'ADD PRIMARY KEY (UserID)';
+        }
+
+        if (count($alter) > 0) {
+            $this->execute("ALTER TABLE users_info " . implode(', ', $alter));
+        }
     }
 
     public function down(): void {
