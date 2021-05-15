a4cfde03a258ef3ae72892f53f1d45813192ffd2
Spine <spine@orpheus.network>
2021-05-15 10:48:52 +0000

    remove postid arg from paginators

--- app/CommentViewer/Artist.php
+++ app/CommentViewer/Artist.php
@@ -3,10 +3,9 @@
 namespace Gazelle\CommentViewer;
 
 class Artist extends \Gazelle\CommentViewer {
-
-    public function __construct(\Twig\Environment $twig, int $viewerId, int $artistId) {
-        parent::__construct($twig, $viewerId);
-        $this->baseLink = "artist.php?id={$artistId}&postid=%d#post%d";
+    public function __construct(int $viewerId, int $artistId) {
+        parent::__construct($viewerId);
+        $this->baseLink = "artist.php?id={$artistId}";
         $this->page     = 'artist';
     }
 }
