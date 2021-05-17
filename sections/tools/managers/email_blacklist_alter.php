00517c86239ef91eb9d85894070773b09eb55706
Spine <spine@orpheus.network>
2021-05-17 21:42:18 +0000

    twigify email blacklist toolbox

--- sections/tools/managers/email_blacklist_alter.php
+++ sections/tools/managers/email_blacklist_alter.php
@@ -4,45 +4,36 @@ if (!check_perms('users_view_email')) {
 }
 
 authorize();
+$emailBlacklist = new Gazelle\Manager\EmailBlacklist;
 
 if ($_POST['submit'] === 'Delete') { // Delete
-    if (!is_number($_POST['id']) || $_POST['id'] === '') {
+    if (!$emailBlacklist->remove((int)$_POST['id'])) {
         error(0);
     }
-    $DB->prepared_query("
-        DELETE FROM email_blacklist
-        WHERE ID = ?
-        ", (int)$_POST['id']
-    );
 } else { // Edit & Create, Shared Validation
-    $Val = new Gazelle\Util\Validator;
-    $Val->setField('email', '1', 'string', 'The email must be set', ['minlength'=>1]);
-    $Val->setField('comment', '0', 'string', 'The description has a max length of 255 characters', ['maxlength'=>255]);
-    if (!$Val->validate($_POST)) {
-        error($Val->errorMessage());
+    $validator = new Gazelle\Util\Validator;
+    $validator->setField('email', '1', 'string', 'The email must be set', ['minlength'=>6]);
+    $validator->setField('comment', '0', 'string', 'The description has a max length of 255 characters', ['maxlength'=>255]);
+    if (!$validator->validate($_POST)) {
+        error($validator->errorMessage());
     }
 
     if ($_POST['submit'] === 'Edit') { // Edit
-        if (!is_number($_POST['id']) || $_POST['id'] === '') {
+        if (!$emailBlacklist->modify((int)$_POST['id'], [
+            'email'   => trim($_POST['email']),
+            'comment' => trim($_POST['comment']),
+            'user_id' => $LoggedUser['ID'],
+        ])) {
             error(0);
         }
-        $DB->prepared_query("
-            UPDATE email_blacklist SET
-                Email   = ?,
-                Comment = ?,
-                UserID  = ?,
-                Time    = now()
-            WHERE ID = ?
-            ", trim($_POST['email']), trim($_POST['comment']), $LoggedUser['ID'],
-                $_POST['id']
-        );
     } else { // Create
-        $DB->prepared_query("
-            INSERT INTO email_blacklist
-                   (Email, Comment, UserID)
-            VALUES (?,     ?,       ?)
-            ", trim($_POST['email']), trim($_POST['comment']), $LoggedUser['ID']
-        );
+        if (!$emailBlacklist->create([
+            'email'   => trim($_POST['email']),
+            'comment' => trim($_POST['comment']),
+            'user_id' => $LoggedUser['ID'],
+        ])) {
+            error(0);
+        }
     }
 }
 
