e7abebd552b81b80ad6376ed9e073ac10b1296cf
Spine <spine@orpheus.network>
2021-06-12 17:56:56 +0000

    better nullness in torrents table

---
+++ db/migrations/20210612171704_torrent_not_null.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+/* see comment in db/migrations/20200219075508_drop_secret.php */
+
+final class TorrentNotNull extends AbstractMigration
+{
+    public function up(): void {
+        if (!getenv('LOCK_MY_DATABASE')) {
+            die("Migration cannot proceed, use the source: " . __FILE__ . "\n");
+        }
+        $this->table('torrents')
+            ->changeColumn('UserID', 'integer', ['length' => 10, 'null' => false])
+            ->changeColumn('time', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->removeColumn('TranscodedFrom')
+            ->save();
+    }
+
+    public function down(): void {
+        if (!getenv('LOCK_MY_DATABASE')) {
+            die("Migration cannot proceed, use the source: " . __FILE__ . "\n");
+        }
+        $this->table('torrents')
+            ->changeColumn('UserID', 'integer', ['length' => 10, 'null' => true])
+            ->changeColumn('time', 'datetime', ['null' => true, 'default' => null])
+            ->addColumn('TranscodedFrom', 'integer', ['limit' => 10, 'default' => 0])
+            ->save();
+    }
+}
