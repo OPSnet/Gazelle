8f9cfd75169f5a297263ef4cbdabe46416c4cd89
Spine <spine@orpheus.network>
2021-07-06 12:40:19 +0000

    paginate xbt log

--- classes/config.template.php
+++ classes/config.template.php
@@ -191,6 +191,7 @@ define('COLLAGES_PER_PAGE', 25);
 define('CONTEST_ENTRIES_PER_PAGE', 50);
 define('FRIENDS_PER_PAGE', 20);
 define('INVITES_PER_PAGE', 50);
+define('ITEMS_PER_PAGE', 50);
 define('IPS_PER_PAGE', 50);
 define('LOG_ENTRIES_PER_PAGE', 50);
 define('MESSAGES_PER_PAGE', 25);
