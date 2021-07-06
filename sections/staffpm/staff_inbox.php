07b7cdedd8bdc6bcaac8133c194ac4ad70039ea6
Spine <spine@orpheus.network>
2021-07-06 16:34:31 +0000

    paginate staff pms

--- sections/staffpm/staff_inbox.php
+++ sections/staffpm/staff_inbox.php
@@ -1,120 +1,69 @@
 <?php
 
-View::show_header('Staff Inbox');
+$View = empty($_GET['view']) ? '' : display_str($_GET['view']);
 
-$View = display_str(empty($_GET['view']) ? '' : $_GET['view']);
-
-// Setup for current view mode
-$SortStr = 'IF(AssignedToUser = ' . $Viewer->id() . ', 0, 1) ASC, ';
-switch ($View) {
-    case 'unanswered':
-        $ViewString = 'Unanswered';
-        $Status = "Unanswered";
-        break;
-    case 'open':
-        $ViewString = 'Unresolved';
-        $Status = "Open', 'Unanswered";
-        $SortStr = '';
-        break;
-    case 'resolved':
-        $ViewString = 'Resolved';
-        $Status = "Resolved";
-        $SortStr = '';
-        break;
-    case 'my':
-        $ViewString = 'Your Unanswered';
-        $Status = "Unanswered";
-        break;
-    default:
-        $Status = "Unanswered";
-        if ($Viewer->isStaff()) {
-            $ViewString = 'Your Unanswered';
-        } else {
-            // FLS
-            $ViewString = 'Unanswered';
-        }
-        break;
+$staffpmMan = new Gazelle\Manager\StaffPM;
+$viewMap = [
+    'unanswered' => [
+        'view'   => 'Unanswered',
+        'status' => ['Unanswered'],
+        'count'  => $staffpmMan->countByStatus($Viewer, ['Unanswered']),
+    ],
+    'open' => [
+        'view'    => 'Unresolved',
+        'status'  => ['Open', 'Unanswered'],
+        'count'   => $staffpmMan->countByStatus($Viewer, ['Open', 'Unanswered']),
+        'no_sort' => true,
+    ],
+    'resolved' => [
+        'view'    => 'Resolved',
+        'status'  => ['Resolved'],
+        'no_sort' => true,
+    ],
+    'my' => [
+        'view'   => 'Your Unanswered',
+        'status' => ['Unanswered'],
+    ],
+    '' => [
+        'view'   => $Viewer->isStaff() ? 'Your Unanswered' : 'Unanswered',
+        'status' => ['Unanswered'],
+    ],
+];
+if (!isset($viewMap[$View])) {
+    error(0);
 }
-$UserLevel = $Viewer->effectiveClass();
-$WhereCondition = "
-    WHERE (spc.Level <= $UserLevel OR spc.AssignedToUser = '".$Viewer->id()."')
-        AND spc.Status IN ('$Status')";
+$cond = ['spc.Status IN (' . placeholders($viewMap[$View]['status']) . ') AND (spc.Level <= ? OR spc.AssignedToUser = ?)'];
+$args = array_merge($viewMap[$View]['status'], [$Viewer->effectiveClass(), $Viewer->id()]);
 
 $Classes = (new Gazelle\Manager\User)->classList();
-if ($ViewString == 'Your Unanswered') {
-    if ($UserLevel >= $Classes[MOD]['Level']) {
-        $WhereCondition .= " AND spc.Level >= " . $Classes[MOD]['Level'];
-    } else if ($UserLevel == $Classes[FORUM_MOD]['Level']) {
-        $WhereCondition .= " AND spc.Level >= " . $Classes[FORUM_MOD]['Level'];
+if ($viewMap[$View]['view'] === 'Your Unanswered') {
+    if ($Viewer->effectiveClass() >= $Classes[MOD]['Level']) {
+        $cond[] = 'spc.Level >= ?';
+        $args[] = $Classes[MOD]['Level'];
+    } else if ($Viewer->effectiveClass() == $Classes[FORUM_MOD]['Level']) {
+        $cond[] = 'spc.Level >= ?';
+        $args[] = $Classes[FORUM_MOD]['Level'];
     }
 }
+$where = implode(' AND ', $cond);
 
-$Sections = [
-    'unanswered' => "All unanswered",
-    'open' => "Unresolved",
-    'resolved' => 'Resolved',
-];
-if ($Viewer->isStaff()) {
-    $Sections = ['' => 'Your unanswered'] + $Sections;
-}
-
-// Start page
-?>
-<div class="thin">
-    <div class="header">
-        <h2><?=$ViewString?> Staff PMs</h2>
-        <div class="linkbox">
-<?php
-foreach ($Sections as $Section => $Text) {
-    if ($Section == 'unanswered') {
-        $Text .= '(' . $DB->scalar("
-            SELECT count(*)
-            FROM staff_pm_conversations
-            WHERE Status IN ('Unanswered')
-                AND (Level <= ? OR AssignedToUser = ?)
-            ", $UserLevel, $Viewer->id()
-        ) . ')';
-    } elseif ($Section == 'open') {
-        $Text .= '(' . $DB->scalar("
-            SELECT count(*)
-            FROM staff_pm_conversations
-            WHERE Status IN ('Open', 'Unanswered')
-                AND (Level <= ? OR AssignedToUser = ?)
-            ", $UserLevel, $Viewer->id()
-        ) . ')';
-    }
-    if ($Section == $View) {
-        $Text = "<strong>$Text</strong>";
-    }
-
-    $Section = ($Section) ? "?view=$Section" : '';
-
-    // Make sure the trailing space in this output remains.
-    ?><a href="staffpm.php<?= $Section ?>" class="brackets"><?= $Text ?></a>&nbsp;<?php
-}
-
-if (check_perms('admin_staffpm_stats')) { ?>
-            <a href="staffpm.php?action=scoreboard&amp;view=user" class="brackets">View user scoreboard</a>
-            <a href="staffpm.php?action=scoreboard&amp;view=staff" class="brackets">View staff scoreboard</a>
-<?php
-}
-    if ($Viewer->isFLS()) { ?>
-            <span class="tooltip" title="This is the inbox where replies to Staff PMs you have sent are."><a href="staffpm.php?action=userinbox" class="brackets">Personal Staff Inbox</a></span>
-<?php    } ?>
-        </div>
-    </div>
-    <br />
-    <br />
-<?php
-[$Page, $Limit] = Format::page_limit(MESSAGES_PER_PAGE);
-
-$NumResults = $DB->scalar("
+$paginator = new Gazelle\Util\Paginator(MESSAGES_PER_PAGE, (int)($_GET['page'] ?? 1));
+$paginator->setTotal($DB->scalar("
     SELECT count(*)
     FROM staff_pm_conversations AS spc
     LEFT JOIN staff_pm_messages spm ON (spm.ConvID = spc.ID)
-    $WhereCondition
-");
-// Get messages
+    WHERE $where
+    ", ...$args
+));
+
+if (isset($viewMap[$View]['no_sort'])) {
+    $orderBy = 'spc.Date DESC';
+} else {
+    $orderBy = 'IF(AssignedToUser = ?, 0, 1) ASC, spc.Date DESC';
+    $args[] = $Viewer->id();
+}
+array_push($args, $paginator->limit(), $paginator->offset());
+
 $StaffPMs = $DB->prepared_query("
     SELECT spc.ID,
         spc.Subject,
@@ -128,33 +77,70 @@ $StaffPMs = $DB->prepared_query("
         spc.ResolverID
     FROM staff_pm_conversations AS spc
     LEFT JOIN staff_pm_messages spm ON (spm.ConvID = spc.ID)
-    $WhereCondition
+    WHERE $where
     GROUP BY spc.ID
-    ORDER BY $SortStr spc.Date DESC
-    LIMIT $Limit
-");
+    ORDER BY $orderBy
+    LIMIT ? OFFSET ?
+    ", ...$args
+);
 
-$Pages = Format::get_pages($Page, $NumResults, MESSAGES_PER_PAGE, 9);
+$Sections = [
+    'unanswered' => "All unanswered",
+    'open'       => "Unresolved",
+    'resolved'   => 'Resolved',
+];
+if ($Viewer->isStaff()) {
+    $Sections = ['' => 'Your unanswered'] + $Sections;
+}
+$viewingResolved = $viewMap[$View]['view'] === 'Resolved';
+
+View::show_header('Staff Inbox');
 ?>
-    <div class="linkbox">
-        <?=$Pages?>
+<div class="thin">
+    <div class="header">
+        <h2><?= $viewMap[$View]['view'] ?> Staff PMs</h2>
+        <div class="linkbox">
+<?php
+foreach ($Sections as $Section => $Text) {
+    if (in_array($Section, ['unanswered', 'open'])) {
+        $Text .= '(' . $viewMap[$Section]['count'] . ')';
+    }
+    if ($Section === $View) {
+        $Text = "<strong>$Text</strong>";
+    }
+    // Make sure the trailing space in this output remains.
+    ?><a href="staffpm.php<?= $Section ? "?view=$Section" : '' ?>" class="brackets"><?= $Text ?></a>&nbsp;<?php
+}
+
+if ($Viewer->permitted('admin_staffpm_stats')) {
+?>
+            <a href="staffpm.php?action=scoreboard&amp;view=user" class="brackets">View user scoreboard</a>
+            <a href="staffpm.php?action=scoreboard&amp;view=staff" class="brackets">View staff scoreboard</a>
+<?php
+}
+if ($Viewer->isFLS()) { ?>
+            <span class="tooltip" title="This is the inbox where replies to Staff PMs you have sent are."><a href="staffpm.php?action=userinbox" class="brackets">Personal Staff Inbox</a></span>
+<?php } ?>
+        </div>
     </div>
+    <br />
+    <?= $paginator->linkbox() ?>
     <div class="box pad" id="inbox">
 <?php if (!$DB->has_results()) { ?>
         <h2>No messages</h2>
 <?php
 } else {
     // Messages, draw table
-    if ($ViewString != 'Resolved' && $Viewer->isStaff()) {
+    if (!$viewingResolved && $Viewer->isStaff()) {
         // Open multiresolve form
 ?>
         <form class="manage_form" name="staff_messages" method="post" action="staffpm.php" id="messageform">
             <input type="hidden" name="action" value="multiresolve" />
             <input type="hidden" name="view" value="<?=strtolower($View)?>" />
 <?php } ?>
-            <table class="message_table<?=($ViewString != 'Resolved' && $Viewer->isStaff()) ? ' checkboxes' : '' ?>">
+            <table class="message_table<?=(!$viewingResolved && $Viewer->isStaff()) ? ' checkboxes' : '' ?>">
                 <tr class="colhead">
-<?php     if ($ViewString != 'Resolved' && $Viewer->isStaff()) { ?>
+<?php     if (!$viewingResolved && $Viewer->isStaff()) { ?>
                     <td width="10"><input type="checkbox" onclick="toggleChecks('messageform', this);" /></td>
 <?php     } ?>
                     <td>Subject</td>
@@ -162,7 +148,7 @@ $Pages = Format::get_pages($Page, $NumResults, MESSAGES_PER_PAGE, 9);
                     <td>Date</td>
                     <td>Assigned to</td>
                     <td>Replies</td>
-<?php    if ($ViewString == 'Resolved') { ?>
+<?php    if ($viewingResolved) { ?>
                     <td>Resolved by</td>
 <?php    } ?>
                 </tr>
@@ -186,7 +172,7 @@ $Pages = Format::get_pages($Page, $NumResults, MESSAGES_PER_PAGE, 9);
         }
 ?>
                 <tr class="row<?= $Row ?>">
-<?php         if ($ViewString != 'Resolved' && $Viewer->isStaff()) { ?>
+<?php         if (!$viewingResolved && $Viewer->isStaff()) { ?>
                     <td class="center"><input type="checkbox" name="id[]" value="<?=$ID?>" /></td>
 <?php         } ?>
                     <td><a href="staffpm.php?action=viewconv&amp;id=<?=$ID?>"><?=display_str($Subject)?></a></td>
@@ -194,7 +180,7 @@ $Pages = Format::get_pages($Page, $NumResults, MESSAGES_PER_PAGE, 9);
                     <td><?= time_diff($Date, 2, true) ?></td>
                     <td><?= $Assigned ?></td>
                     <td><?= max(0, $NumReplies - 1) ?></td>
-<?php        if ($ViewString == 'Resolved') { ?>
+<?php        if ($viewingResolved) { ?>
                     <td><?= Users::format_username($ResolverID, true, true, true, true) ?></td>
 <?php        } ?>
                 </tr>
@@ -203,7 +189,7 @@ $Pages = Format::get_pages($Page, $NumResults, MESSAGES_PER_PAGE, 9);
     } //while
 ?>
             </table>
-<?php     if ($ViewString != 'Resolved' && $Viewer->isStaff()) { ?>
+<?php     if (!$viewingResolved && $Viewer->isStaff()) { ?>
             <div class="submit_div">
                 <input type="submit" value="Resolve selected" />
             </div>
@@ -213,9 +199,7 @@ $Pages = Format::get_pages($Page, $NumResults, MESSAGES_PER_PAGE, 9);
 } // $DB->has_results()
 ?>
     </div>
-    <div class="linkbox">
-        <?=$Pages?>
-    </div>
+    <?= $paginator->linkbox() ?>
 </div>
 <?php
 View::show_footer();
