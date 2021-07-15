4a2c6d662bdab1047cdd57bcf455ae9cf2d799ec
Spine <spine@orpheus.network>
2021-07-15 16:17:07 +0000

    paginate ajax inbox

--- sections/ajax/inbox/inbox.php
+++ sections/ajax/inbox/inbox.php
@@ -7,6 +7,7 @@ if (!empty($_GET['search'])) {
 }
 
 $json->setVersion(1)
+    ->setPage((int)($_GET['page'] ?? 1))
     ->setUnreadFirst(($_GET['sort'] ?? 'unread') === 'unread')
     ->setFolder($_GET['type'] ?? 'inbox')
     ->setViewerId($Viewer->id())
