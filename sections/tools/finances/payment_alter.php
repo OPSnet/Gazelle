3441d400505edc1b883030cdd8cb59146a2846aa
Spine <spine@orpheus.network>
2021-07-19 16:46:03 +0000

    twigify payment reminders

--- sections/tools/finances/payment_alter.php
+++ sections/tools/finances/payment_alter.php
@@ -1,5 +1,5 @@
 <?php
-if (!check_perms('admin_manage_payments')) {
+if (!$Viewer->permitted('admin_manage_payments')) {
     error(403);
 }
 $Payment = new \Gazelle\Manager\Payment;
@@ -10,15 +10,15 @@ if ($_POST['submit'] == 'Delete') {
     }
     $Payment->remove($_POST['id']);
 } else {
-    $Val = new Gazelle\Util\Validator;
-    $Val->setFields([
+    $Validator = new Gazelle\Util\Validator;
+    $Validator->setFields([
         ['text', '1', 'string', 'The payment text must be set, and has a max length of 100 characters', ['maxlength' => 100]],
         ['rent', '1', 'number', 'Rent must be zero or positive)', ['min' => 0, 'allowperiod' => true]],
         ['cc', '1', 'regex', 'The currency code must follow the ISO-4217 standard', ['regex' => '/^(XBT|EUR|USD)$/']],
         ['expiry', '1', 'regex', 'The expiry must be a date in the form of YYYY-MM-DD', ['regex' => '/^\d{4}-\d{2}-\d{2}$/']],
     ]);
-    if (!$Val->validate($_POST)) {
-        $Err = $Validate->errorMessage();
+    if (!$Validator->validate($_POST)) {
+        $Err = $Validator->errorMessage();
         require_once('payment_list.php');
         exit;
     }
@@ -28,7 +28,7 @@ if ($_POST['submit'] == 'Delete') {
         'expiry' => $_POST['expiry'],
         'rent'   => $_POST['rent'],
         'cc'     => $_POST['cc'],
-        'active' => $_POST['active'] == 'on' ? 1 : 0,
+        'active' => isset($_POST['active']) && $_POST['active'] == 'on' ? 1 : 0,
     ];
     if ($_POST['submit'] == 'Create') {
         $Payment->create($values);
@@ -36,7 +36,6 @@ if ($_POST['submit'] == 'Delete') {
         $Payment->modify($_POST['id'], $values);
     }
 }
-
 $Cache->delete_value('due_payments');
 
 header('Location: tools.php?action=payment_list');
