85eef48ab20e967d9382cdd642f965888dca9a98
Spine <spine@orpheus.network>
2021-07-27 16:37:11 +0000

    paginate ratio watch

--- sections/tools/data/ratio_watch.php
+++ sections/tools/data/ratio_watch.php
@@ -1,23 +1,17 @@
 <?php
-if (!check_perms('site_view_flow')) {
+
+if (!$Viewer->permitted('site_view_flow')) {
     error(403);
 }
 
 $userMan = new Gazelle\Manager\User;
 $paginator = new Gazelle\Util\Paginator(USERS_PER_PAGE, (int)($_GET['page'] ?? 1));
-
-$Results = $userMan->totalRatioWatchUsers();
-$Pages = Format::get_pages($paginator->page(), $Results, $paginator->limit());
-
-$TotalDisabled = $userMan->totalBannedForRatio();
-$Users = $userMan->ratioWatchUsers($paginator->limit(), $paginator->offset());
+$paginator->setTotal($userMan->totalRatioWatchUsers());
 
 View::show_header('Ratio Watch');
-
 echo $Twig->render('admin/ratio-watch.twig', [
-    'total'          => $Results,
-    'total_disabled' => $TotalDisabled,
-    'linkbox'        => $Pages,
-    'users'          => $Users,
+    'paginator'      => $paginator,
+    'total_disabled' => $userMan->totalBannedForRatio(),
+    'users'          => $userMan->ratioWatchUsers($paginator->limit(), $paginator->offset()),
 ]);
 View::show_footer();
