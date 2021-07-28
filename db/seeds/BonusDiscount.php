72c4febc746cbea267d67b2b392f7f0219b14d49
Spine <spine@orpheus.network>
2021-07-28 21:44:21 +0000

    ensure bonus discounts work on a fresh install

---
+++ db/seeds/BonusDiscount.php
@@ -0,0 +1,16 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class BonusDiscount extends AbstractSeed
+{
+    public function run()
+    {
+        $this->table('site_options')->insert([
+            'Name'    => 'bonus-discount',
+            'Value'   => 0,
+            'Comment' => 'Bonus store discount (0 = no discount, 100 = everything free)',
+        ])->save();
+
+    }
+}
