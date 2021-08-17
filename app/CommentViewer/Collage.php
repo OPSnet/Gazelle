b6f3f99cdbf97fe69c3d8e30805d570dfbc80eff
itismadness <itismadness@orpheus.network>
2021-08-17 05:25:41 +0000

    Fix getting edit history of non-forum comments

--- app/CommentViewer/Collage.php
+++ app/CommentViewer/Collage.php
@@ -6,6 +6,6 @@ class Collage extends \Gazelle\CommentViewer {
     public function __construct(int $viewerId, int $collageId) {
         parent::__construct($viewerId);
         $this->baseLink = "collages.php?action=comments&collageid={$collageId}";
-        $this->page     = 'collage';
+        $this->page     = 'collages';
     }
 }
