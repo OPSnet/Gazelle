ee8e2597a445d4c106dc324d20e9338cadb96507
itismadness <itismadness@orpheus.network>
2021-08-17 05:25:41 +0000

    Fix getting edit history of non-forum comments

--- app/CommentViewer/Request.php
+++ app/CommentViewer/Request.php
@@ -6,6 +6,6 @@ class Request extends \Gazelle\CommentViewer {
     public function __construct(int $viewerId, int $requestId) {
         parent::__construct($viewerId);
         $this->baseLink = "requests.php?action=view&id={$requestId}";
-        $this->page     = 'request';
+        $this->page     = 'requests';
     }
 }
--- app/CommentViewer/Torrent.php
+++ app/CommentViewer/Torrent.php
@@ -6,6 +6,6 @@ class Torrent extends \Gazelle\CommentViewer {
     public function __construct(int $viewerId, int $groupId) {
         parent::__construct($viewerId);
         $this->baseLink = "torrents.php?id={$groupId}";
-        $this->page     = 'torrent';
+        $this->page     = 'torrents';
     }
 }
