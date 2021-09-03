93d64db0f84d5de558022bef5149451a97888c80
Spine <spine@orpheus.network>
2021-09-03 19:09:37 +0000

    artists_group.Name must never be null

---
+++ db/migrations/20210903190550_artist_name_not_null.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class ArtistNameNotNull extends AbstractMigration
+{
+    public function up(): void {
+        $this->table('artists_group')
+            ->changeColumn('Name', 'string', ['length' => 200, 'null' => false])
+            ->save();
+    }
+
+    public function down(): void {
+        $this->table('artists_group')
+            ->changeColumn('Name', 'string', ['length' => 200, 'null' => true])
+            ->save();
+    }
+}
