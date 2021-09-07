5c7cb725d6956cd8fb22894334247f297653622e
Spine <spine@orpheus.network>
2021-09-07 21:55:38 +0000

    Add foreign keys to principal forum tables

---
+++ db/migrations/20210907213146_forum_post_foreign_keys.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class ForumPostForeignKeys extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('forums_posts')
+            ->changeColumn('AuthorID', 'integer', ['signed' => false])
+            ->changeColumn('EditedUserID', 'integer', ['signed' => false])
+            ->addForeignKey('TopicID', 'forums_topics', 'ID', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->save();
+    }
+
+    public function down(): void
+    {
+        $this->table('forums_posts')
+            ->dropForeignKey('AuthorID')
+            ->dropForeignKey('TopicID')
+            ->save();
+
+        $this->table('forums_posts')
+            ->changeColumn('AuthorID', 'integer', ['signed' => true])
+            ->changeColumn('EditedUserID', 'integer', ['signed' => true])
+            ->save();
+    }
+}
