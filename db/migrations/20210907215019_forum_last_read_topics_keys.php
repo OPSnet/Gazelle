16983d788eb9768dd017500f8a3ac42c7bed3929
Spine <spine@orpheus.network>
2021-09-07 21:55:38 +0000

    Add foreign keys to principal forum tables

---
+++ db/migrations/20210907215019_forum_last_read_topics_keys.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class ForumLastReadTopicsKeys extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('forums_last_read_topics')
+            ->changeColumn('UserID', 'integer', ['signed' => false])
+            ->addForeignKey('PostID', 'forums_posts', 'ID', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->addForeignKey('TopicID', 'forums_topics', 'ID', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->addForeignKey('UserID', 'users_main', 'ID', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->save();
+    }
+
+    public function down(): void
+    {
+        $this->table('forums_last_read_topics')
+            ->dropForeignKey('PostID')
+            ->dropForeignKey('TopicID')
+            ->dropForeignKey('UserID')
+            ->save();
+
+        $this->table('forums_last_read_topics')
+            ->changeColumn('UserID', 'integer', ['signed' => true])
+            ->save();
+    }
+}
