a102356bbc1025d3a7bbc0886ea56e30e2398020
Spine <spine@orpheus.network>
2021-09-08 20:59:06 +0000

    staff can reset 2fa from user page

--- sections/user/2fa/complete.php
+++ sections/user/2fa/complete.php
@@ -4,6 +4,10 @@ $user = (new Gazelle\Manager\User)->findById((int)($_REQUEST['userid'] ?? 0));
 if (is_null($user)) {
     error(404);
 }
+if ($user->TFAKey()) {
+    error($Viewer->permitted('users_edit_password') ? '2FA is already configured' : 404);
+}
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start(['read_and_close' => true]);
 }
--- sections/user/2fa/remove.php
+++ sections/user/2fa/remove.php
@@ -5,16 +5,19 @@ $user = (new Gazelle\Manager\User)->findById((int)($_GET['userid'] ?? 0));
 if (is_null($user)) {
     error(404);
 }
-$userId = $user->id();
+if (!$user->TFAKey()) {
+    error($Viewer->permitted('users_edit_password') ? 'No 2FA configured' : 404);
+}
 
-if (!check_perms('users_edit_password')) {
+$userId = $user->id();
+if (!$Viewer->permitted('users_edit_password')) {
     if ($userId !== $Viewer->id()) {
         error(403);
     } elseif (empty($_POST['password'])) {
         require_once('confirm.php');
         exit;
     } elseif (!$user->validatePassword($_POST['password'])) {
-        header('Location: user.php?action=2fa&do=confirm&invalid&userid=' . $userId);
+        header('Location: user.php?action=2fa&do=confirm=invalid&userid=' . $userId);
         exit;
     }
 }
