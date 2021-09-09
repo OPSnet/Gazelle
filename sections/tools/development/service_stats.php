16d3ae292ae7dfed1ca38ae6af5582eeb9da6e2a
Spine <spine@orpheus.network>
2021-09-09 16:57:21 +0000

    inspect queries directly from debug panel

--- sections/tools/development/service_stats.php
+++ sections/tools/development/service_stats.php
@@ -1,5 +1,5 @@
 <?php
-if (!check_perms('site_debug') || !check_perms('admin_clear_cache')) {
+if (!$Viewer->permitted('site_debug') || !$Viewer->permitted('admin_clear_cache')) {
     error(403);
 }
 if (isset($_POST['global_flush'])) {
@@ -7,21 +7,16 @@ if (isset($_POST['global_flush'])) {
     $Cache->flush();
 }
 
-$DB->prepared_query('SHOW GLOBAL STATUS');
-$DBStats = $DB->to_array('Variable_name');
-
-$DB->prepared_query('SHOW GLOBAL VARIABLES');
-$DBVars = $DB->to_array('Variable_name');
-
+$inspectDb = new Gazelle\DB;
 $MemStats = $Cache->getStats();
 $MemStats = $MemStats[array_keys($MemStats)[0]];
 
 View::show_header("Service Stats");
 echo $Twig->render('admin/cache-db-stats.twig', [
     'auth'       => $Viewer->auth(),
-    'can_see_db' => check_perms('site_database_specifics'),
-    'db_stats'   => $DBStats,
-    'db_vars'    => $DBVars,
+    'can_see_db' => $Viewer->permitted('site_database_specifics'),
+    'db_stats'   => $inspectDb->globalStatus(),
+    'db_vars'    => $inspectDb->globalVariables(),
     'mem_stats'  => $MemStats,
     'now'        => time(),
 ]);
