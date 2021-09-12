461ec23efa2244023a8009de139a6aef9c6ba1e5
Spine <spine@orpheus.network>
2021-09-12 09:11:55 +0000

    drop old users_summary table

--- app/Stats/Users.php
+++ app/Stats/Users.php
@@ -147,31 +147,6 @@ class Users extends \Gazelle\Base {
 
     public function refresh(): int {
         $this->db->prepared_query("
-            INSERT INTO users_summary (UserID, Groups)
-                SELECT UserID, COUNT(DISTINCT GroupID)
-                FROM torrents t
-                INNER JOIN users_main u ON u.ID = t.UserID
-                GROUP BY UserID
-            ON DUPLICATE KEY UPDATE Groups = VALUES(Groups)
-        ");
-
-        $this->db->prepared_query("
-            INSERT INTO users_summary (UserID, PerfectFlacs)
-                SELECT t.UserID, COUNT(t.ID)
-                FROM torrents t
-                INNER JOIN users_main u ON u.ID = t.UserID
-                WHERE (t.Format = 'FLAC'
-                    AND (
-                        (t.LogScore = 100 AND t.Media = 'CD')
-                        OR
-                        t.Media IN ('Vinyl', 'WEB', 'DVD', 'Soundboard', 'Cassette', 'SACD', 'BD', 'DAT')
-                    )
-                )
-                GROUP BY t.UserID
-            ON DUPLICATE KEY UPDATE PerfectFlacs = VALUES(PerfectFlacs)
-        ");
-
-        $this->db->prepared_query("
             CREATE TEMPORARY TABLE user_summary_new LIKE user_summary
         ");
 
---
+++ db/migrations/20210911215404_drop_users_summary.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class DropUsersSummary extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('users_summary')->drop()->update();
+    }
+
+    public function down(): void
+    {
+        $this->table('users_summary', ['id' => false, 'primary_key' => 'UserID'])
+             ->addColumn('UserID', 'integer', ['limit' => 10, 'signed' => false])
+             ->addColumn('Groups', 'integer', ['limit' => 10, 'default' => 0])
+             ->addColumn('PerfectFlacs', 'integer', ['limit' => 10, 'default' => 0])
+             ->addForeignKey('UserID', 'users_main', 'ID')
+             ->create();
+    }
+}
