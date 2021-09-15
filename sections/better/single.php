eaa889dff341c192ecf6695904c5fe9df14ab3c6
Spine <spine@orpheus.network>
2021-09-15 19:18:49 +0000

    Better unseeded, now with seeding size

--- sections/better/single.php
+++ sections/better/single.php
@@ -1,14 +1,6 @@
 <?php
-$results = (new Gazelle\Manager\Better(new Gazelle\ReleaseType))->twigGroups($better->singleSeeded());
 
 echo $Twig->render('better/single.twig', [
-    'results'      => $results,
-    'result_count' => count($results),
-    'auth_key'     => $Viewer->auth(),
-    'torrent_pass' => $Viewer->announceKey(),
-    'tokens'       => $Viewer->tokenCount() > 0,
-    'torrent_ids'  => implode(',', array_keys($results)),
-    'perms'        => [
-        'zip_downloader' => $Viewer->permitted('zip_downloader'),
-    ],
+    'results' => (new Gazelle\Manager\Better(new Gazelle\ReleaseType))->singleSeeded($Viewer->id()),
+    'viewer'  => $Viewer,
 ]);
