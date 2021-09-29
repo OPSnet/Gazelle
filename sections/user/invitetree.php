ffab0d2e664e0ab05ffd99662a5bb4fb3647b52c
Spine <spine@orpheus.network>
2021-09-29 21:15:01 +0000

    mean cache and query reqs/sec (and fix invitetree for mods)

--- sections/user/invitetree.php
+++ sections/user/invitetree.php
@@ -1,19 +1,18 @@
 <?php
 
 if (!isset($_GET['userid'])) {
-    $userId = $Viewer->id();
+    $user = $Viewer;
 } else {
     if (!$Viewer->permitted('users_view_invites')) {
         error(403);
     }
-    $userId = (int)$_GET['userid'];
-}
-$user = (new Gazelle\Manager\User)->find($userId);
-if (is_null($user)) {
-    error(404);
+    $user = (new Gazelle\Manager\User)->findById((int)$_GET['userid']);
+    if (is_null($user)) {
+        error(404);
+    }
 }
 
-echo $Twig->render('user/invite-tree.twig', array_merge(
+echo $Twig->render('user/invite-tree-page.twig', array_merge(
     ['user' => $user],
-    (new Gazelle\InviteTree($userId))->details(),
+    (new Gazelle\InviteTree($user->id()))->details(),
 ));
