cc1e787f45f9e1aff9d35b04a5ef51bc55dc84ee
Spine <spine@orpheus.network>
2021-10-09 11:29:56 +0000

    upload logs as a distinct privilege

--- sections/tools/managers/permissions_alter.php
+++ sections/tools/managers/permissions_alter.php
@@ -1,150 +1,89 @@
 <?php
-if (!check_perms('admin_manage_permissions')) {
+
+if (!$Viewer->permitted('admin_manage_permissions')) {
     error(403);
 }
 
-$id = $_REQUEST['id'] ?? null;
-if ($id) {
-    if (is_numeric($id)) {
-        $DB->prepared_query('
-            SELECT
-                p.ID,
-                p.Name,
-                p.Level,
-                p.Secondary,
-                p.PermittedForums,
-                p.Values,
-                p.DisplayStaff,
-                p.StaffGroup,
-                p.badge,
-                count(u.ID) + count(DISTINCT l.UserID)
-            FROM permissions AS p
-            LEFT JOIN users_main AS u ON (u.PermissionID = p.ID)
-            LEFT JOIN users_levels AS l ON (l.PermissionID = p.ID)
-            WHERE p.ID = ?
-            GROUP BY p.ID
-            ', $id
-        );
-        list($id, $name, $level, $secondary, $forums, $values, $displayStaff, $staffGroup, $badge, $userCount) = $DB->next_record(MYSQLI_NUM, [5]);
+$privMan = new Gazelle\Manager\Privilege;
 
-        if (!check_perms('admin_manage_permissions', $level)) {
-            error(403);
-        }
-
-        $values = unserialize($values);
+$privilege = $privMan->findById((int)($_REQUEST['removeid'] ?? 0));
+if ($privilege) {
+    if ($privilege->userTotal() > 0) {
+        error('You cannot delete a class with users.');
     }
-
-    if (!empty($_POST['submit'])) {
-        $Val = new Gazelle\Util\Validator;
-        $Val->setFields([
-            ['name', true, 'string', 'You did not enter a valid name for this permission set.'],
-            ['level', true, 'number', 'You did not enter a valid level for this permission set.'],
-        ]);
-        if (!$Val->validate($_POST)) {
-            error($Val->errorMessage());
-        }
-
-        if (!is_numeric($id)) {
-            if ($DB->scalar('SELECT ID FROM permissions WHERE Level = ?', $_REQUEST['level'])) {
-                error('There is already a permission class with that level.');
-            }
-        } else if (empty($_REQUEST['secondary']) == $secondary) {
-            if (!$secondary && $DB->scalar('SELECT count(*) FROM users_main WHERE PermissionID = ?', $id) ||
-                 $secondary && $DB->scalar('SELECT count(*) FROM users_levels WHERE PermissionID = ?', $id)) {
-                error("You can't toggle secondary when there are users");
-            }
-        }
-
-        $values = [];
-        foreach ($_REQUEST as $key => $perms) {
-            if (substr($key, 0, 5) == 'perm_') {
-                $values[substr($key, 5)] = (int)$perms;
-            }
-        }
-
-        $name = $_REQUEST['name'];
-        $level = $_REQUEST['level'];
-        $secondary = empty($_REQUEST['secondary']) ? 0 : 1;
-        $forums = $_REQUEST['forums'];
-        $displayStaff = empty($_REQUEST['displaystaff']) ? '0' : '1';
-        $staffGroup = $_REQUEST['staffgroup'] ?? null;
-        $badge = $_REQUEST['badge'] ?? '';
-
-        if (!$secondary) {
-            $badge = '';
-        }
-
-        if (!is_numeric($id)) {
-            $DB->prepared_query('
-                INSERT INTO permissions
-                       (Level, Name, Secondary, PermittedForums, `Values`, DisplayStaff, StaffGroup, badge)
-                VALUES (?,     ?,    ?,         ?,                ?,       ?,            ?,          ?)
-                ', $level, $name, $secondary, $forums, serialize($values), $displayStaff, $staffGroup, $badge
-            );
-        } else {
-            $DB->prepared_query('
-                UPDATE permissions
-                SET Level = ?,
-                    Name = ?,
-                    Secondary = ?,
-                    PermittedForums = ?,
-                    `Values` = ?,
-                    DisplayStaff = ?,
-                    StaffGroup = ?,
-                    badge = ?
-                WHERE ID = ?
-                ', $level, $name, $secondary, $forums, serialize($values), $displayStaff, $staffGroup, $badge, $id
-            );
-            if ($secondary) {
-                $DB->prepared_query("
-                    SELECT DISTINCT concat('user_info_heavy_', UserID)
-                    FROM users_levels
-                    WHERE PermissionID = ?
-                    ", $id
-                );
-            } else {
-                $DB->prepared_query("
-                    SELECT DISTINCT concat('user_info_heavy_', ID)
-                    FROM users_main
-                    WHERE PermissionID = ?
-                    ", $id
-                );
-            }
-            $Cache->deleteMulti(array_merge(['perm_'.$id], $DB->collect(0, false)));
-        }
-        $Cache->deleteMulti(['classes', 'staff']);
-    }
-
-    require_once('permissions_edit.php');
-} else {
-    $id = $_REQUEST['removeid'] ?? null;
-    if ($id) {
-        if ($DB->scalar('SELECT count(*) FROM users_main WHERE PermissionID = ?', $id)) {
-            error('You cannot delete a class with users.');
-        }
-        if ($DB->scalar('SELECT Secondary FROM permissions WHERE ID = ?', $id)) {
-            $DB->prepared_query("
-                SELECT DISTINCT concat('user_info_heavy_', UserID)
-                FROM users_levels
-                WHERE PermissionID = ?
-                ", $id
-            );
-            $Cache->deleteMulti($DB->collect(0, false));
-            $DB->prepared_query('
-                DELETE FROM users_levels
-                WHERE PermissionId = ?
-                ', $id
-            );
-        }
-
-        $DB->prepared_query('
-            DELETE FROM permissions
-            WHERE ID = ?
-            ', $id
-        );
-
-        $Cache->delete_value('classes');
-    }
-
+    $privilege->remove();
     require_once('permissions_list.php');
+    exit;
 }
+
+if (!isset($_REQUEST['id'])) {
+    require_once('permissions_list.php');
+    exit;
+}
+
+$privilege = $privMan->findById((int)($_REQUEST['id'] ?? 0));
+if (!isset($_REQUEST['submit'])) {
+    require_once('permissions_edit.php');
+    exit;
+}
+
+$validator = new Gazelle\Util\Validator;
+$validator->setFields([
+    ['name', true, 'string', 'You did not enter a valid name for this permission set.'],
+    ['level', true, 'number', 'You did not enter a valid level for this permission set.'],
+]);
+if (!$validator->validate($_POST)) {
+    error($validator->errorMessage());
+}
+$check = $privMan->findByLevel($_REQUEST['level']);
+if ($privilege && $check && $privilege->id() != $check->id()) {
+    error('There is already a permission class with that level.');
+}
+
+if ($privilege && empty($_REQUEST['secondary']) == $privilege->isSecondary() && $privilege->userTotal() > 0) {
+    error("You can't toggle secondary when there are users");
+}
+
+$name         = $_REQUEST['name'];
+$forums       = $_REQUEST['forums'];
+$staffGroup   = $_REQUEST['staffgroup'] ?? null;
+$displayStaff = isset($_REQUEST['displaystaff']);
+$level        = (int)$_REQUEST['level'];
+$secondary    = (int)isset($_REQUEST['secondary']);
+$badge        = $secondary ? ($_REQUEST['badge'] ?? '') : '';
+$values       = [];
+foreach ($_REQUEST as $key => $perm) {
+    if (substr($key, 0, 5) == 'perm_') {
+        $values[substr($key, 5)] = (int)$perm;
+    }
+}
+
+if (is_null($privilege)) {
+    $privilege = $privMan->create($name, $level, $secondary, $forums, $values, $staffGroup, $badge, $displayStaff);
+} else {
+    if ($badge != $privilege->badge()) {
+        $privilege->setUpdate('Badge', $badge);
+    }
+    if ($forums != $privilege->displayStaff()) {
+        $privilege->setUpdate('DisplayStaff', $displayStaff);
+    }
+    if ($level != $privilege->level()) {
+        $privilege->setUpdate('Level', $level);
+    }
+    if ($name != $privilege->name()) {
+        $privilege->setUpdate('Name', $name);
+    }
+    if ((bool)$secondary != $privilege->isSecondary()) {
+        $privilege->setUpdate('Secondary', $secondary);
+    }
+    if ($forums != $privilege->permittedForums()) {
+        $privilege->setUpdate('PermittedForums', $forums);
+    }
+    if ($forums != $privilege->staffGroup()) {
+        $privilege->setUpdate('StaffGroup', $staffGroup);
+    }
+    $privilege->setUpdate('`Values`', serialize($values))
+        ->modify();
+}
+$Cache->deleteMulti(['classes', 'staff']);
+require_once('permissions_edit.php');
