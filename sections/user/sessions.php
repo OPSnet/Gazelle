9669dbe70cca468e06e1da0e81d2dc9bab2ab703
Spine <spine@orpheus.network>
2021-10-14 20:29:58 +0000

    move View::show* methods into template

--- sections/user/sessions.php
+++ sections/user/sessions.php
@@ -1,18 +1,18 @@
 <?php
 
 if (!isset($_GET['userid'])) {
-    $userId = $Viewer->id();
-} elseif (!check_perms('users_view_ips') || !check_perms('users_logout')) {
-        error(403);
+    $user = $Viewer;
 } else {
-    $userId = (int)$_GET['userid'];
-}
-$user = (new Gazelle\Manager\User)->findById($userId);
-if (is_null($user)) {
-    error(404);
+    if (!$Viewer->permitted('users_view_ips') || !$Viewer->permitted('users_logout')) {
+        error(403);
+    }
+    $user = (new Gazelle\Manager\User)->findById((int)$_GET['userid']);
+    if (is_null($user)) {
+        error(404);
+    }
 }
 
-$sessionMan = new Gazelle\Session($userId);
+$sessionMan = new Gazelle\Session($user->id());
 if (isset($_POST['all'])) {
     authorize();
     $sessionMan->dropAll();
@@ -22,11 +22,9 @@ if (isset($_POST['session'])) {
     $sessionMan->drop($_POST['session']);
 }
 
-View::show_header($user->username().' &rsaquo; Sessions');
 echo $Twig->render('user/session.twig', [
     'auth'    => $Viewer->auth(),
     'current' => $SessionID,
     'session' => $sessionMan->loadSessions(),
     'user'    => $user,
 ]);
-View::show_footer();
