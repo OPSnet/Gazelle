d4c6fb906d0c0fcf5e1fcb700c0d0d3f2e94cc65
Spine <spine@orpheus.network>
2021-10-16 22:21:40 +0000

    more torrent methods

--- app/Json/Torrent.php
+++ app/Json/Torrent.php
@@ -3,9 +3,9 @@
 namespace Gazelle\Json;
 
 class Torrent extends \Gazelle\Json {
-    protected $torrent;
+    protected \Gazelle\Torrent $torrent;
+    protected bool $showSnatched = false;
     protected $userId;
-    protected $showSnatched = false;
 
     public function __construct() {
         parent::__construct();
@@ -17,26 +17,28 @@ class Torrent extends \Gazelle\Json {
         return $this;
     }
 
-    public function setShowSnatched(int $showSnatched) {
+    public function setShowSnatched(bool $showSnatched) {
         $this->showSnatched = $showSnatched;
         return $this;
     }
 
     public function findById(int $id) {
-        $this->torrent = (new \Gazelle\Manager\Torrent)->findById($id);
-        if (!$this->torrent) {
+        $torrent = (new \Gazelle\Manager\Torrent)->findById($id);
+        if (is_null($torrent)) {
             $this->failure("bad id parameter");
             return null;
         }
+        $this->torrent = $torrent;
         return $this;
     }
 
     public function findByInfohash(string $hash) {
-        $this->torrent = (new \Gazelle\Manager\Torrent)->findByInfohash($hash);
-        if (!$this->torrent) {
+        $torrent = (new \Gazelle\Manager\Torrent)->findByInfohash($hash);
+        if (is_null($torrent)) {
             $this->failure("bad hash parameter");
             return null;
         }
+        $this->torrent = $torrent;
         return $this;
     }
 
@@ -46,75 +48,72 @@ class Torrent extends \Gazelle\Json {
             return null;
         }
 
-        $this->torrent->setViewerId($this->userId)->setShowSnatched($this->showSnatched);
-        $info  = $this->torrent->info();
-        $group = $this->torrent->group()->showFallbackImage(false)->info();
+        $torrent = $this->torrent->setViewerId($this->userId)->setShowSnatched($this->showSnatched);
+        $group = $torrent->group();
 
         // TODO: implement as a Gazelle class
-        $categoryName = ($group['CategoryID'] == 0) ? "Unknown" : CATEGORY[$group['CategoryID'] - 1];
+        $categoryName = $group->categoryId() == 0 ? "Unknown" : CATEGORY[$group->categoryId() - 1];
 
-        // Convert file list back to the old format
         $torMan = new \Gazelle\Manager\Torrent;
-        foreach ($info['FileList'] as &$file) {
-            $file = $torMan->apiFilename($file);
-        }
-        unset($file);
-
         return [
             'group' => [
-                'wikiBody'        => \Text::full_format($group['WikiBody']),
-                'wikiBBcode'      => $group['WikiBody'],
-                'wikiImage'       => $group['WikiImage'],
-                'id'              => $group['ID'],
-                'name'            => $group['Name'],
-                'year'            => $group['Year'],
-                'recordLabel'     => $group['RecordLabel'] ?? '',
-                'catalogueNumber' => $group['CatalogueNumber'] ?? '',
-                'releaseType'     => $group['ReleaseType'] ?? '',
-                'releaseTypeName' => (new \Gazelle\ReleaseType)->findNameById($group['ReleaseType']),
-                'categoryId'      => $group['CategoryID'],
+                'wikiBody'        => \Text::full_format($group->description()),
+                'wikiBBcode'      => $group->description(),
+                'wikiImage'       => $group->image(),
+                'id'              => $group->id(),
+                'name'            => $group->name(),
+                'year'            => $group->year(),
+                'recordLabel'     => $group->recordLabel() ?? '',
+                'catalogueNumber' => $group->catalogueNumber() ?? '',
+                'releaseType'     => $group->releaseType() ?? '',
+                'releaseTypeName' => (new \Gazelle\ReleaseType)->findNameById($group->releaseType()),
+                'categoryId'      => $group->categoryId(),
                 'categoryName'    => $categoryName,
-                'time'            => $group['Time'],
-                'vanityHouse'     => $group['VanityHouse'],
-                'isBookmarked'    => (new \Gazelle\Bookmark)->isTorrentBookmarked($this->userId, $group['ID']),
-                'tags'            => explode('|', $group['tagNames']),
+                'time'            => $group->time(),
+                'vanityHouse'     => $group->isShowcase(),
+                'isBookmarked'    => (new \Gazelle\Bookmark)->isTorrentBookmarked($this->userId, $group->id()),
+                'tags'            => $group->tagNameList(),
                 'musicInfo'       => ($categoryName != "Music")
-                    ? null : \Artists::get_artist_by_type($group['ID']),
+                    ? null : \Artists::get_artist_by_type($group->id()),
             ],
             'torrent' => array_merge(
-                !is_null($this->torrent->infohash()) || $this->torrent->uploader()->id() == $this->userId
-                    ? [ 'infoHash' => $this->torrent->infohash() ]
+                !is_null($torrent->infohash()) || $torrent->uploaderId() == $this->userId
+                    ? [ 'infoHash' => $torrent->infohash() ]
                     : [],
                 [
-                    'id'            => $this->torrent->id(),
-                    'media'         => $info['Media'],
-                    'format'        => $info['Format'],
-                    'encoding'      => $info['Encoding'],
-                    'remastered'    => $this->torrent->isRemastered(),
-                    'remasterYear'  => $info['RemasterYear'],
-                    'remasterTitle' => $info['RemasterTitle'],
-                    'remasterRecordLabel' => $info['RemasterRecordLabel'],
-                    'remasterCatalogueNumber' => $info['RemasterCatalogueNumber'],
-                    'scene'         => $info['Scene'],
-                    'hasLog'        => $info['HasLog'],
-                    'hasCue'        => $info['HasCue'],
-                    'logScore'      => $info['LogScore'],
-                    'logChecksum'   => $info['LogChecksum'],
-                    'logCount'      => $info['LogCount'],
-                    'ripLogIds'     => $info['ripLogIds'],
-                    'fileCount'     => $info['FileCount'],
-                    'size'          => $info['Size'],
-                    'seeders'       => $info['Seeders'],
-                    'leechers'      => $info['Leechers'],
-                    'snatched'      => $info['Snatched'],
-                    'freeTorrent'   => $info['FreeTorrent'],
-                    'reported'      => $torMan->hasReport($this->torrent->id()),
-                    'time'          => $info['Time'],
-                    'description'   => $info['Description'],
-                    'fileList'      => implode('|||', $info['FileList']),
-                    'filePath'      => $info['FilePath'],
-                    'userId'        => $info['UserID'],
-                    'username'      => $this->torrent->uploader()->username(),
+                    'id'            => $torrent->id(),
+                    'media'         => $torrent->media(),
+                    'format'        => $torrent->format(),
+                    'encoding'      => $torrent->encoding(),
+                    'remastered'    => $torrent->isRemastered(),
+                    'remasterYear'  => $torrent->remasterYear(),
+                    'remasterTitle' => $torrent->remasterTitle(),
+                    'remasterRecordLabel'
+                                    => $torrent->remasterRecordLabel(),
+                    'remasterCatalogueNumber'
+                                    => $torrent->remasterCatalogueNumber(),
+                    'scene'         => $torrent->isScene(),
+                    'hasLog'        => $torrent->hasLog(),
+                    'hasCue'        => $torrent->hasCue(),
+                    'logScore'      => $torrent->logScore(),
+                    'logChecksum'   => $torrent->logChecksum(),
+                    'logCount'      => count($torrent->ripLogIdList()),
+                    'ripLogIds'     => $torrent->ripLogIdList(),
+                    'fileCount'     => $torrent->fileTotal(),
+                    'size'          => $torrent->size(),
+                    'seeders'       => $torrent->seederTotal(),
+                    'leechers'      => $torrent->leecherTotal(),
+                    'snatched'      => $torrent->snatchTotal(),
+                    'freeTorrent'   => $torrent->freeleechStatus(),
+                    'reported'      => $torMan->hasReport($torrent->id()),
+                    'time'          => $torrent->uploadDate(),
+                    'description'   => $torrent->description(),
+                    'fileList'      => implode('|||',
+                        array_map(function ($f) use ($torMan) {return $torMan->apiFilename($f);}, $torrent->filelist())
+                    ),
+                    'filePath'      => $torrent->path(),
+                    'userId'        => $torrent->uploaderId(),
+                    'username'      => $torrent->uploader()->username(),
                 ]
             ),
         ];
