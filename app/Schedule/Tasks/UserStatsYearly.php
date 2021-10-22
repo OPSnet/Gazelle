f530f05fc427a7012930eb1bc33d223dbe27cc59
Spine <spine@orpheus.network>
2021-10-22 16:43:20 +0000

    refactor user activity stats registration

--- app/Schedule/Tasks/UserStatsYearly.php
+++ app/Schedule/Tasks/UserStatsYearly.php
@@ -6,26 +6,7 @@ class UserStatsYearly extends \Gazelle\Schedule\Task
 {
     public function run()
     {
-        $this->db->prepared_query("
-            INSERT INTO users_stats_yearly (UserID, Uploaded, Downloaded, BonusPoints, Torrents, PerfectFLACs)
-            SELECT um.ID, uls.Uploaded, uls.Downloaded, coalesce(ub.points, 0), COUNT(t.ID) AS Torrents, COALESCE(p.Perfects, 0) AS PerfectFLACs
-            FROM users_main um
-            INNER JOIN users_leech_stats uls ON (uls.UserID = um.ID)
-            LEFT JOIN user_bonus ub ON (ub.user_id = um.ID)
-            LEFT JOIN torrents t ON (t.UserID = um.ID)
-            LEFT JOIN
-            (
-                SELECT UserID, COUNT(ID) AS Perfects
-                FROM torrents
-                WHERE( Format = 'FLAC'
-                    AND (
-                        Media IN ('Vinyl', 'WEB', 'DVD', 'Soundboard', 'Cassette', 'SACD', 'BD', 'DAT')
-                        OR
-                        (LogScore = 100 AND Media = 'CD')))
-                GROUP BY UserID
-            ) p ON (p.UserID = um.ID)
-            GROUP BY um.ID
-        ");
-        $this->processed = $this->db->affected_rows();
+        $this->processed += (new \Gazelle\Stats\Users)
+            ->registerActivity('users_stats_yearly', 0);
     }
 }
