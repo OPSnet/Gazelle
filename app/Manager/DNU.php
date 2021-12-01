b4708dae408bfc5b842ef9a1c62ee1242d5d0eae
Spine <spine@orpheus.network>
2021-12-01 22:37:13 +0000

    twigify DNU admin page

---
+++ app/Manager/DNU.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gazelle\Manager;
+
+class DNU extends \Gazelle\Base {
+
+    public function dnuList(): array {
+        $this->db->prepared_query("
+            SELECT d.ID    AS id,
+                d.Name     AS name,
+                d.Comment  AS comment,
+                d.UserID   AS user_id,
+                d.Time     AS time
+            FROM do_not_upload AS d
+            ORDER BY d.Sequence
+        ");
+        return $this->db->to_array(false, MYSQLI_ASSOC, false);
+    }
+}
