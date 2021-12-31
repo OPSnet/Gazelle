d046ee3b78cb015215d72b06f62346c6b43a37ab
Spine <spine@orpheus.network>
2021-12-31 10:50:19 +0000

    move View::show_header() functionality into User\Activity

--- classes/view.class.php
+++ classes/view.class.php
@@ -27,10 +27,7 @@ class View {
             'global.css',
         ];
         if (!empty($option['css'])) {
-            $Style = array_merge(
-                $Style,
-                array_map(fn($s) => "$s/style.css", explode(',', $option['css']))
-            );
+            array_push($Style, ...array_map(fn($s) => "$s/style.css", explode(',', $option['css'])));
         }
 
         $Scripts = [
@@ -43,17 +40,17 @@ class View {
             'autocomplete',
         ];
         if (!empty($option['js'])) {
-            $Scripts = array_merge($Scripts, explode(',', $option['js']));
+            array_push($Scripts, ...explode(',', $option['js']));
         }
         if (DEBUG_MODE || $Viewer->permitted('site_debug')) {
             array_push($Scripts, 'jquery-migrate', 'debug');
         }
         if ($Viewer->option('Tooltipster') ?? 1) {
             array_push($Scripts, 'tooltipster', 'tooltipster_settings');
-            array_push($Style, 'tooltipster/style.css');
+            $Style[] = 'tooltipster/style.css';
         }
         if ($Viewer->option('UseOpenDyslexic')) {
-            array_push($Style, 'opendyslexic/style.css');
+            $Style[] = 'opendyslexic/style.css';
         }
 
         $notifMan = new Gazelle\Manager\Notification($Viewer->id());
@@ -70,124 +67,25 @@ class View {
             }
         }
 
-        $activity = new Gazelle\Activity;
-        if ($Viewer->onRatioWatch()) {
-            $activity->setAlert('<a class="nobr" href="rules.php?p=ratio">Ratio Watch</a>: You have '
-                . time_diff($Viewer->ratioWatchExpiry(), 3)
-                . ' to get your ratio over your required ratio or your leeching abilities will be disabled.'
-            );
-        } elseif (!$Viewer->canLeech()) {
-            $activity->setAlert('<a class="nobr" href="rules.php?p=ratio">Ratio Watch</a>: Your downloading privileges are disabled until you meet your required ratio.');
-        }
+        $payMan   = new Gazelle\Manager\Payment;
+        $spmMan   = new Gazelle\Manager\StaffPM;
+        $spmTotal = $spmMan->countAtLevel($Viewer, ['Unanswered']);
 
-        $needStaffInbox = false;
+        $activity = new Gazelle\User\Activity($Viewer);
+        $activity->configure()
+            ->setNotification($notifMan)
+            ->setStaffPM($spmMan);
+
         if ($Viewer->permitted('users_mod')) {
-            $activity->setAction('<a class="nobr" href="tools.php">Toolbox</a>');
-        }
-        if ($Viewer->isStaff()) {
-            $activity->setNotification($notifMan);
-            $staff = new Gazelle\Staff($Viewer);
-            $count = $staff->pmCount();
-            if ($count > 0) {
-                $needStaffInbox = true;
-                $activity->setAction('<a class="nobr" href="staffpm.php">' . $count . ' Staff PMs</a>');
-            }
+            $activity->setStaff(new Gazelle\Staff($Viewer))
+                ->setReport(new Gazelle\Report)
+                ->setPayment($payMan)
+                ->setApplicant(new Gazelle\Manager\Applicant)
+                ->setDb(new Gazelle\DB)
+                ->setScheduler(new Gazelle\Schedule\Scheduler);
 
-            if ($staff->blogAlert()) {
-                $activity->setAlert('<a class="nobr" href="staffblog.php">New staff blog post!</a>');
-            }
-            if (FEATURE_EMAIL_REENABLE) {
-                global $Cache, $DB;
-                $NumEnableRequests = $Cache->get_value(AutoEnable::CACHE_KEY_NAME);
-                if ($NumEnableRequests === false) {
-                    $NumEnableRequests = $DB->scalar("SELECT count(*) FROM users_enable_requests WHERE Outcome IS NULL");
-                    $Cache->cache_value(AutoEnable::CACHE_KEY_NAME, $NumEnableRequests);
-                }
-                if ($NumEnableRequests > 0) {
-                    $activity->setAction('<a class="nobr" href="tools.php?action=enable_requests">' . $NumEnableRequests . " Enable requests</a>");
-                }
-            }
-        }
-
-        $payMan = new Gazelle\Manager\Payment;
-        if ($Viewer->permitted('admin_manage_payments')) {
-            $due = $payMan->due();
-            if ($due) {
-                $AlertText = '<a class="nobr" href="tools.php?action=payment_list">Payments due</a>';
-                foreach ($due as $p) {
-                    [$Text, $Expiry] = array_values($p);
-                    $Color = strtotime($Expiry) < (strtotime('+3 days')) ? 'red' : 'orange';
-                    $AlertText .= sprintf(' | <span style="color: %s">%s: %s</span>', $Color, $Text, date('Y-m-d', strtotime($Expiry)));
-                }
-                $activity->setAlert($AlertText);
-            }
-        }
-
-        if ($Viewer->permitted('admin_reports')) {
-            $repoMan = new Gazelle\Report;
-            $open = $repoMan->openCount();
-            $activity->setAction("<a class=\"nobr\" href=\"reportsv2.php\">$open Report" . plural($open) . '</a>');
-            $other = $repoMan->otherCount();
-            if ($other > 0) {
-                $activity->setAction("<a class=\"nobr\" href=\"reports.php\">$other Other report" . plural($other) . '</a>');
-            }
-        } elseif ($Viewer->permitted('site_moderate_forums')) {
-            $open = (new Gazelle\Report)->forumCount();
-            if ($open > 0) {
-                $activity->setAction("<a href=\"reports.php\">$open Forum report" . plural($open) . '</a>');
-            }
-        }
-
-        if ($Viewer->permitted('admin_manage_applicants')) {
-            $appMan = new Gazelle\Manager\Applicant;
-            $count = $appMan->newApplicantCount();
-            if ($count > 0) {
-                $activity->setAction(sprintf(
-                    '<a href="apply.php?action=view">%d new Applicant%s</a>', $count, plural($count)
-                ));
-            }
-            $count = $appMan->newReplyCount();
-            if ($count > 0) {
-                $activity->setAction(sprintf(
-                    '<a href="apply.php?action=view">%d new Applicant %s</a>', $count, ($count == 1 ? 'Reply' : 'Replies')
-                ));
-            }
-        }
-
-        if ($Viewer->permitted('admin_site_debug')) {
-            $longRunning = (new Gazelle\DB)->longRunning();
-            if ($longRunning > 0) {
-                $activity->setAlert('<span style="color: red">' . $longRunning . ' DB operation' . plural($longRunning) . ' running.</span>');
-            }
-        }
-
-        if ($Viewer->permitted('admin_periodic_task_view')) {
-            global $DB;
-            $lastSchedulerRun = $DB->scalar("
-                SELECT now() - max(launch_time) FROM periodic_task_history
-            ");
-            if ($lastSchedulerRun > SCHEDULER_DELAY) {
-                $activity->setAlert('<span style="color: red">Scheduler not running</span>');
-            }
-        }
-
-        if (OPEN_EXTERNAL_REFERRALS) {
-            if ($Viewer->permitted('admin_site_debug')) {
-                if (!apcu_exists('DB_KEY') || !apcu_fetch('DB_KEY')) {
-                    $activity->setAlert('<a href="tools.php?action=dbkey"><span style="color: red">DB key not loaded</span></a>');
-                }
-            }
-
-            if ($Viewer->permitted('admin_manage_referrals')) {
-                if (!(new Gazelle\Manager\Referral)->checkBouncer()) {
-                    $activity->setAlert('<a href="tools.php?action=referral_sandbox"><span class="nobr" style="color: red">Referral bouncer not responding</span></a>');
-                }
-            }
-        }
-
-        if ($Viewer->permitted('admin_periodic_task_view')) {
-            if ($insane = (new Gazelle\Schedule\Scheduler)->getInsaneTasks()) {
-                $activity->setAlert(sprintf('<a href="tools.php?action=periodic&amp;mode=view">There are %d insane tasks</a>', $insane));
+            if (OPEN_EXTERNAL_REFERRALS) {
+                $activity->setReferral(new Gazelle\Manager\Referral);
             }
         }
 
@@ -228,7 +126,7 @@ class View {
                     $extraClass[] = 'active';
                 }
             } elseif ($Key === 'staffinbox') {
-                if ($needStaffInbox) {
+                if ($activity->showStaffInbox()) {
                     $extraClass[] = 'new-subscriptions';
                 }
                 if (self::add_active($PageID, $Tests)) {
