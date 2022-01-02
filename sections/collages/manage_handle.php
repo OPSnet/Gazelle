7f5c09f7d469d55fc605f85f850d08ee116f2c75
Spine <spine@orpheus.network>
2022-01-02 14:35:06 +0000

    log collage entry removals

--- sections/collages/manage_handle.php
+++ sections/collages/manage_handle.php
@@ -2,31 +2,33 @@
 
 authorize();
 
-$collageID = (int)$_POST['collageid'];
-if (!$collageID) {
-    error(404);
-}
-$collage = new Gazelle\Collage($collageID);
-if ($collage->isPersonal() && !$collage->isOwner($Viewer->id()) && !$Viewer->permitted('site_collages_delete')) {
-    error(403);
-}
-
 $groupId = (int)$_POST['groupid'];
 if (!$groupId) {
     error(404);
 }
+$collage = new Gazelle\Collage((int)($_POST['collageid']));
+if (is_null($collage)) {
+    error(404);
+}
+if ($collage->isPersonal() && !$collage->isOwner($Viewer->id()) && !$Viewer->permitted('site_collages_delete')) {
+    error(403);
+}
 
 if (isset($_POST['submit']) && $_POST['submit'] === 'Remove') {
-    $collage->removeEntry($groupId);
+    if ($collage->removeEntry($groupId)) {
+        (new Gazelle\Log)->general(sprintf("Collage %d (%s) group $groupId removed by %s",
+            $collage->id(), $collage->name(), $Viewer->username()
+        ));
+    }
 } elseif (isset($_POST['drag_drop_collage_sort_order'])) {
     $collage->updateSequence($_POST['drag_drop_collage_sort_order']);
 } else {
-    $sequence = $_POST['sort'];
-    if (!is_number($sequence)) {
+    $sequence = (int)_POST['sort'];
+    if (!$sequence) {
         error(404);
     }
     $collage->updateSequenceEntry($groupId, $sequence);
 }
 $collage->flush();
 
-header("Location: collages.php?action=manage&collageid=$collageID");
+header('Location: ' . $collage->url() . '&action=manage');
