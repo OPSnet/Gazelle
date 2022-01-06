86e845126671daca48fd02d84def2d35bdf760a7
Spine <spine@orpheus.network>
2022-01-06 15:23:33 +0000

    twigify error page

--- sections/error/index.php
+++ sections/error/index.php
@@ -2,8 +2,7 @@
 
 use Gazelle\Util\Irc;
 
-function notify ($Channel, $Message) {
-    global $Viewer;
+function notify ($Viewer, $Channel, $Message) {
     Irc::sendRaw("PRIVMSG "
         . $Channel . " :" . $Message . " error by "
         . ($Viewer
@@ -16,66 +15,49 @@ function notify ($Channel, $Message) {
     );
 }
 
-$Errors = ['403','404','413','504'];
+switch ($Error) {
+    case '403':
+        $Title = "Error 403";
+        $Description = "You tried to go to a page that you don't have enough permission to view.";
+        notify($Viewer, STATUS_CHAN, 403);
+        break;
+    case '404':
+        $Title = "Error 404";
+        $Description = "You tried to go to a page that doesn't exist.";
+        break;
+    case '429':
+        $Title = "Error 429";
+        $Description = "You tried to do something too frequently.";
+        notify($Viewer, STATUS_CHAN, 429);
+        break;
+    case '0':
+        $Title = "Invalid Input";
+        $Description = "Something was wrong with the input provided with your request, and the server is refusing to fulfill it.";
+        notify($Viewer, STATUS_CHAN, 'PHP-0');
+        break;
+    case '-1':
+        $Title = "Invalid request";
+        $Description = "Something was wrong with your request, and the server is refusing to fulfill it.";
+        break;
+    default:
+        if (empty($Error)) {
+            $Title = "Unexpected Error";
+            $Description = "You have encountered an unexpected error.";
+        } else {
+            $Title = 'Error';
+            $Description = $Error;
+        }
+}
 
-if (!empty($_GET['e']) && in_array($_GET['e'],$Errors)) {
-    // Web server error i.e. http://sitename/madeupdocument.php
-    include($_GET['e'].'.php');
+if (isset($Log) && $Log) {
+    $Description .= ' <a href="log.php?search='.$Log.'">Search Log</a>';
+}
+
+if (empty($NoHTML) && $Error != -1) {
+    echo $Twig->render('error.twig', [
+        'title'       => $Title,
+        'description' => $Description,
+    ]);
 } else {
-    // Gazelle error (Come from the error() function)
-    switch ($Error) {
-
-        case '403':
-            $Title = "Error 403";
-            $Description = "You tried to go to a page that you don't have enough permission to view.";
-            notify(STATUS_CHAN, 403);
-            break;
-        case '404':
-            $Title = "Error 404";
-            $Description = "You tried to go to a page that doesn't exist.";
-            break;
-        case '429':
-            $Title = "Error 429";
-            $Description = "You tried to do something too frequently.";
-            notify(STATUS_CHAN, 429);
-            break;
-        case '0':
-            $Title = "Invalid Input";
-            $Description = "Something was wrong with the input provided with your request, and the server is refusing to fulfill it.";
-            notify(STATUS_CHAN, 'PHP-0');
-            break;
-        case '-1':
-            $Title = "Invalid request";
-            $Description = "Something was wrong with your request, and the server is refusing to fulfill it.";
-            break;
-        default:
-            if (!empty($Error)) {
-                $Title = 'Error';
-                $Description = $Error;
-            } else {
-                $Title = "Unexpected Error";
-                $Description = "You have encountered an unexpected error.";
-            }
-    }
-
-    if (isset($Log) && $Log) {
-        $Description .= ' <a href="log.php?search='.$Log.'">Search Log</a>';
-    }
-
-    if (empty($NoHTML) && $Error != -1) {
-        View::show_header($Title);
-?>
-    <div class="thin">
-        <div class="header">
-            <h2><?=$Title?></h2>
-        </div>
-        <div class="box pad">
-            <p><?=$Description?></p>
-        </div>
-    </div>
-<?php
-        View::show_footer();
-    } else {
-        echo $Description;
-    }
+    echo $Description;
 }
