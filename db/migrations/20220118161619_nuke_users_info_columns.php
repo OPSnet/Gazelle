d44170841fa115c95e9613ab730d56ab329c25f7
Spine <spine@orpheus.network>
2022-01-18 17:16:15 +0000

    remove unused columns from users_info table

---
+++ db/migrations/20220118161619_nuke_users_info_columns.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class NukeUsersInfoColumns extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('users_info')
+            ->removeColumn('BitcoinAddress')
+            ->removeColumn('HideCountryChanges')
+            ->removeColumn('NotifyOnQuote')
+            ->save();
+    }
+
+    public function down(): void
+    {
+        $this->table('users_info')
+            ->addColumn('BitcoinAddress', 'string', ['limit' => 34])
+            ->addColumn('HideCountryChanges', 'enum', ['values' => ['0', '1'], 'default' => '0'])
+            ->addColumn('NotifyOnQuote', 'enum', [ 'values' => ['0', '1', '2'], 'default' => '0'])
+            ->save();
+    }
+}
