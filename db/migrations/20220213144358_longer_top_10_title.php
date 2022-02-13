dde1a324b4b5605f0756726855d7779b54acb126
Spine <spine@orpheus.network>
2022-02-13 14:49:12 +0000

    TitleString was too short

---
+++ db/migrations/20220213144358_longer_top_10_title.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class LongerTop10Title extends AbstractMigration {
+
+    public function up(): void {
+        $this->table('top10_history_torrents')
+            ->ChangeColumn('TitleString', 'string', ['length' => 400, 'default' => ''])
+            ->save();
+    }
+
+    public function down(): void {
+        $this->table('top10_history_torrents')
+            ->ChangeColumn('TitleString', 'string', ['length' => 150, 'default' => ''])
+            ->save();
+    }
+}
