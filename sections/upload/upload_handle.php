33e9d9fc50178f3959934a6d0c5c33622dfb60b0
Spine <spine@orpheus.network>
2022-02-13 14:23:03 +0000

    RemasterYear is null if absent

--- sections/upload/upload_handle.php
+++ sections/upload/upload_handle.php
@@ -26,13 +26,16 @@ if ($Properties['Remastered'] || !empty($_POST['unknown'])) {
     $Properties['UnknownRelease'] = !empty($_POST['unknown']) ? 1 : 0;
     $Properties['RemasterYear'] = trim($_POST['remaster_year'] ?? '');
     $_POST['remaster_year'] = $Properties['RemasterYear'];
+    if ($Properties['RemasterYear'] === '') {
+        $Properties['RemasterYear'] = null;
+    }
     $Properties['RemasterTitle'] = trim($_POST['remaster_title'] ?? '');
     $Properties['RemasterRecordLabel'] = trim($_POST['remaster_record_label'] ?? '');
     $Properties['RemasterCatalogueNumber'] = trim($_POST['remaster_catalogue_number'] ?? '');
 }
 if (!$Properties['Remastered'] || $Properties['UnknownRelease']) {
     $Properties['UnknownRelease'] = 1;
-    $Properties['RemasterYear'] = '';
+    $Properties['RemasterYear'] = null;
     $Properties['RemasterTitle'] = '';
     $Properties['RemasterRecordLabel'] = '';
     $Properties['RemasterCatalogueNumber'] = '';
