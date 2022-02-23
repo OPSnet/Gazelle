895487fc4987eecfee2f70be965adcb7fc434447
Spine <spine@orpheus.network>
2022-02-23 20:41:45 +0000

    steer around Mysql brane-damage

--- app/BaseObject.php
+++ app/BaseObject.php
@@ -8,7 +8,6 @@ abstract class BaseObject extends Base {
 
     /* used for handling updates */
     protected array $updateField = [];
-    protected array $updateFieldPassThru = [];
     protected array $updateFieldRaw = [];
 
     public function __construct(int $id) {
@@ -26,17 +25,11 @@ abstract class BaseObject extends Base {
 
     public function dirty(): bool {
         return !empty($this->updateField)
-            || !empty($this->updateFieldPassThru)
             || !empty($this->updateFieldRaw);
     }
 
     public function setUpdate(string $field, $value) {
         $this->updateField[$field] = $value;
-        return $this;
-    }
-
-    public function setUpdatePassThru(string $field, $value) {
-        $this->updateFieldPassThru[$field] = $value;
         return $this;
     }
 
@@ -55,18 +48,15 @@ abstract class BaseObject extends Base {
         }
         $set = implode(', ', array_merge(
             array_map(fn($f) => "$f = ?", array_keys($this->updateField)),
-            array_keys($this->updateFieldPassThru),
             $this->updateFieldRaw
         ));
         $args = array_merge(
             array_values($this->updateField),
-            array_values($this->updateFieldPassThru)
         );
         $args[] = $this->id;
         self::$db->prepared_query(
-            "UPDATE " . $this->tableName() . " SET
-                $set WHERE ID = ?
-            ", ...$args
+            "UPDATE " . $this->tableName() . " SET $set WHERE ID = ?",
+            ...$args
         );
         $success = (self::$db->affected_rows() === 1);
         if ($success) {
