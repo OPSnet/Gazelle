f374cb7f68bac37ae3682657b72b0869d386784e
Spine <spine@orpheus.network>
2022-02-27 11:27:29 +0000

    reports.Notes may be null

---
+++ db/migrations/20220227112327_reports_notes_null.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class ReportsNotesNull extends AbstractMigration {
+    public function up(): void {
+        $this->query("
+            ALTER TABLE reports MODIFY COLUMN Notes mediumtext NULL
+        ");
+    }
+
+    public function down(): void {
+        $this->query("
+            ALTER TABLE reports MODIFY COLUMN Notes mediumtext NOT NULL
+        ");
+    }
+}
