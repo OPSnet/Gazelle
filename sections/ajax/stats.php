1e59d30b057de52cecffc84fb05d2cda3d095cba
Spine <spine@orpheus.network>
2022-03-02 10:33:43 +0000

    calculate global seeder and leecher counts more quickly

--- sections/ajax/stats.php
+++ sections/ajax/stats.php
@@ -1,61 +1,47 @@
 <?php
 // Begin user stats
 
-$userMan = new Gazelle\Manager\User;
-$UserStats = $userMan->globalActivityStats();
+$userMan   = new Gazelle\Manager\User;
+$userStats = new Gazelle\Stats\Users;
+$torrStats = new Gazelle\Stats\Torrent;
+
+$UserStats = $userStats->globalActivityStats();
 $UserCount = $userMan->getEnabledUsersCount();
 
 // Begin request stats
 if (($RequestStats = $Cache->get_value('stats_requests')) === false) {
-    $RequestCount = $DB->scalar("
+    $RequestCount = (int)$DB->scalar("
         SELECT count(*) FROM requests
     ");
-    $FilledCount = $DB->scalar("
+    $FilledCount = (int)$DB->scalar("
         SELECT count(*) FROM requests WHERE FillerID > 0
     ");
     $Cache->cache_value('stats_requests', [$RequestCount, $FilledCount], 11280);
 } else {
-    list($RequestCount, $FilledCount) = $RequestStats;
+    [$RequestCount, $FilledCount] = $RequestStats;
 }
 
-// Begin swarm stats
-if (($PeerStats = $Cache->get_value('stats_peers')) === false) {
-    //Cache lock!
-    if ($Cache->get_query_lock('peer_stats')) {
-        $DB->prepared_query("
-            SELECT IF(remaining=0,'Seeding','Leeching') AS Type, COUNT(uid)
-            FROM xbt_files_users
-            WHERE active = 1
-            GROUP BY Type
-        ");
-        $PeerCount = $DB->to_array(0, MYSQLI_NUM, false);
-        $LeecherCount = isset($PeerCount['Leeching']) ? $PeerCount['Leeching'][1] : 0;
-        $SeederCount = isset($PeerCount['Seeding']) ? $PeerCount['Seeding'][1] : 0;
-        $Cache->cache_value('stats_peers', [$LeecherCount, $SeederCount], 1209600); // 2 week cache
-        $Cache->clear_query_lock('peer_stats');
-    } else {
-        $LeecherCount = $SeederCount = 0;
-    }
-} else {
-    list($LeecherCount, $SeederCount) = $PeerStats;
+$PeerStats = $Cache->get_value('stats_peers');
+if ($PeerStats === false) {
+    $userStats->frontPage();
+    $PeerStats = $Cache->get_value('stats_peers');
 }
 
-$torrentStatsMan = new Gazelle\Stats\Torrent;
 json_print("success", [
-    'maxUsers' => USER_LIMIT,
-    'enabledUsers' => (int) $UserCount,
-    'usersActiveThisDay' => (int) $UserStats['Day'],
-    'usersActiveThisWeek' => (int) $UserStats['Week'],
-    'usersActiveThisMonth' => (int) $UserStats['Month'],
+    'maxUsers'             => USER_LIMIT,
+    'enabledUsers'         => $UserCount,
+    'usersActiveThisDay'   => $UserStats['Day'],
+    'usersActiveThisWeek'  => $UserStats['Week'],
+    'usersActiveThisMonth' => $UserStats['Month'],
 
-    'torrentCount'     => $torrentStatsMan->torrentCount(),
-    'releaseCount'     => $torrentStatsMan->albumCount(),
-    'artistCount'      => $torrentStatsMan->artistCount(),
-    'perfectFlacCount' => $torrentStatsMan->perfectCount(),
+    'torrentCount'     => $torrStats->torrentCount(),
+    'releaseCount'     => $torrStats->albumCount(),
+    'artistCount'      => $torrStats->artistCount(),
+    'perfectFlacCount' => $torrStats->perfectCount(),
 
-    'requestCount' => (int) $RequestCount,
-    'filledRequestCount' => (int) $FilledCount,
+    'requestCount'       => $RequestCount,
+    'filledRequestCount' => $FilledCount,
 
-    'seederCount' => (int) $SeederCount,
-    'leecherCount' => (int) $LeecherCount
+    'seederCount'  => $PeerStats[0],
+    'leecherCount' => $PeerStats[1],
 ]);
