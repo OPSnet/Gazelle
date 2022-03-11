2f0c6c9d23085509ebbcde5aeda13f1760570796
Spine <spine@orpheus.network>
2022-03-11 21:56:38 +0000

    users_notify_filters.RecordLabels can be null

---
+++ db/migrations/20220311215012_user_notify_null_record_label.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class UserNotifyNullRecordLabel extends AbstractMigration {
+    public function up(): void {
+        $this->query("
+            ALTER TABLE users_notify_filters MODIFY RecordLabels text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+        ");
+    }
+
+    public function down(): void {
+        $this->query("
+            ALTER TABLE users_notify_filters MODIFY RecordLabels longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+        ");
+    }
+}
