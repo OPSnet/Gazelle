61d9eed67bdbdda0262fdc0e5f3909b277193d2a
Spine <spine@orpheus.network>
2022-03-26 13:40:30 +0000

    align sphinx_data column lengths with source tables

---
+++ db/migrations/20220325180536_align_sphinx_delta.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AlignSphinxDelta extends AbstractMigration {
+    public function up(): void {
+        $this->table('sphinx_delta')
+            ->changeColumn('GroupName', 'string', ['length' => 300])
+            ->changeColumn('Encoding', 'string', ['length' => 15, 'null' => true])
+            ->changeColumn('Format', 'string', ['length' => 10, 'null' => true])
+            ->changeColumn('Media', 'string', ['length' => 20])
+            ->changeColumn('TagList', 'string', ['length' => 500])
+            ->changeColumn('CatalogueNumber', 'string', ['length' => 80, 'null' => true])
+            ->changeColumn('RecordLabel', 'string', ['length' => 80, 'null' => true])
+            ->changeColumn('RemasterCatalogueNumber', 'string', ['length' => 80, 'null' => true])
+            ->changeColumn('RemasterRecordLabel', 'string', ['length' => 80, 'null' => true])
+            ->changeColumn('RemasterTitle', 'string', ['length' => 80, 'null' => true])
+            ->changeColumn('RemasterYear', 'integer', ['null' => true])
+            ->save();
+    }
+
+    public function down(): void {
+        $this->table('sphinx_delta')
+            ->changeColumn('GroupName', 'string', ['length' => 255])
+            ->changeColumn('Encoding', 'string', ['length' => 255, 'null' => true])
+            ->changeColumn('Format', 'string', ['length' => 255, 'null' => true])
+            ->changeColumn('Media', 'string', ['length' => 255, 'null' => true])
+            ->changeColumn('TagList', 'string', ['length' => 768, 'null' => true])
+            ->changeColumn('CatalogueNumber', 'string', ['length' => 50, 'null' => true])
+            ->changeColumn('RecordLabel', 'string', ['length' => 50, 'null' => true])
+            ->changeColumn('RemasterCatalogueNumber', 'string', ['length' => 50, 'null' => true])
+            ->changeColumn('RemasterRecordLabel', 'string', ['length' => 50, 'null' => true])
+            ->changeColumn('RemasterTitle', 'string', ['length' => 512, 'null' => true])
+            ->changeColumn('RemasterYear', 'string', ['length' => 50])
+            ->save();
+    }
+}
