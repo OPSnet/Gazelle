51f192b05ccb241a42318c4178ad27e304629222
Spine <spine@orpheus.network>
2022-03-27 21:09:36 +0000

    more sphix_delta fuckery

---
+++ db/migrations/20220327210720_sphinx_delta_group_name.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class SphinxDeltaGroupName extends AbstractMigration {
+    public function up(): void {
+        $this->table('sphinx_delta')
+            ->changeColumn('GroupName', 'string', ['length' => 300, 'null' => true])
+            ->save();
+    }
+
+    public function down(): void {
+        $this->table('sphinx_delta')
+            ->changeColumn('GroupName', 'string', ['length' => 300])
+            ->save();
+    }
+}
