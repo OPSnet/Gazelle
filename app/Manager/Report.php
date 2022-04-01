99da002174fc5221e44b595976fc041dacef53e2
Spine <spine@orpheus.network>
2022-04-01 16:39:08 +0000

    appify non-torrent reports

---
+++ app/Manager/Report.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Gazelle\Manager;
+
+class Report extends \Gazelle\Base {
+
+    protected const ID_KEY = 'zz_r_%d';
+
+    protected User $userMan;
+
+    public function setUserManager(User $userMan): \Gazelle\Manager\Report {
+        $this->userMan = $userMan;
+        return $this;
+    }
+
+    public function findById(int $reportId): ?\Gazelle\Report {
+        $key = sprintf(self::ID_KEY, $reportId);
+        $id = self::$cache->get_value($key);
+        if ($id === false) {
+            $id = self::$db->scalar("
+                SELECT ID FROM reports WHERE ID = ?
+                ", $reportId
+            );
+            if (!is_null($id)) {
+                self::$cache->cache_value($key, $id, 7200);
+            }
+        }
+        if (!$id) {
+            return $null;
+        }
+        $report = new \Gazelle\Report($id);
+        if (isset($this->userMan)) {
+            $report->setUserManager($this->userMan);
+        }
+        return $report;
+    }
+}
---
+++ app/Report.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Gazelle;
+
+class Report extends BaseObject {
+
+    protected array $info;
+    protected Manager\User $userMan;
+
+    public function tableName(): string { return 'reports'; }
+    public function flush() {}
+
+    public function url(): string {
+        return "reports.php?id={$this->id}#report{$this->id}";
+    }
+
+    public function link(): string {
+        return sprintf('<a href="%s">Report #%d</a>', $this->url(), $this->id());
+    }
+
+    public function setUserManager(Manager\User $userMan) {
+        $this->userMan = $userMan;
+        return $this;
+    }
+
+    public function info(): array {
+        return $this->info ??= self::$db->rowAssoc("
+            SELECT r.UserID    AS reporter_user_id,
+                r.ThingID      AS subject_id,
+                r.Type         AS subject_type,
+                r.ReportedTime AS created,
+                r.Reason       AS reason,
+                r.Status       AS status,
+                r.ClaimerID    AS claimer_user_id,
+                r.Notes        AS notes,
+                r.ResolverID   AS resolver_user_id,
+                r.ResolvedTime AS resolved_time
+            FROM reports AS r
+            WHERE r.ID = ?
+            ", $this->id
+        );
+    }
+
+    public function created(): string {
+        return $this->info()['created'];
+    }
+
+    public function notes(): ?string {
+        return $this->info()['notes'];
+    }
+
+    public function reason(): string {
+        return $this->info()['reason'];
+    }
+
+    public function resolved(): string {
+        return $this->info()['resolved_time'];
+    }
+
+    public function status(): string {
+        return $this->info()['status'];
+    }
+
+    public function subjectId(): int {
+        return $this->info()['subject_id'];
+    }
+
+    public function subjectType(): string {
+        return $this->info()['subject_type'];
+    }
+
+    public function isClaimed(): bool {
+        return (bool)$this->info()['claimer_user_id'];
+    }
+
+    public function claimer(): ?User {
+        return $this->userMan->findById((int)$this->info()['claimer_user_id']);
+    }
+
+    public function reporter(): ?User {
+        return $this->userMan->findById((int)$this->info()['reporter_user_id']);
+    }
+
+    public function resolver(): ?User {
+        return $this->userMan->findById((int)$this->info()['resolver_user_id']);
+    }
+
+    public function claim(int $userId): int {
+        self::$db->prepared_query("
+            UPDATE reports SET
+                ClaimerID = ?
+            WHERE ID = ?
+            ", $userId, $this->id
+        );
+        return self::$db->affected_rows();
+    }
+}
