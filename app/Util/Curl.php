a9fc10898b6924a447a326de8a99d48c45a3bfbb
Spine <spine@orpheus.network>
2022-04-02 16:32:11 +0000

    override the proxy and HTTP method

--- app/Util/Curl.php
+++ app/Util/Curl.php
@@ -2,39 +2,64 @@
 
 namespace Gazelle\Util;
 
+enum CurlMethod {
+    case GET;
+    case HEAD;
+    case POST;
+    case PUT;
+}
+
 class Curl {
 
     protected $curl;
     protected $result;
+    protected bool $useProxy = true;
+    protected CurlMethod $method = CurlMethod::GET;
 
     public function __construct() {
         $this->curl = curl_init();
-        curl_setopt_array($this->curl, [
-            CURLOPT_HEADER         => false,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_TIMEOUT        => 5,
-            CURLOPT_USERAGENT      => FAKE_USERAGENT,
-        ]);
-        if (HTTP_PROXY) {
-            curl_setopt_array($this->curl, [
-                CURLOPT_HTTPPROXYTUNNEL => true,
-                CURLOPT_PROXY => HTTP_PROXY,
-            ]);
-        }
     }
 
     public function __destruct() {
         curl_close($this->curl);
     }
 
+    public function setMethod(CurlMethod $method): Curl {
+        $this->method = $method;
+        return $this;
+    }
+
+    public function setUseProxy(bool $useProxy): Curl {
+        $this->useProxy = $useProxy;
+        return $this;
+    }
+
     public function fetch(string $url): bool {
-        curl_setopt($this->curl, CURLOPT_URL, $url);
+        if (HTTP_PROXY && $this->useProxy) {
+            curl_setopt_array($this->curl, [
+                CURLOPT_HTTPPROXYTUNNEL => true,
+                CURLOPT_PROXY           => HTTP_PROXY,
+            ]);
+        }
+        curl_setopt_array($this->curl, [
+            CURLOPT_HEADER         => false,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT        => 5,
+            CURLOPT_USERAGENT      => FAKE_USERAGENT,
+            CURLOPT_URL            => $url,
+            match ($this->method) {
+                CurlMethod::GET  => CURLOPT_HTTPGET,
+                CurlMethod::HEAD => CURLOPT_NOBODY,
+                CurlMethod::POST => CURLOPT_POST,
+                CurlMethod::PUT  => CURLOPT_PUT,
+            } => true,
+        ]);
         $this->result = curl_exec($this->curl);
         return $this->result !== false || $this->responseCode() === 200;
     }
 
-    public function result(): string {
+    public function result(): ?string {
         return $this->result;
     }
 
