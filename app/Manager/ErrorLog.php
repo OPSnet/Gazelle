08529d19610d28de2a2628bee9335255fcd7c874
Spine <spine@orpheus.network>
2022-04-15 18:37:37 +0000

    avoid burning PK ids on updates

--- app/Manager/ErrorLog.php
+++ app/Manager/ErrorLog.php
@@ -6,6 +6,36 @@ class ErrorLog extends \Gazelle\Base {
 
     protected string $filter;
 
+    public function create(
+       string $uri, int $userId, float $duration, int $memory, int $nrQuery, int $nrCache,
+       string $digest, string $trace, string $request, string $errorList, string $loggedVar
+    ): int {
+        self::$db->begin_transaction();
+        $id = self::$db->scalar("
+            SELECT error_log_id FROM error_log WHERE digest = ?
+            ", $digest
+        );
+        if ($id) {
+            self::$db->prepared_query("
+                UPDATE error_log SET
+                    updated = now(),
+                    seen = seen + 1
+                WHERE error_log_id = ?
+                ", $id
+            );
+        } else {
+            self::$db->prepared_query("
+                INSERT INTO error_log
+                       (uri, user_id, duration, memory, nr_query, nr_cache, digest, trace, request, error_list, logged_var)
+                VALUES (?,   ?,       ?,        ?,      ?,        ?,        ?,      ?,     ?,       ?,          ?)
+                ", $uri, $userId, $duration, $memory, $nrQuery, $nrCache, $digest, $trace, $request, $errorList, $loggedVar
+            );
+            $id = self::$db->inserted_id();
+        }
+        self::$db->commit();
+        return $id;
+    }
+
     /**
      * Get an eror log based on its ID
      */
