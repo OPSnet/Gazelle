a776a6f98beb0fb022fb493d6f8f3e1c02783965
Spine <spine@orpheus.network>
2022-05-18 17:27:35 +0000

    refactor bits of the comments page

--- sections/comments/comments.php
+++ sections/comments/comments.php
@@ -13,17 +13,6 @@
  *     If missing or invalid, this defaults to the comments one made
  */
 
-function linkBuild(string $id, string $action): string {
-    preg_match('/^(.*?)s?$/', $action, $match);
-    return sprintf('<a href="comments.php?action=%s%s" class="brackets">%s comments</a>',
-        $action, $id, ucfirst($match[1])
-    );
-}
-
-function createdBy(int $ownProfile, string $user, string $objects): string {
-    return $ownProfile ? "your $objects" : "$objects created by $user";
-}
-
 $userMan = new Gazelle\Manager\User;
 if (!isset($_GET['id'])) {
     $User = $Viewer;
@@ -52,32 +41,29 @@ $Type     = $_REQUEST['type'] ?? 'default';
 $BaseLink = "comments.php?action=$Action$linkId";
 
 // SQL components
-$condArgs   = [];
+$condArgs  = [];
 $condition = [];
-$Join     = [];
-$joinArgs = [];
+$Join      = [];
+$joinArgs  = [];
 
-$ActionLinks = [];
 $TypeLinks = [];
 
 switch ($Action) {
     case 'artist':
-        $Title = '%s &rsaquo; Artist comments';
-
+        $Title       = '%s &rsaquo; Artist comments';
         $table       = 'artists_group AS ag';
         $idField     = 'ag.ArtistID';
         $nameField   = 'ag.Name';
         $condition[] = "C.AuthorID = ?";
         $condArgs[]  = $UserID;
-
-        $ActionLinks = [
-            linkBuild($linkId, 'collages'),
-            linkBuild($linkId, 'requests'),
-            linkBuild($linkId, 'torrents'),
-        ];
         break;
 
     case 'collages':
+        $table       = 'collages AS cl';
+        $idField     = 'cl.ID';
+        $nameField   = 'cl.Name';
+        $condition[] = "cl.Deleted = '0'";
+
         switch ($Type) {
             case 'created':
                 $Title = "%s &rsaquo; Comments on their collages";
@@ -112,20 +98,13 @@ switch ($Action) {
                 ];
                 break;
         }
-
-        $table       = 'collages AS cl';
-        $idField     = 'cl.ID';
-        $nameField   = 'cl.Name';
-        $condition[] = "cl.Deleted = '0'";
-
-        $ActionLinks = [
-            linkBuild($linkId, 'artist'),
-            linkBuild($linkId, 'requests'),
-            linkBuild($linkId, 'torrents'),
-        ];
         break;
 
     case 'requests':
+        $table      = 'requests AS r';
+        $idField    = 'r.ID';
+        $nameField  = 'r.Title';
+
         switch($Type) {
             case 'created':
                 $Title = "%s &rsaquo; Comments on their requests";
@@ -154,25 +133,24 @@ switch ($Action) {
                 $Title = "%s &rsaquo; Request comments";
                 $condition[] = "C.AuthorID = ?";
                 $condArgs[] = $UserID;
-                $TypeLinks = [
-                    ["$BaseLink&amp;type=created", "$Username &rsaquo; Created request comments"],
-                    ["$BaseLink&amp;type=contributed", "$Username &rsaquo; Voted-on request comments"],
-                ];
+                $TypeLinks = $ownProfile
+                    ? [
+                        ["$BaseLink&amp;type=created", "Comments on your created requests"],
+                        ["$BaseLink&amp;type=contributed", "Your comments on requests"],
+                    ]
+                    : [
+                        ["$BaseLink&amp;type=created", "$Username &rsaquo; Created request comments"],
+                        ["$BaseLink&amp;type=contributed", "$Username &rsaquo; Request comments"],
+                    ];
                 break;
         }
-
-        $table      = 'requests AS r';
-        $idField    = 'r.ID';
-        $nameField  = 'r.Title';
-
-        $ActionLinks = [
-            linkBuild($linkId, 'artist'),
-            linkBuild($linkId, 'collages'),
-            linkBuild($linkId, 'torrents'),
-        ];
         break;
 
     case 'torrents':
+        $table     = 'torrents_group AS tg';
+        $idField   = 'tg.ID';
+        $nameField = 'tg.Name';
+
         switch($Type) {
             case 'uploaded':
                 $Title = "%s &rsaquo; Comments on their uploads";
@@ -182,25 +160,21 @@ switch ($Action) {
                 $condition[] = "t.UserID = ?";
                 $condArgs[] = $UserID;
                 $condArgs[] = $UserID;
-                $TypeLinks[] = [$BaseLink, "$Username &rsaquo; Torrent comments"];
+                $TypeLinks[] = [
+                    $BaseLink,
+                    $ownProfile ? "Your torrent comments" : "$Username &rsaquo; Torrent comments"
+                ];
                 break;
             default:
                 $Title = "%s &rsaquo; Torrent comments";
                 $condition[] = "C.AuthorID = ?";
                 $condArgs[] = $UserID;
-                $TypeLinks[] = ["$BaseLink&amp;type=uploaded", "$Username &rsaquo; Comments on their uploads"];
+                $TypeLinks[] = [
+                    "$BaseLink&amp;type=uploaded",
+                    $ownProfile ? "Comments on your uploads" : "$Username &rsaquo; Comments on their uploads"
+                ];
                 break;
         }
-
-        $table     = 'torrents_group AS tg';
-        $idField   = 'tg.ID';
-        $nameField = 'tg.Name';
-
-        $ActionLinks = [
-            linkBuild($linkId, 'artist'),
-            linkBuild($linkId, 'collages'),
-            linkBuild($linkId, 'requests'),
-        ];
         break;
 }
 
@@ -216,8 +190,6 @@ $paginator->setTotal(
         SELECT count(DISTINCT(C.ID)) FROM $table $Join $cond", ...array_merge($joinArgs, $condArgs)
     )
 );
-
-array_push($condArgs, $paginator->limit(), $paginator->offset());
 
 $Comments = $DB->prepared_query("
     SELECT C.AuthorID,
@@ -235,7 +207,7 @@ $Comments = $DB->prepared_query("
     GROUP BY C.ID
     ORDER BY C.ID DESC
     LIMIT ? OFFSET ?
-    ", ...array_merge($joinArgs, $condArgs)
+    ", ...[...$joinArgs, ...$condArgs, $paginator->limit(), $paginator->offset()]
 );
 
 if ($Action == 'requests') {
@@ -249,21 +221,24 @@ if ($Action == 'requests') {
     $Artists = Artists::get_artists($GroupIDs);
 }
 
-$Links = implode(' ', $ActionLinks)
-    . ($TypeLinks
-        ? (' <br />' . implode(' ', array_map(
-            function ($x) {
-                return sprintf('<a href="%s" class="brackets">%s</a>', $x[0], $x[1]);
-            }, $TypeLinks
-        )))
-        : ''
-    );
+$Links = implode(' ', 
+    // show links to the other types of pages having comments
+    array_map(fn($a) => "<a href=\"comments.php?action=$a$linkId\" class=\"brackets\">" . ucfirst(rtrim($a, 's')) . ' comments</a>', 
+        array_filter(['artist', 'collages', 'requests', 'torrents'], fn($a) => $a != $Action)
+    )
+);
+if ($TypeLinks) {
+    // and any extra links for this page type
+    $Links .= ' <br />' . implode(' ', array_map(
+        fn($link) => sprintf('<a href="%s" class="brackets">%s</a>', $link[0], $link[1]), $TypeLinks
+    ));
+}
 
 View::show_header(sprintf($Title, $Username), ['js' => 'bbcode,comments']);
 ?>
 <div class="thin">
     <div class="header">
-        <h2><?= sprintf($Title, Users::format_username($UserID, false, false, false)) ?></h2>
+        <h2><?= sprintf($Title, $User->link()) ?></h2>
 <?php if ($Links != '') { ?>
         <div class="linkbox">
             <?= $Links ?>
@@ -275,26 +250,11 @@ View::show_header(sprintf($Title, $Username), ['js' => 'bbcode,comments']);
 <?php
 } else {
     echo $paginator->linkbox();
-    $isAdmin = $Viewer->permitted('site_admin_forums');
     $commentMan = new Gazelle\Manager\Comment;
     $DB->set_query_id($Comments);
     while ([$AuthorID, $Page, $PageID, $Name, $PostID, $Body, $AddedTime, $EditedTime, $EditedUserID] = $DB->next_record()) {
         $author = new Gazelle\User($AuthorID);
-        $ownProfile = $AuthorID == $Viewer->id();
-        switch ($Page) {
-            case 'artist':
-                $heading = " on <a href=\"artist.php?id=$PageID\">$Name</a>";
-                break;
-            case 'collages':
-                $heading = " on <a href=\"collages.php?id=$PageID\">$Name</a>";
-                break;
-            case 'requests':
-                $heading = ' on ' . Artists::display_artists($Artists[$PageID]) . " <a href=\"requests.php?action=view&id=$PageID\">$Name</a>";
-                break;
-            case 'torrents':
-                $heading = ' on ' . Artists::display_artists($Artists[$PageID]) . " <a href=\"torrents.php?id=$PageID\">$Name</a>";
-                break;
-        }
+        $ownComment = $AuthorID == $Viewer->id();
         echo $Twig->render('comment/comment.twig', [
             'added_time'  => $AddedTime,
             'author'      => $author,
@@ -303,15 +263,17 @@ View::show_header(sprintf($Title, $Username), ['js' => 'bbcode,comments']);
             'editor'      => $userMan->findById((int)$EditedUserID),
             'edit_time'   => $EditedTime,
             'id'          => $PostID,
-            'is_admin'    => $isAdmin,
-            'heading'     => $heading,
-            'show_avatar' => $Viewer->showAvatars(),
-            'show_delete' => $Viewer->permitted('site_forum_post_delete'),
-            'show_edit'   => $Viewer->permitted('site_moderate_forums') || $ownProfile,
-            'show_warn'   => $Viewer->permitted('users_warn') && !$ownProfile && $Viewer->classLevel() >= $author->classLevel(),
-            'show_unread' => false,
+            'heading'     => match($Page) {
+                'artist'   => "<a href=\"artist.php?id=$PageID\">$Name</a>",
+                'collages' => "<a href=\"collages.php?id=$PageID\">$Name</a>",
+                'requests' => Artists::display_artists($Artists[$PageID]) . " <a href=\"requests.php?action=view&id=$PageID\">$Name</a>",
+                'torrents' => Artists::display_artists($Artists[$PageID]) . " <a href=\"torrents.php?id=$PageID\">$Name</a>",
+            },
+            'show_edit'   => $Viewer->permitted('site_moderate_forums') || $ownComment,
+            'show_warn'   => $Viewer->permitted('users_warn') && !$ownComment && $Viewer->classLevel() >= $author->classLevel(),
             'url'         => $commentMan->findById($PostID)->url(),
-            'page'        => $Action
+            'page'        => $Action,
+            'viewer'      => $Viewer,
         ]);
         $DB->set_query_id($Comments);
     }
