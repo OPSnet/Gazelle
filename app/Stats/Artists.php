653da5a33fc56f1d19418a027b34ecd0f04d0b06
Spine <spine@orpheus.network>
2022-08-09 19:55:43 +0000

    Recalculate artist usage in a Stats class

---
+++ app/Stats/Artists.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gazelle\Stats;
+
+class Artists extends \Gazelle\Base {
+
+    public function updateUsage(): int {
+        self::$db->begin_transaction();
+        self::$db->prepared_query("
+            DELETE FROM artist_usage
+        ");
+        self::$db->prepared_query("
+            INSERT IGNORE INTO artist_usage (artist_id, role, uses)
+            SELECT ArtistID, Importance, count(*) AS uses
+            FROM torrents_artists
+            GROUP BY ArtistID, Importance
+        ");
+        $affected = self::$db->affected_rows();
+        self::$db->commit();
+        return $affected;
+    }
+}
