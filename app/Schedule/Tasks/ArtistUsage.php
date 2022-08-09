e50e5e585680d8682a8f219aa7919f05a7118520
Spine <spine@orpheus.network>
2022-08-09 19:55:43 +0000

    Recalculate artist usage in a Stats class

--- app/Schedule/Tasks/ArtistUsage.php
+++ app/Schedule/Tasks/ArtistUsage.php
@@ -2,17 +2,8 @@
 
 namespace Gazelle\Schedule\Tasks;
 
-class ArtistUsage extends \Gazelle\Schedule\Task
-{
-    public function run()
-    {
-        self::$db->prepared_query('
-            INSERT INTO artist_usage (artist_id, role, uses)
-            SELECT ArtistID, Importance, count(*) AS uses
-            FROM torrents_artists
-            GROUP BY ArtistID, Importance
-            ON DUPLICATE KEY UPDATE uses = VALUES(uses)
-        ');
-        $this->processed = self::$db->affected_rows();
+class ArtistUsage extends \Gazelle\Schedule\Task {
+    public function run() {
+        $this->processed = (new \Gazelle\Stats\Artists)->updateUsage();
     }
 }
