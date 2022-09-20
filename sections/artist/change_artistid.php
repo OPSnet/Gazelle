47b8cb035804851e00d258f25867d5a9509ab31f
Spine <spine@orpheus.network>
2022-09-20 19:29:19 +0000

    twigify artist merge

--- sections/artist/change_artistid.php
+++ sections/artist/change_artistid.php
@@ -5,42 +5,41 @@ if (!$Viewer->permitted('torrents_edit')) {
 }
 authorize();
 
-$artist = (new Gazelle\Manager\Artist)->findById((int)$_POST['artistid']);
+$artMan = new Gazelle\Manager\Artist;
+$artist = $artMan->findById((int)($_POST['artistid'] ?? 0));
 if (is_null($artist)) {
     error('Please select a valid artist to change.');
 }
-$ArtistID = $artist->id();
-$ArtistName = $artist->name();
 
 if (!empty($_POST['newartistid']) && !empty($_POST['newartistname'])) {
     error('Please enter a valid artist ID number or a valid artist name.');
 }
-$NewArtistID = (int)$_POST['newartistid'];
-$NewArtistName = $_POST['newartistname'];
 
-if (empty($NewArtistName) && (!$NewArtistID || !is_number($NewArtistID))) {
+$newId = (int)($_POST['newartistid'] ?? 0);
+$newName = $_POST['newartistname'] ?? '';
+
+if (empty($newName) && !$newId) {
     error('Please enter a valid artist ID number or a valid artist name.');
 }
 
-if ($NewArtistID > 0) {
+if ($newId) {
     // Make sure that's a real artist ID number, and grab the name
-    $NewArtistName = $DB->scalar("
-        SELECT Name FROM artists_group WHERE ArtistID = ?
-        ", $NewArtistID
-    );
-    if (is_null($NewArtistName)) {
+    $new = $artMan->findById($newId);
+    if (is_null($new)) {
         error('Please enter a valid artist ID number.');
     }
 } else {
     // Didn't give an ID, so try to grab based on the name
-    $NewArtistID = $DB->scalar("
-        SELECT ArtistID FROM artists_alias WHERE Name = ?
-        ", $NewArtistName
-    );
-    if (is_null($NewArtistID)) {
+    $new = $artMan->findByName($newName);
+    if (is_null($new)) {
         error('No artist by that name was found.');
     }
 }
+
+$ArtistID = $artist->id();
+$ArtistName = $artist->name();
+$NewArtistID = $new->id();
+$NewArtistName = $new->name();
 
 if ($ArtistID == $NewArtistID) {
     error('You cannot merge an artist with itself.');
@@ -173,8 +172,7 @@ if (isset($_POST['confirm'])) {
     }
 
     $artist->flushCache();
-    $newArtist = new \Gazelle\Artist($NewArtistID);
-    $newArtist->flushCache();
+    $new->flushCache();
     $Cache->delete_value("artist_groups_$ArtistID");
     $Cache->delete_value("artist_groups_$NewArtistID");
 
@@ -192,22 +190,8 @@ if (isset($_POST['confirm'])) {
     exit;
 }
 
-View::show_header('Merging Artists');
-?>
-    <div class="header">
-        <h2>Confirm merge</h2>
-    </div>
-    <form class="merge_form" name="artist" action="artist.php" method="post">
-        <input type="hidden" name="action" value="change_artistid" />
-        <input type="hidden" name="auth" value="<?= $Viewer->auth() ?>" />
-        <input type="hidden" name="artistid" value="<?=$ArtistID?>" />
-        <input type="hidden" name="newartistid" value="<?=$NewArtistID?>" />
-        <input type="hidden" name="confirm" value="1" />
-        <div style="text-align: center;">
-            <p>Please confirm that you wish to make <a href="artist.php?id=<?=$ArtistID?>"><?=display_str($ArtistName)?> (<?=$ArtistID?>)</a> into a non-redirecting alias of <a href="artist.php?id=<?=$NewArtistID?>"><?=display_str($NewArtistName)?> (<?=$NewArtistID?>)</a>.</p>
-            <br />
-            <input type="submit" value="Confirm" />
-        </div>
-    </form>
-<?php
-View::show_footer();
+echo $Twig->render('artist/merge.twig', [
+    'artist'   => $artist,
+    'new'      => $new,
+    'viewer'   => $Viewer,
+]);
