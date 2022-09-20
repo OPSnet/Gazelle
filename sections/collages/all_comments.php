e08c7dfdef2725749a8cd708358904ec46a7c1bb
Spine <spine@orpheus.network>
2022-09-20 17:20:10 +0000

    twigify collage comments

--- sections/collages/all_comments.php
+++ sections/collages/all_comments.php
@@ -1,17 +1,11 @@
 <?php
 
-/* Things to expect in $_GET:
- *     CollageID: ID of the collage curently being browsed
- *     page:    The page the user's on.
- *     page = 1 is the same as no page
- */
-
-$CollageID = (int)$_GET['collageid'];
-if ($CollageID < 1) {
+$collage = new Gazelle\Collage((int)($_GET['collageid'] ?? 0));
+if (is_null($collage)) {
     error(404);
 }
 
-$commentPage = new Gazelle\Comment\Collage($CollageID);
+$commentPage = new Gazelle\Comment\Collage($collage->id());
 if (isset($_GET['postid'])) {
     $commentPage->setPostId((int)$_GET['postid']);
 } elseif (isset($_GET['page'])) {
@@ -22,43 +16,16 @@ $commentPage->load()->handleSubscription($Viewer);
 $paginator = new Gazelle\Util\Paginator(TORRENT_COMMENTS_PER_PAGE, $commentPage->pageNum());
 $paginator->setAnchor('comments')->setTotal($commentPage->total())->removeParam('postid');
 
-$isSubscribed = (new Gazelle\Subscription($Viewer))->isSubscribedComments('collages', $CollageID);
-$Collage = new Gazelle\Collage($CollageID);
-
-View::show_header("Comments for collage " . $Collage->name(), ['js' => 'comments,bbcode,subscriptions']);
-?>
-<div class="thin">
-    <div class="header">
-        <h2><a href="collages.php">Collages</a> &rsaquo; <?= $Collage->link() ?></h2>
-        <div class="linkbox">
-            <a href="#" id="subscribelink_collages<?=$CollageID?>" class="brackets" onclick="SubscribeComments('collages', <?=$CollageID?>); return false;"><?=
-                $isSubscribed ? 'Unsubscribe' : 'Subscribe'?></a>
-        </div>
-    </div>
-<?php
-echo $Twig->render('comment/thread.twig', [
-    'url'       => $_SERVER['REQUEST_URI'],
-    'page'      => $commentPage->page(),
-    'thread'    => $commentPage->thread(),
-    'unread'    => $commentPage->lastRead(),
-    'paginator' => $paginator,
-    'userMan'   => $userMan,
-    'viewer'    => $Viewer,
-]);
 $textarea = new Gazelle\Util\Textarea('quickpost', '', 90, 8);
 $textarea->setPreviewManual(true);
-echo $Twig->render('reply.twig', [
-    'action'   => 'take_post',
-    'auth'     => $Viewer->auth(),
-    'avatar'   => (new Gazelle\Manager\User)->avatarMarkup($Viewer, $Viewer),
-    'id'       => $CollageID,
-    'name'     => 'pageid',
-    'subbed'   => $isSubscribed,
-    'textarea' => $textarea,
-    'url'      => 'comments.php?page=collages',
-    'user'     => $Viewer,
+
+echo $Twig->render('collage/comment.twig', [
+    'avatar'        => (new Gazelle\Manager\User)->avatarMarkup($Viewer, $Viewer),
+    'collage'       => $collage,
+    'comment'       => $commentPage,
+    'is_subscribed' => (new Gazelle\Subscription($Viewer))->isSubscribedComments('collages', $collage->id()),
+    'paginator'     => $paginator,
+    'textarea'      => $textarea,
+    'url'           => $_SERVER['REQUEST_URI'],
+    'viewer'        => $Viewer,
 ]);
-?>
-</div>
-<?php
-View::show_footer();
