b80f7eb1820738d2568183a114ce4bcfe2c36b9c
Spine <spine@orpheus.network>
2022-09-21 22:05:18 +0000

    twigify recovery browse

--- sections/recovery/browse.php
+++ sections/recovery/browse.php
@@ -8,99 +8,20 @@ $recovery = new Gazelle\Recovery;
 if (isset($_POST['username']) && strlen($_POST['username'])) {
     $class = 'username';
     $target = trim($_POST['username']);
-    $List = $recovery->findByUsername($target);
-}
-elseif (isset($_POST['email']) && strlen($_POST['email'])) {
+    $list = $recovery->findByUsername($target);
+} elseif (isset($_POST['email']) && strlen($_POST['email'])) {
     $class = 'email';
     $target = trim($_POST['email']);
-    $List = $recovery->findByEmail($target);
-}
-elseif (isset($_POST['announce']) && strlen($_POST['announce'])) {
+    $list = $recovery->findByEmail($target);
+} elseif (isset($_POST['announce']) && strlen($_POST['announce'])) {
     $class = 'announce';
     $target = trim($_POST['announce']);
-    $List = $recovery->findByAnnounce($target);
+    $list = $recovery->findByAnnounce($target);
 }
 
-View::show_header('Recovery browse users');
-?>
-<div class="thin">
-
-<div class="linkbox">
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=pending">Pending</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=validated">Validated</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=accepted">Accepted</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=denied">Denied</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=claimed">Your claimed</a>
-    <a class="brackets" href="/recovery.php?action=browse">Browse</a>
-    <a class="brackets" href="/recovery.php?action=pair">Pair</a>
-</div>
-
-<?php if (isset($List)) { ?>
-<div class="box pad">
-<?php
-    if (!count($List)) {
-        echo "<p>No $class matched <b>$target</b></p>";
-    }
-    else {
-?>
-<div class="head">Users matched search criteria</div>
-<p>The following <?= $class ?> match <b><?= $target ?></b> in the previous site</p>
-
-<table>
-<tr>
-<th>Username</th>
-<th>ID</th>
-<th>Email</th>
-<th>Uploaded</th>
-<th>Downloaded</th>
-<th>Enabled</th>
-<th>Torrents</th>
-<th>Announce</th>
-</tr>
-
-<?php foreach ($List as $r) { ?>
-<tr>
-<td><?= $r['Username'] ?></td>
-<td><?= $r['UserID'] ?></td>
-<td><?= $r['Email'] ?></td>
-<td title="<?= $r['Uploaded'] ?>"><?= Format::get_size($r['Uploaded']) ?></td>
-<td title="<?= $r['Downloaded'] ?>"><?= Format::get_size($r['Downloaded']) ?></td>
-<td><?= $r['Enabled'] ?></td>
-<td><?= $r['nr_torrents'] ?></td>
-<td><?= $r['torrent_pass'] ?></td>
-</tr>
-<?php } /* foreach */ ?>
-</table>
-<?php } /* count() */ ?>
-</div>
-<?php } /* isset() */ ?>
-
-<div class="box">
-    <div class="head">Browse recovery details</div>
-
-    <div class="pad">
-        <form method="post" action="/recovery.php?action=browse">
-        <p>Enter one of the following fields to search for members in the backup (Use <tt>%</tt> as a wildcard character, <i>e.g.</i> <tt>C17%</tt>).</p>
-        <table>
-            <tr>
-                <th>Username</th>
-                <td><input type="text" name="username" width="20" /></td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td><input type="text" name="email" width="20" /></td>
-            </tr>
-            <tr>
-                <th>Announce</th>
-                <td><input type="text" name="announce" width="20" /></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" value="Browse" /></td>
-            </tr>
-        </table>
-        </form>
-    </div>
-</div>
-<?php
-View::show_footer();
+echo $Twig->render('recovery/browse.twig', [
+    'class'  => $class ?? '',
+    'list'   => $list ?? [],
+    'target' => $target ?? '',
+    'viewer' => $Viewer,
+]);
--- sections/recovery/view.php
+++ sections/recovery/view.php
@@ -6,7 +6,7 @@ if (!$Viewer->permitted('admin_recovery')) {
 $recovery = new Gazelle\Recovery;
 
 if (isset($_GET['id']) && (int)$_GET['id'] > 0) {
-    $ID = (int)$_GET['id'];
+    $id = (int)$_GET['id'];
     $search = false;
 } elseif (isset($_GET['action']) && $_GET['action'] == 'search') {
     $search = true;
@@ -14,158 +14,31 @@ if (isset($_GET['id']) && (int)$_GET['id'] > 0) {
     error(404);
 }
 
+$terms = [];
 if ($search) {
-    $terms = [];
-    foreach (explode(' ', 'token username email announce') as $key) {
+    foreach (['token', 'username', 'email', 'announce'] as $key) {
         if (isset($_GET[$key])) {
             $terms[] = [$key => $_GET[$key]];
         }
     }
-    $Info = $recovery->search($terms);
-    $ID = $Info['recovery_id'];
+    $info = $recovery->search($terms);
+    $id = $info['recovery_id'];
 } else {
     if (isset($_GET['claim']) and (int)$_GET['claim'] > 0) {
-        $claim_id = (int)$_GET['claim'];
-        if ($claim_id == $Viewer->id()) {
-            $recovery->claim($ID, $claim_id, $Viewer->username());
+        $claimId = (int)$_GET['claim'];
+        if ($claimId == $Viewer->id()) {
+            $recovery->claim($id, $claimId, $Viewer->username());
         }
     }
-    $Info = $recovery->getDetails($ID);
+    $info = $recovery->info($id);
 }
 
-$Email = ($Info['email'] == $Info['email_clean'])
-    ? $Info['email']
-    : $Info['email_clean'] . "<br />(cleaned from " . $Info['email'] . ")";
-
-$Candidate = $recovery->getCandidate($Info['username']);
 $enabled = ['Unconfirmed', 'Enabled', 'Disabled'];
-?>
 
-View::show_header('Recovery view user');
-<div class="thin">
-
-<div class="linkbox">
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=pending">Pending</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=validated">Validated</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=accepted">Accepted</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=denied">Denied</a>
-    <a class="brackets" href="/recovery.php?action=admin&amp;state=claimed">Your claimed</a>
-    <a class="brackets" href="/recovery.php?action=browse">Browse</a>
-    <a class="brackets" href="/recovery.php?action=pair">Pair</a>
-</div>
-
-<?php
-if (!$Info) { ?>
-<h3>Nobody home</h3>
-
-<p>No recovery request matched the search terms<p>
-<blockquote>
-<ul>
-<?php
-    foreach ($terms as $t) {
-        foreach ($t as $field => $value) {
-            echo "<li>$field: <tt>$value</tt></li>";
-        }
-    }
-?>
-</ul>
-</blockquote>
-
-<?php
-} else {
-    $userMan = new Gazelle\Manager\User;
-?>
-
-<h3>View recovery details for <?= $Info['username'] ?></h3>
-
-<div class="box">
-    <div class="head">Registration details</div>
-    <div class="pad">
-        <table>
-            <tr>
-                <th>Username</th>
-                <td><?= $Info['username'] ?></td>
-                <td><?= count($Candidate) ? '<font color="#008000">Username matches (ID=' . $Candidate['UserID'] . ')</font>' : '<font color="#800000">No match for username</font>' ?></td>
-                <th>state</th>
-                <td><?= $Info['state'] ?></td>
-            </tr>
-<?php if (count($Candidate)) { ?>
-            <tr>
-                <td><?= $enabled[$Candidate['Enabled']] ?></td>
-                <td><?= $userMan->userclassName($Candidate['PermissionID']) ?></th>
-                <td><?= $Candidate['nr_torrents'] ?> torrents</td>
-                <td><?= Format::get_size($Candidate['Uploaded']) ?> up</td>
-                <td><?= Format::get_size($Candidate['Downloaded']) ?> down</td>
-            </tr>
-<?php } ?>
-            <tr>
-                <th>Password verified</th>
-                <td colspan="2"><?= $Info['password_ok'] ? 'Yes' : 'No' ?></td>
-                <th>Claimed by</th>
-                <td><?= $Info['admin_user_id'] ? Users::format_username($Info['admin_user_id']) : 'nobody' ?></td>
-            </tr>
-            <tr>
-                <th>email</th>
-                <td><?= $Email ?></td>
-                <td><?= $Candidate['Email'] ?></td>
-                <th>Created</th>
-                <td><?= time_diff($Info['created_dt']) ?></td>
-            </tr>
-            <tr>
-                <th>Announce key</th>
-                <td><?= $Info['announce'] ?></td>
-                <td><?= $Candidate['torrent_pass'] ?></td>
-                <th>Updated</th>
-                <td><?= time_diff($Info['updated_dt']) ?></td>
-            </tr>
-            <tr>
-                <th>Remote IP</th>
-                <td><?= $Info['ipaddr'] ?></td>
-                <td><?= str_replace(',', '<br />', $Candidate['ips']) ?></td>
-                <th>Token</th>
-                <td><tt><?= $Info['token'] ?></tt></td>
-            </tr>
-            <tr>
-                <th>Screenshot info</th>
-                <td colspan="4"><?= $Info['screenshot'] ?></td>
-            </tr>
-            <tr>
-                <th>Invitation email</th>
-                <td colspan="4"><pre><?= $Info['invite'] ?></pre></td>
-            </tr>
-            <tr>
-                <th>info</th>
-                <td colspan="4"><pre><?= $Info['info'] ?></pre></td>
-            </tr>
-            <tr>
-                <th>log</th>
-                <td colspan="4"><pre><?= $Info['log'] ?><pre></td>
-            </tr>
-        </table>
-<?php
-    if (in_array($Info['state'], ['PENDING', 'VALIDATED'])) { ?>
-        <h2>Actions</h2>
-<?php   if (in_array($Info['state'], ['PENDING', 'VALIDATED'])) { ?>
-        <p><a class="brackets" href="/recovery.php?action=admin&amp;task=accept&amp;id=<?= $ID ?>">Accept</a> - An invite will be emailed to the user</p>
-        <p><a class="brackets" href="/recovery.php?action=admin&amp;task=deny&amp;id=<?= $ID ?>">Deny</a> - The request is denied, no e-mail will be sent</p>
-<?php
-        }
-        if ($Info['admin_user_id'] == $Viewer->id()) {
-?>
-        <p><a class="brackets" href="/recovery.php?action=admin&amp;task=unclaim&amp;id=<?= $ID ?>">Unclaim</a> - Release the claim on this request, you don't know what to do.</p>
-<?php
-        } else {
-?>
-        <p><a class="brackets" href="/recovery.php?action=view&amp;id=<?= $ID ?>&amp;claim=<?= $Viewer->id() ?>">Claim</a> - Claim this request, you need to contact the person via IRC.</p>
-<?php
-        }
-    }
-?>
-    </div>
-</div>
-
-<?php
-} /* $Info */ ?>
-</div>
-<?php
-View::show_footer();
+echo $Twig->render('recovery/view.twig', [
+    'candidate' => $recovery->findCandidate($info['username']),
+    'id'        => $id,
+    'info'      => $info,
+    'terms'     => $terms,
+    'viewer'    => $Viewer,
+]);
