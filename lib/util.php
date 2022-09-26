2dc57fefd34477d56f9cd8c0bc076e37ded9f8f3
Spine <spine@orpheus.network>
2022-09-26 17:54:59 +0000

    PCRE comments are Perl comments, not PHP comments

--- lib/util.php
+++ lib/util.php
@@ -332,22 +332,21 @@ function placeholders(array $list, $placeholder = '?') {
 }
 
 /**
- * Magical function.
- *
- * @param string $Str function to detect encoding on.
- * @return bool true if the string is in UTF-8.
+ * Parse a string to determine if it valid UTF-8
  */
-function is_utf8($Str) {
-    return (bool)preg_match('%^(?:
-        [\x09\x0A\x0D\x20-\x7E]              // ASCII
-        | [\xC2-\xDF][\x80-\xBF]             // non-overlong 2-byte
-        | \xE0[\xA0-\xBF][\x80-\xBF]         // excluding overlongs
-        | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  // straight 3-byte
-        | \xED[\x80-\x9F][\x80-\xBF]         // excluding surrogates
-        | \xF0[\x90-\xBF][\x80-\xBF]{2}      // planes 1-3
-        | [\xF1-\xF3][\x80-\xBF]{3}          // planes 4-15
-        | \xF4[\x80-\x8F][\x80-\xBF]{2}      // plane 16
-        )*$%xs', $Str
+function is_utf8(string $s): bool {
+    return (bool)preg_match('/
+        ^(?:
+            [\x09\x0A\x0D\x20-\x7E]            # ASCII
+          | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+          | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+          | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+          | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+          | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+          | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+          | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+        )*$
+        /xs', $s
     );
 }
 
@@ -362,8 +361,7 @@ function make_utf8($str) {
         return $str;
     }
     $encoding = mb_detect_encoding($str, 'UTF-8, ISO-8859-1', true);
-    return $encoding === 'ISO-8859-1'
-        ? @mb_convert_encoding($str, 'UTF-8', $encoding) : $str;
+    return $encoding === 'ISO-8859-1' ? @mb_convert_encoding($str, 'UTF-8', $encoding) : $str;
 }
 
 /**
