4b286da20870d0be535d1b73e64d20cda3bf540c
Spine <spine@orpheus.network>
2022-10-10 07:45:30 +0000

    twigify ajax post history

--- sections/ajax/post_edit.php
+++ sections/ajax/post_edit.php
@@ -4,49 +4,38 @@ if (!$Viewer->permitted('site_admin_forums')) {
     error(403);
 }
 
-$PostID = (int)$_GET['postid'];
-if (!$PostID) {
-    die();
+$postId = (int)($_GET['postid'] ?? 0);
+if (!$postId) {
+    die("bad id");
 }
-if (!in_array($_GET['type'], ['forums', 'collages', 'requests', 'torrents', 'artist'])) {
-    die();
+$pageType = ($_GET['type'] ?? '');
+if (!in_array($pageType, ['forums', 'collages', 'requests', 'torrents', 'artist'])) {
+    die("bad pagetype");
 }
-if ($_GET['depth'] != (int)$_GET['depth']) {
-    die();
+$depth = (int)($_GET['depth'] ?? 0);
+if ($_GET['depth'] != $depth) {
+    die("bad depth");
 }
-$Depth = (int)$_GET['depth'];
 
 $commentMan = new Gazelle\Manager\Comment;
-$Edits = $commentMan->loadEdits($_GET['type'], $PostID);
+$history = $commentMan->loadEdits($pageType, $postId);
 
-[$UserID, $Time] = $Edits[$Depth];
-if ($Depth != 0) {
-    $Body = $Edits[$Depth - 1][2];
+[$userId, $editTime] = $history[$depth];
+if ($depth != 0) {
+    $body = $history[$depth - 1][2];
 } else {
-    //Not an edit, have to get from the original
-    $Body = match($_GET['type']) {
-        'forums' => (new Gazelle\Manager\ForumPost)->findById($PostID)->body(),
-        default  => $commentMan->findById($PostID)->body(),
+    $body = match($pageType) {
+        'forums' => (new Gazelle\Manager\ForumPost)->findById($postId)->body(),
+        default  => $commentMan->findById($postId)->body(),
     };
 }
-?>
-                <?=Text::full_format($Body)?>
-                <br />
-                <br />
-                <span class="last_edited">
-<?php if ($Depth < count($Edits)) { ?>
 
-                    <a href="#edit_info_<?=$PostID?>" onclick="LoadEdit('<?= $_GET['type'] ?>', <?=$PostID?>, <?=($Depth + 1)?>); return false;">&laquo;</a>
-                    <?=(($Depth == 0) ? 'Last edited by' : 'Edited by')?>
-                    <?=Users::format_username($UserID, false, false, false) ?> <?=time_diff($Time, 2)?>
-
-<?php } else { ?>
-                    <em>Original Post</em>
-<?php
-}
-
-if ($Depth > 0) {
-?>
-                    <a href="#edit_info_<?=$PostID?>" onclick="LoadEdit('<?= $_GET['type'] ?>', <?=$PostID?>, <?=($Depth - 1)?>); return false;">&raquo;</a>
-<?php } ?>
-                </span>
+echo $Twig->render('post.twig', [
+    'body'      => $body,
+    'depth'     => $depth,
+    'edit_time' => $editTime,
+    'post_id'   => $postId,
+    'initial'   => $depth === count($history),
+    'page_type' => $pageType,
+    'user_id'   => $userId,
+]);
