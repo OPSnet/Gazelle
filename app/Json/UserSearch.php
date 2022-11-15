75550013324636bcee1826404eb267008b1de3f8
Spine <spine@orpheus.network>
2022-11-15 12:02:59 +0000

    jsonify ajax user search endpoint

---
+++ app/Json/UserSearch.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Gazelle\Json;
+
+class UserSearch extends \Gazelle\Json {
+    public function __construct(
+        protected string                  $search,
+        protected \Gazelle\User           $viewer,
+        protected \Gazelle\Manager\User   $manager,
+        protected \Gazelle\Util\Paginator $paginator,
+    ) {}
+
+    public function payload(): array {
+        $condition = $this->viewer->permitted('site_advanced_search')
+            ? "Username LIKE concat('%', ?, '%')"
+            : "Username = ?";
+
+        self::$db->prepared_query("
+            SELECT um.ID
+            FROM users_main AS um
+            WHERE $condition
+            ORDER BY Username
+            LIMIT ? OFFSET ?
+            ", $this->search, $this->paginator->limit(), $this->paginator->offset()
+        );
+
+        $payload = [];
+        foreach (self::$db->collect(0, false) as $userId) {
+            $user = $this->manager->findById($userId);
+            $payload[] = [
+                'userId'   => $user->id(),
+                'username' => $user->username(),
+                'donor'    => (new \Gazelle\User\Privilege($user))->isDonor(),
+                'warned'   => $user->isWarned(),
+                'enabled'  => $user->isEnabled(),
+                'class'    => $user->userclassName(),
+                'avatar'   => $user->avatar(),
+            ];
+        }
+
+        return [
+            'results'     => $payload,
+            'currentPage' => $this->paginator->page(),
+            'pages'       => (int)self::$db->scalar("
+                SELECT ceil(count(*) / ?) as pages FROM users_main WHERE $condition
+                ", AJAX_USERS_PER_PAGE, $this->search
+            ),
+        ];
+    }
+}
--- sections/ajax/usersearch.php
+++ sections/ajax/usersearch.php
@@ -1,49 +1,16 @@
 <?php
 
-use Gazelle\Util\Paginator;
 
 $search = trim($_GET['search'] ?? '');
 if (!strlen($search)) {
     json_die("failure", "no search terms");
 }
 
-$condition = $Viewer->permitted('site_advanced_search')
-    ? "Username LIKE concat('%', ?, '%')"
-    : "Username = ?";
-
-$total = $DB->scalar("
-    SELECT count(*) FROM users_main WHERE $condition
-    ", $search
-);
-
-$paginator = new Paginator(AJAX_USERS_PER_PAGE, (int)($_GET['page'] ?? 1));
-$DB->prepared_query("
-    SELECT um.ID
-    FROM users_main AS um
-    WHERE $condition
-    ORDER BY Username
-    LIMIT ? OFFSET ?
-    ", $search, $paginator->limit(), $paginator->offset()
-);
-$userIds = $DB->collect(0, false);
-
-$payload = [];
-$userMan = new Gazelle\Manager\User;
-foreach ($userIds as $userId) {
-    $user = $userMan->findById($userId);
-    $payload[] = [
-        'userId'   => $user->id(),
-        'username' => $user->username(),
-        'donor'    => (new Gazelle\User\Privilege($user))->isDonor(),
-        'warned'   => $user->isWarned(),
-        'enabled'  => $user->isEnabled(),
-        'class'    => $user->userclassName(),
-        'avatar'   => $user->avatar(),
-    ];
-}
-
-json_print("success", [
-    'currentPage' => $paginator->page(),
-    'pages'       => (int)ceil($total / AJAX_USERS_PER_PAGE),
-    'results'     => $payload,
-]);
+(new Gazelle\Json\UserSearch(
+    $search,
+    $Viewer,
+    new Gazelle\Manager\User,
+    new Gazelle\Util\Paginator(AJAX_USERS_PER_PAGE, (int)($_GET['page'] ?? 1)),
+))
+    ->setVersion(2)
+    ->emit();
