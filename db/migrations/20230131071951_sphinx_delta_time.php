6c412ecc9990671b8f6d8bc4cd555a77298c5ceb
Spine <spine@orpheus.network>
2023-01-30 17:23:35 +0000

    align docker with production

---
+++ db/migrations/20230131071951_sphinx_delta_time.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class SphinxDeltaTime extends AbstractMigration {
+    public function up(): void {
+        $this->query('ALTER TABLE sphinx_delta MODIFY Time datetime DEFAULT NULL');
+    }
+
+    public function down(): void {
+        $this->query('ALTER TABLE sphinx_delta MODIFY Time timestamp DEFAULT NULL');
+    }
+}
