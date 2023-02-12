4c81a9da4b8b2b610e60990b95c1e7764d803a9f
Spine <spine@orpheus.network>
2023-02-12 18:32:44 +0000

    simplify get_host endpoint

--- sections/tools/services/get_host.php
+++ sections/tools/services/get_host.php
@@ -1,26 +1,18 @@
 <?php
 
 if (!$Viewer->permitted('users_view_ips')) {
-    die('Access denied.');
+    echo 'Forbidden';
+    exit;
 }
 
 if (isset($_SERVER['http_if_modified_since'])) {
     header('Status: 304 Not Modified');
-    die();
+    exit;
 }
 
-header('Expires: '.date('D, d-M-Y H:i:s \U\T\C', time() + 3600 * 24 * 120)); //120 days
-header('Last-Modified: '.date('D, d-M-Y H:i:s \U\T\C', time()));
-
-if ($_GET['ip'] != long2ip(ip2long($_GET['ip']))) {
-    die('Invalid IPv4 address.');
+$hostname = gethostbyaddr(trim($_GET['ip'] ?? ''));
+if ($hostname === false) {
+    header('Expires: ' . date('D, d-M-Y H:i:s \U\T\C', time() + 3600 * 24 * 120)); // 120 days
+    header('Last-Modified: ' . date('D, d-M-Y H:i:s \U\T\C', time()));
 }
-
-$Output = explode(' ', shell_exec('host -W 1 ' . escapeshellarg($_GET['ip'])));
-if (count($Output) == 1 && empty($Output[0])) {
-    trigger_error('no output received: ensure that "host -W" functions correctly');
-} elseif (count($Output) != 5) {
-    print 'Could not retrieve host.';
-} else {
-    echo trim($Output[4]);
-}
+echo $hostname;
