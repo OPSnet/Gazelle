4129cf9063910d9fd23fa6681fbb6c4c03dc58bb
Spine <spine@orpheus.network>
2023-03-02 04:27:42 +0000

    drop unused api_tokens.scope column

---
+++ db/migrations/20230302000000_api_token_drop_scope.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+final class ApiTokenDropScope extends AbstractMigration {
+    public function up(): void {
+        $this->table('api_tokens')->removeColumn('scope')->save();
+    }
+
+    public function down(): void {
+        $this->table('users_info')->addColumn('scope', 'text', ['limit' => MysqlAdapter::TEXT_MEDIUM])->save();
+    }
+}
