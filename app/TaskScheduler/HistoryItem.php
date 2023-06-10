ab4e4216769f5b8a9336d1d428a0c5683aab2826
Spine <spine@orpheus.network>
2023-06-10 21:55:57 +0000

    task history fields are public readonly

--- app/TaskScheduler/HistoryItem.php
+++ app/TaskScheduler/HistoryItem.php
@@ -4,11 +4,11 @@ namespace Gazelle\TaskScheduler;
 
 class HistoryItem {
     public function __construct(
-        protected string $launchTime,
-        protected string $status,
-        protected int $numErrors,
-        protected int $numItems,
-        protected int $duration,
-        protected array $events = [],
+        public readonly string $launchTime,
+        public readonly string $status,
+        public readonly int $numErrors,
+        public readonly int $numItems,
+        public readonly int $duration,
+        public readonly array $events = [],
     ) {}
 }
