4e8777291ecf0e85cd48a0dd96186095c80aee66
Spine <spine@orpheus.network>
2023-10-25 02:29:24 +0000

    simplify top10 tag logic to prepare for Twig

--- sections/top10/tags.php
+++ sections/top10/tags.php
@@ -7,7 +7,7 @@ $details = in_array($_GET['details'] ?? '', ['top_used', 'top_request', 'top_vot
 $limit = $_GET['limit'] ?? 10;
 $limit = in_array($limit, [10, 100, 250]) ? $limit : 10;
 
-$tag = new Gazelle\Top10\Tag;
+$tag = new Gazelle\Manager\Tag;
 
 View::show_header(TOP_TEN_HEADING . " – Tags");
 ?>
@@ -19,60 +19,39 @@ View::show_header(TOP_TEN_HEADING . " – Tags");
 
 <?php
 
-if ($details == 'all' || $details == 'top_used') {
-    $topUsedTags = $tag->getTopUsedTags($limit);
-
-    generate_tag_table('Most Used Torrent Tags', 'top_used', $topUsedTags, $limit);
+if (in_array($details, ['all', 'top_used'])) {
+    generate_tag_table('Most Used Torrent Tags', 'top_used', $tag->topTGroupList($limit), $limit);
 }
 
-if ($details == 'all' || $details == 'top_request') {
-    $topRequestTags = $tag->getTopRequestTags($limit);
-
-    generate_tag_table('Most Used Request Tags', 'top_request', $topRequestTags, $limit, false, true);
+if (in_array($details, ['all', 'top_request'])) {
+    generate_tag_table('Most Used Request Tags', 'top_request', $tag->topRequestList($limit), $limit, request: true);
 }
 
-if ($details == 'all' || $details == 'top_voted') {
-    $topVotedTags = $tag->getTopVotedTags($limit);
-
-    generate_tag_table('Most Highly Voted Tags', 'top_voted', $topVotedTags, $limit);
+if (in_array($details, ['all', 'top_voted'])) {
+    generate_tag_table('Most Highly Voted Tags', 'top_voted', $tag->topVotedList($limit), $limit);
 }
 
 echo '</div>';
 View::show_footer();
 
 // generate a table based on data from most recent query
-function generate_tag_table(string $caption, string $tag, array $details, int $limit, bool $showVotes = true, bool $requestTable = false): void {
-    if ($requestTable) {
-        $URLString = 'requests.php?tags=';
-    } else {
-        $URLString = 'torrents.php?taglist=';
-    }
+function generate_tag_table(string $caption, string $tag, array $details, int $limit, bool $request = false): void {
 ?>
-    <h3>Top <?=$limit . ' ' . $caption?>
+    <h3>Top <?= $limit ?> <?= $caption ?>
         <small class="top10_quantity_links">
-<?php
-    switch ($limit) {
-        case 100:
-            ?>
+<?php if ($limit == 100) { ?>
             - <a href="top10.php?type=tags&amp;details=<?=$tag?>" class="brackets">Top 10</a>
             - <span class="brackets">Top 100</span>
             - <a href="top10.php?type=tags&amp;limit=250&amp;details=<?=$tag?>" class="brackets">Top 250</a>
-        <?php
-            break;
-        case 250:
-            ?>
+<?php } elseif ($limit == 250) { ?>
             - <a href="top10.php?type=tags&amp;details=<?=$tag?>" class="brackets">Top 10</a>
             - <a href="top10.php?type=tags&amp;limit=100&amp;details=<?=$tag?>" class="brackets">Top 100</a>
             - <span class="brackets">Top 250</span>
-        <?php
-            break;
-        default:
-            ?>
+<?php } else { ?>
             - <span class="brackets">Top 10</span>
             - <a href="top10.php?type=tags&amp;limit=100&amp;details=<?=$tag?>" class="brackets">Top 100</a>
             - <a href="top10.php?type=tags&amp;limit=250&amp;details=<?=$tag?>" class="brackets">Top 250</a>
-<?php
-    } ?>
+<?php } ?>
         </small>
     </h3>
     <table class="border">
@@ -80,15 +59,12 @@ function generate_tag_table(string $caption, string $tag, array $details, int $l
         <td class="center">Rank</td>
         <td>Tag</td>
         <td style="text-align: right;">Uses</td>
-<?php
-    if ($showVotes) {    ?>
+<?php if (!$request) { ?>
         <td style="text-align: right;">Pos. votes</td>
         <td style="text-align: right;">Neg. votes</td>
-<?php
-    }    ?>
+<?php } ?>
     </tr>
-<?php
-    if (empty($details)) { ?>
+<?php if (empty($details)) { ?>
         <tr class="rowb">
             <td colspan="9" class="center">
                 Found no tags matching the criteria
@@ -101,11 +77,11 @@ function generate_tag_table(string $caption, string $tag, array $details, int $l
     foreach ($details as $index => $detail) { ?>
     <tr class="row<?=$index % 2 ? 'a' : 'b'?>">
         <td class="center"><?= $index + 1 ?></td>
-        <td><a href="<?=$URLString?><?=$detail['Name']?>"><?=$detail['Name']?></a></td>
-        <td class="number_column"><?=number_format($detail['Uses'])?></td>
-<?php   if ($showVotes) { ?>
-        <td class="number_column"><?=number_format($detail['PositiveVotes'])?></td>
-        <td class="number_column"><?=number_format($detail['NegativeVotes'])?></td>
+        <td><a href="<?= $request ? 'requests.php?tags=' : 'torrents.php?taglist=' ?><?=$detail['name']?>"><?=$detail['name']?></a></td>
+        <td class="number_column"><?=number_format($detail['uses'])?></td>
+<?php   if (!$request) { ?>
+        <td class="number_column"><?=number_format($detail['posVotes'])?></td>
+        <td class="number_column"><?=number_format($detail['negVotes'])?></td>
 <?php   } ?>
     </tr>
 <?php
