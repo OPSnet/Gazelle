35efaf0a6fb6fe285f6be5736f46703f72864966
Spine <spine@orpheus.network>
2023-10-25 06:27:37 +0000

    sending a PM from a report is dead simple

--- sections/reports/takecompose.php
+++ sections/reports/takecompose.php
@@ -1,54 +1,23 @@
 <?php
+
 authorize();
 
-
-if (empty($_POST['toid'])) {
-    error(404);
+$userMan = new Gazelle\Manager\User;
+$recipient = $userMan->findById((int)($_POST['toid'] ?? 0));
+if (is_null($recipient)) {
+    error("No such recipient!");
 }
 
-if ($Viewer->disablePm() && !isset($StaffIDs[$_POST['toid']])) {
-    error(403);
+$subject = trim($_POST['subject']);
+if (empty($subject)) {
+    error("You can't send a message without a subject.");
 }
 
-$ConvID = false;
-if (isset($_POST['convid']) && is_number($_POST['convid'])) {
-    $ConvID = $_POST['convid'];
-    $Subject = '';
-    $ToID = (int)$_POST['toid'];
-    $db = Gazelle\DB::DB();
-    $db->prepared_query("
-        SELECT UserID
-        FROM pm_conversations_users
-        WHERE UserID = ?
-            AND ConvID = ?
-        ", $Viewer->id(), $ConvID
-    );
-    if (!$db->has_results()) {
-        error(403);
-    }
-} else {
-    $ToID = (int)$_POST['toid'];
-    $Subject = trim($_POST['subject']);
-    if (empty($Subject)) {
-        $Err = "You can't send a message without a subject.";
-    }
-}
-if (!$ToID) {
-    $Err = 'This recipient does not exist.';
-}
-$Body = trim($_POST['body'] ?? '');
-if ($Body === '') {
-    $Err = "You can't send a message without a body!";
+$body = trim($_POST['body'] ?? '');
+if ($body === '') {
+    error("You can't send a message without a body!");
 }
 
-if (!empty($Err)) {
-    error($Err);
-}
-
-if ($ConvID) {
-    (new Gazelle\Manager\User)->replyPM($ToID, $Viewer->id(), $Subject, $Body, $ConvID);
-} else {
-    (new Gazelle\Manager\User)->sendPM($ToID, $Viewer->id(), $Subject, $Body);
-}
+$userMan->sendPM($recipient->id(), $Viewer->id(), $subject, $body);
 
 header('Location: reports.php');
