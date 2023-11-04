2244f579bb1dba139969736226adbdee6af5132c
Spine <spine@orpheus.network>
2023-11-04 02:09:17 +0000

    fix email forwarding

--- sections/inbox/forward.php
+++ sections/inbox/forward.php
@@ -2,13 +2,13 @@
 
 authorize();
 
-$pm = (new Gazelle\Manager\PM($Viewer))->findById((int)$_GET['id']);
+$pm = (new Gazelle\Manager\PM($Viewer))->findById((int)$_POST['convid']);
 if (is_null($pm)) {
-    error(404);
+    error('Sorry, there is no trace of that conversation in your folder');
 }
 $recipient = (new Gazelle\Manager\User)->findById((int)$_POST['receiverid']);
 if (is_null($recipient)) {
-    error(404);
+    error('Sorry, there is no-one here by that name');
 }
 if (!$Viewer->permitted('users_mod') && !$recipient->isStaffPMReader()) {
     error(403);
