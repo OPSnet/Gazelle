b775ad6870e64f1c23381df2e363e16c5bbce5b5
Spine <spine@orpheus.network>
2023-11-13 22:38:22 +0000

    rename inbox handler files

--- sections/inbox/index.php
+++ sections/inbox/index.php
@@ -4,9 +4,9 @@ require_once(match ($_REQUEST['action'] ?? '') {
     'compose'     => 'compose.php',
     'forward'     => 'forward.php',
     'get_post'    => 'get_post.php',
-    'masschange'  => 'take_massdelete.php',
-    'takecompose' => 'take_compose.php',
-    'takeedit'    => 'take_edit.php',
+    'masschange'  => 'massdelete_handle.php',
+    'takecompose' => 'compose_handle.php',
+    'takeedit'    => 'edit_handle.php',
     'viewconv'    => 'conversation.php',
     default       => 'inbox.php',
 });
