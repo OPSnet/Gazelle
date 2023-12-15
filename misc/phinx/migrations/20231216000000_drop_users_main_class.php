1dcd48ad828da6951dcd0424dc862651c338c3af
Spine <spine@orpheus.network>
2023-12-15 23:19:22 +0000

    drop users_main.Class

---
+++ misc/phinx/migrations/20231216000000_drop_users_main_class.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+final class DropUsersMainClass extends AbstractMigration {
+    public function up(): void {
+        $this->table('users_main')
+            ->removeColumn('Class')
+            ->save();
+    }
+
+    public function down(): void {
+        $this->table('users_main')
+            ->addColumn('Class', 'integer', [
+                'limit' => MysqlAdapter::INT_TINY,
+            ])
+            ->save();
+    }
+}
