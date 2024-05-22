eb72b3826e5a0414c5f638e8bbf579c0327dfab5
Spine <spine@orpheus.network>
2024-05-22 19:39:14 +0000

    finish Top10 Tag twigification

--- sections/top10/tags.php
+++ sections/top10/tags.php
@@ -1,49 +1,10 @@
 <?php
 
-$details = $_GET['details'] ?? 'all';
-$details = in_array($_GET['details'] ?? '', ['top_used', 'top_request', 'top_voted'])
-    ? $details : 'all';
+$detail = $_GET['details'] ?? 'all';
+$limit  = $_GET['limit'] ?? 10;
 
-$limit = $_GET['limit'] ?? 10;
-$limit = in_array($limit, [10, 100, 250]) ? $limit : 10;
-
-$tag = new Gazelle\Manager\Tag();
-
-View::show_header(TOP_TEN_HEADING . " – Tags");
-?>
-<div class="thin">
-    <div class="header">
-        <h2><?= TOP_TEN_HEADING ?> – Tags</h2>
-        <?= $Twig->render('top10/linkbox.twig', ['selected' => 'tags']) ?>
-    </div>
-
-<?php
-if (in_array($details, ['all', 'top_used'])) {
-    echo $Twig->render('top10/tag.twig', [
-        'caption' => 'Most Used Torrent Tags',
-        'detail'  => 'top_used',
-        'list'    => $tag->topTGroupList($limit),
-        'limit'   => $limit,
-    ]);
-}
-
-if (in_array($details, ['all', 'top_request'])) {
-    echo $Twig->render('top10/tag.twig', [
-        'caption' => 'Most Used Request Tags',
-        'detail'  => 'top_request',
-        'list'    => $tag->topTGroupList($limit),
-        'limit'   => $limit,
-    ]);
-}
-
-if (in_array($details, ['all', 'top_voted'])) {
-    echo $Twig->render('top10/tag.twig', [
-        'caption' => 'Most Highly Voted Tags',
-        'detail'  => 'top_voted',
-        'list'    => $tag->topTGroupList($limit),
-        'limit'   => $limit,
-    ]);
-}
-?>
-</div>
-<?php View::show_footer() ?>
+echo $Twig->render('top10/tag.twig', [
+    'detail'  => in_array($detail, ['all', 'top_used', 'top_request', 'top_voted']) ? $detail : 'all',
+    'limit'   => in_array($limit, [10, 100, 250]) ? $limit : 10,
+    'manager' => new Gazelle\Manager\Tag(),
+]);
