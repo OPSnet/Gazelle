852b4a6549b68680280139cc984a2b1540ee6a0f
wally-crunk <122107861+wally-crunk@users.noreply.github.com>
2024-05-23 14:15:46 -0400

    pretty good progress on apollostage_navtive; none on others

---
+++ sass/plan9.sh
@@ -0,0 +1,133 @@
+sed -i -e 's/\$a-color/\$color-text-anchor/g' $1 
+
+sed -i -e 's/\$a-hover-color/\$color-text-anchor-hover/g' $1 
+
+sed -i -e 's/\$base-background-color/\$color-background-base/g' $1 
+
+sed -i -e 's/\$base-text-color/\$color-text-base/g' $1 
+
+sed -i -e 's/\$blockquote-background-color/\$color-background-blockquote/g' $1 
+
+sed -i -e 's/\$blockquote-border-color/\$color-accent-blockquote/g' $1 
+
+sed -i -e 's/\$bold-text-color/\$color-text-bold/g' $1 
+
+sed -i -e 's/\$box-background-color/\$color-background-box/g' $1 
+
+sed -i -e 's/\$button-base-color-1/\$color-obj-button-base-1/g' $1 
+
+sed -i -e 's/\$button-base-color-2/\$color-obj-button-base-2/g' $1 
+
+sed -i -e 's/\$button-text-color/\$color-text-button/g' $1 
+
+sed -i -e 's/\$footer-color/\$color-text-footer/g' $1 
+
+sed -i -e 's/\$fore-500/\$direct-fore-500/g' $1 
+
+sed -i -e 's/\$form-background-color/\$color-background-form/g' $1 
+
+sed -i -e 's/\$gradient-color-1/\$color-background-gradient-1/g' $1 
+
+sed -i -e 's/\$gradient-color-2/\$color-background-gradient-1/g' $1 
+
+sed -i -e 's/\$header-background-color/\$color-background-header/g' $1 
+
+sed -i -e 's/\$input-background-color/\$color-background-input/g' $1 
+
+sed -i -e 's/\$input-border-color-active/\$color-accent-border-active/g' $1 
+
+sed -i -e 's/\$input-border-color/\$color-accent-border-active/g' $1 
+
+sed -i -e 's/\$input-color/\$color-text-input/g' $1 
+
+sed -i -e 's/\$menu-li-fifth-child-background-color/\$color-obj-menu-5/g' $1 
+
+sed -i -e 's/\$menu-li-fourth-child-background-color/\$color-obj-menu-5/g' $1 
+
+sed -i -e 's/\$menu-li-ninth-child-background-color/\$color-obj-menu-9/g' $1 
+
+sed -i -e 's/\$menu-li-second-child-background-color/\$color-obj-menu-2/g' $1 
+
+sed -i -e 's/\$menu-li-seventh-child-background-color/\$color-obj-menu-7/g' $1 
+
+sed -i -e 's/\$menu-li-sixth-child-background-color/\$color-obj-menu-6/g' $1 
+
+sed -i -e 's/\$menu-li-third-child-background-color/\$color-obj-menu-3/g' $1 
+
+sed -i -e 's/\$menu-open-state-icon/\$icon-menu-open-state/g' $1 
+
+sed -i -e 's/\$mobile-pulldown-icon/\$icon-menu-pulldown-mobile/g' $1 
+
+sed -i -e 's/\$mono-text-color/\$color-text-mono/g' $1 
+
+sed -i -e 's/\$notification-background-color/\$color-notification-background/g' $1 
+
+sed -i -e 's/\$number-leechers-icon/\$icon-number-leechers/g' $1 
+
+sed -i -e 's/\$number-seeders-icon/\$icon-number-seeders/g' $1 
+
+sed -i -e 's/\$number-snatches-icon/\$icon-number-snatches/g' $1 
+
+sed -i -e 's/\$offset-accent-color/\$direct-accent-offset/g' $1 
+
+sed -i -e 's/\$poll-background-color/\$color-background-poll/g' $1 
+
+sed -i -e 's/\$primary-accent-color/\$direct-accent-primary/g' $1 
+
+sed -i -e 's/\$ratio-green-color/\$color-text-ratio-greenzone/g' $1 
+
+sed -i -e 's/\$ratio-red-color/\$color-text-ratio-redzone/g' $1 
+
+sed -i -e 's/\$ratio-yellow-color/\$color-text-ratio-yellowzone/g' $1 
+
+sed -i -e 's/\$read-icon/\$icon-message-read/g' $1 
+
+sed -i -e 's/\$read-locked-icon/\$icon-message-read-locked/g' $1 
+
+sed -i -e 's/\$read-locked-sticky-icon/\$icon-message-read-locked-sticky/g' $1 
+
+sed -i -e 's/\$read-sticky-icon/\$icon-message-read-sticky/g' $1 
+
+sed -i -e 's/\$secondary-table-background-color/\$color-background-table-secondary/g' $1 
+
+sed -i -e 's/\$selection-background-color/\$color-background-selection/g' $1 
+
+sed -i -e 's/\$selection-foreground-color/\$color-text-selection/g' $1 
+
+sed -i -e 's/\$site-logo-lockup/\$logo-site-lockup/g' $1 
+
+sed -i -e 's/\$site-logo-square/\$logo-site-square/g' $1 
+
+sed -i -e 's/\$span-blue-color/\$color-text-span-blue/g' $1 
+
+sed -i -e 's/\$span-green-color/\$color-text-span-green/g' $1 
+
+sed -i -e 's/\$table-background-color/\$color-background-table/g' $1 
+
+sed -i -e 's/\$tag-bubble-background-color/\$color-obj-tag_bubble-background/g' $1 
+
+sed -i -e 's/\$tag-bubble-foreground-color/\$color-obj-tag_bubble-foreground/g' $1 
+
+sed -i -e 's/\$torrent-background-color/\$color-background-torrent/g' $1 
+
+sed -i -e 's/\$unread-icon/\$icon-message-unread/g' $1 
+
+sed -i -e 's/\$unread-locked-icon/\$icon-message-unread-locked/g' $1 
+
+sed -i -e 's/\$unread-locked-sticky-icon/\$icon-message-unread-locked-sticky/g' $1 
+
+sed -i -e 's/\$unread-sticky-icon/\$icon-message-unread-sticky/g' $1 
+
+sed -i -e 's/\$userinfo-background-color-1/\$color-background-mobilemenu-userinfo-1/g' $1 
+
+sed -i -e 's/\$userinfo-background-color-2/\$color-background-mobilemenu-userinfo-2/g' $1 
+
+sed -i -e 's/\$userinfo-minor-color/\$color-background-menu-userinfo/g' $1 
+
+sed -i -e 's/\$userinfo-minor-hover-background-color/\$color-background-menu-userinfo-hover/g' $1 
+
+sed -i -e 's/\$userinfo-username-color/\$color-background-menu-username/g' $1 
+
+sed -i -e 's/\$userinfo-username-hover-background-color/\$color-background-menu-username-hover/g' $1 
+
+
