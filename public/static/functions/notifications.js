583cf03f4ef13ecd7931d53f1ee555471098917e
Spine <spine@orpheus.network>
2024-10-03 05:59:06 +0000

    refactor notifications ajax calls

--- public/static/functions/notifications.js
+++ public/static/functions/notifications.js
@@ -1,45 +1,49 @@
-/* global ajax */
+"use strict";
 
-function clearItem(torrentId) {
-    ajax.get("?action=notify_clear_item&torrentid=" + torrentId + "&auth=" + document.body.dataset.auth, function() {
-            $("#torrent" + torrentId).remove();
-        });
+async function clearItem(tid) {
+    await fetch(new Request(
+        "?action=notify_clear_item&torrentid=" + tid
+        + "&auth=" + document.body.dataset.auth
+    ));
+    document.getElementById('torrent' + tid).remove();
 }
 
-function clearSelected(filterId) {
-    var checkBoxes, checkedBoxes = [];
-    if (filterId) {
-        var filterForm = $('#notificationform_' + filterId);
-        checkBoxes = $('.notify_box_' + filterId, filterForm);
-    } else {
-        checkBoxes = $('.notify_box');
-    }
-    for (var i = checkBoxes.length - 1; i >= 0; i--) {
+async function clearSelected(filterId) {
+    const checkBoxes = filterId
+        ? $('.notify_box_' + filterId, $('#notificationform_' + filterId))
+        : $('.notify_box');
+    let checkedBoxes = [];
+    for (let i = checkBoxes.length - 1; i >= 0; i--) {
         if (checkBoxes[i].checked) {
             checkedBoxes.push(checkBoxes[i].value);
         }
     }
-    ajax.get("?action=notify_clear_items&torrentids=" + checkedBoxes.join(',') + "&auth=" + document.body.dataset.auth, function() {
-        for (var i = checkedBoxes.length - 1; i >= 0; i--) {
-            $('#torrent' + checkedBoxes[i]).remove();
-        }
-    });
+    await fetch(new Request(
+        "?action=notify_clear_items&torrentids=" + checkedBoxes.join(',')
+        + "&auth=" + document.body.dataset.auth
+    ));
+    for (let i = checkedBoxes.length - 1; i >= 0; i--) {
+        document.getElementById('torrent' + checkedBoxes[i]).remove();
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var notifyBoxes = $('.notify_box');
+document.addEventListener('DOMContentLoaded', () => {
+    let notifyBoxes = $('.notify_box');
     notifyBoxes.keydown(function(e) {
-        var nextBox, index = notifyBoxes.index($(this));
+        let nextBox = false;
+        let index = notifyBoxes.index($(this));
         if (index > 0 && e.which === 75) { // K
-            nextBox = notifyBoxes.get(index-1);
+            nextBox = notifyBoxes.get(index - 1);
         } else if (index < (notifyBoxes.length - 1) && e.which === 74) { // J
-            nextBox = notifyBoxes.get(index+1);
-        } else if (e.which === 88) {
+            nextBox = notifyBoxes.get(index + 1);
+        } else if (e.which === 88) { // X
             $(this).prop('checked', !$(this).prop('checked'));
         }
         if (nextBox) {
             nextBox.focus();
-            $(window).scrollTop($(nextBox).position()['top']-$(window).height()/4);
+            $(window).scrollTop(
+                $(nextBox).position()['top'] = $(window).height() / 4
+            );
         }
     });
 });
