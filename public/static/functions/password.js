d25a057b12bf5b455e82fd9cb42511010128d204
Spine <spine@orpheus.network>
2024-10-23 08:42:26 +0000

    remove js warning on login cooldown page

--- public/static/functions/password.js
+++ public/static/functions/password.js
@@ -1,15 +1,16 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('password_toggle').addEventListener('click', ev => {
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('password_toggle')?.addEventListener('click', (e) => {
         for (const name of ['password', 'new_pass_1', 'new_pass_2']) {
-            const field = document.getElementById(name);
+            let field = document.getElementById(name);
             if (field) {
                 if (field.getAttribute("type") === "password") {
                     field.setAttribute("type", "text");
-                    ev.target.innerHTML = '&#x1F62E;';
+                    e.target.innerHTML = '😮';
                 } else {
                     field.setAttribute("type", "password");
-                    ev.target.innerHTML = '&#x1FAE3;';
+                    e.target.innerHTML = '🫣';
                 }
             }
         }
