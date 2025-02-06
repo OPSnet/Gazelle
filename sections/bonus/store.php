94af9d94c1a527c6bec2d7473c2d9e0e548dfe28
Spine <spine@orpheus.network>
2025-02-06 06:47:44 +0000

    twigify bonus shop harder

--- sections/bonus/store.php
+++ sections/bonus/store.php
@@ -1,69 +1,36 @@
 <?php
 /** @phpstan-var \Gazelle\User $Viewer */
 /** @phpstan-var \Twig\Environment $Twig */
-// phpcs:disable Generic.WhiteSpace.ScopeIndent.IncorrectExact
-// phpcs:disable Generic.WhiteSpace.ScopeIndent.Incorrect
 
 $bonus = new Gazelle\User\Bonus($Viewer);
-
-View::show_header('Bonus Points Shop', ['js' => 'bonus']);
-?>
-<div class="header">
-    <h2>Bonus Points Shop</h2>
-</div>
-<div class="linkbox">
-    <a href="wiki.php?action=article&amp;name=bonuspoints" class="brackets">About Bonus Points</a>
-    <a href="bonus.php?action=bprates" class="brackets">Bonus Point Rates</a>
-    <a href="bonus.php?action=history" class="brackets">History</a>
-<?php if ($Viewer->permitted('admin_bp_history')) { ?>
-    <a href="bonus.php?action=cacheflush" title="Trigger price recalculation after changing 'bonus-discount' site option" class="brackets">Cache flush</a>
-<?php } ?>
-</div>
-
-<?php if (isset($_GET['complete'])) { ?>
-<div class="alertbar blend">
-    <?= $bonus->item($_GET['complete'])['Title'] ?> purchased!
-</div>
-<?php
-}
-
-if (($_GET['action'] ?? '') == 'donate') {
+$purchase = isset($_GET['complete']) ? $bonus->item($_GET['complete'])['Title'] : false;
+if (($_GET['action'] ?? '') !== 'donate') {
+    $donate = false;
+} else {
     authorize();
     $value = (int)$_POST['donate'];
     if ($Viewer->id() != $_POST['userid']) {
-?>
-<div class="alertbar blend">User error, no bonus points donated.</div>
-<?php } elseif ($value <= 0) { ?>
-<div class="alertbar blend">Warning! You cannot donate negative or no points!</div>
-<?php } elseif ($Viewer->bonusPointsTotal() < $value) { ?>
-<div class="alertbar blend">Warning! You cannot donate <?= number_format($value) ?> if you only have <?= number_format((int)$Viewer->bonusPointsTotal()) ?> points.</div>
-<?php } elseif ($bonus->donate((int)$_POST['poolid'], $value)) { ?>
-<div class="alertbar blend">Success! Your donation to the Bonus Point pool has been recorded.</div>
-<?php } else { ?>
-<div class="alertbar blend">No bonus points donated, insufficient funds.</div>
-<?php
+        $donate = 'User error, no bonus points donated.';
+    } elseif ($value <= 0) {
+        $donate = 'Warning! You cannot donate negative or no points!';
+    } elseif ($Viewer->bonusPointsTotal() < $value) {
+        $donate = 'Warning! You cannot donate ' . number_format($value)
+            . ' if you have only ' . number_format($Viewer->bonusPointsTotal(), 0)
+            . ' points.';
+    } elseif ($bonus->donate((int)$_POST['poolid'], $value)) {
+        $donate = 'Success! Your donation to the Bonus Point pool has been recorded.';
+    } else {
+        $donate = 'No bonus points donated, insufficient funds.';
     }
 }
 
-$points = (int)$Viewer->bonusPointsTotal();
 $bonusMan = new Gazelle\Manager\Bonus();
-$auth = $Viewer->auth();
-$pool = $bonusMan->getOpenPool();
-if ($pool) {
-    echo $Twig->render('bonus/bonus-pool.twig', [
-        'auth'    => $auth,
-        'points'  => $points,
-        'pool'    => $pool,
-        'user_id' => $Viewer->id(),
-    ]);
-}
 
 echo $Twig->render('bonus/store.twig', [
-    'admin'    => $Viewer->permitted('admin_bp_history'),
-    'auth'     => $auth,
-    'class'    => $Viewer->classLevel(),
+    'bonus'    => $bonus,
     'discount' => $bonusMan->discount(),
-    'list'     => $bonus->itemList(),
-    'points'   => $points,
-    'user_id'  => $Viewer->id(),
+    'donate'   => $donate,
+    'pool'     => $bonusMan->getOpenPool(),
+    'purchase' => $purchase,
+    'viewer'   => $Viewer,
 ]);
